999e49aae4001e2f002d5e71ee5ec2855991113b
Ryan Newton <rrnewton@gmail.com>
2016-10-05 00:20:28 +0200

    WIP: Partway through adding indirections to treebench_unpacked.c

--- BintreeBench/treebench_unpacked.c
+++ BintreeBench/treebench_unpacked.c
@@ -6,10 +6,18 @@
 #include <time.h>
 
 #define TRIALS 17
+#define TAG_SIZE 1
+
+// Should be at least log_2(numProcs)
+#define PARLVLS 0
+#define MINSEQLVLS 1
+
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 enum Tree {
     Leaf,
     Node,
+    Indirect,
 };
 
 // Manual layout:
@@ -42,43 +50,87 @@ int treeSize(int n) {
   int leaves = 1 << n;
   int nodes  = leaves - 1;
   // Both nodes and leaves are tagged:
-  int bytes  = (sizeof(Num)*leaves + sizeof(char)*(nodes+leaves));
-  /* printf("treeSize(%d): %d bytes (%d/%d nodes/leaves)\n", */
-  /*        n, bytes, nodes, leaves); */
+  int bytes  = sizeof(Num)*leaves + TAG_SIZE * sizeof(char) * (nodes+leaves);
   return bytes;
 }
 
-TreeRef buildTree(int n) {
+      
+TreeRef buildSeqTree(int n, Num root) {
   int bytes = treeSize(n);
   char* buf = malloc(bytes);
-  char* res = fillTree(buf, n, 1);
-  printf("wrote %d\n", (int)(res - buf));  
+  char* res = fillTree(buf, n, root);
   return buf;
 }
 
+TreeRef buildParTree(int p, int n, Num root) {
+  if (p == 0) return buildSeqTree(n,root);
+  char* start = malloc(3 * TAG_SIZE + 2 * sizeof(void*));
+  char* buf = start;
+  buf[0] = Node;
+  buf[1] = Indirect;
+  buf += 2;
+  ((void**)buf)[0] = buildParTree(p-1, n, root);
+  buf += sizeof(void*);
+  buf[0] = Indirect;
+  buf += 1;
+  ((void**)buf)[0] = buildParTree(p-1, n, root + (1<<((p+n)-1)));
+  return start;
+}
+
+TreeRef buildTree(int n) {
+  if (n >= MINSEQLVLS) {
+    int p  = MIN(PARLVLS, n - MINSEQLVLS);
+    int n2 = n - p;
+    return buildParTree(p, n2, 1);
+  } else {
+    return buildSeqTree(n, 1);
+  }
+}
+
+// Returns the cursor position AFTER scrolling past the input argument.
 TreeRef printTree(TreeRef t) {
-  if (*t == Leaf) {
+  switch(*t) {
+  case Leaf:
     t++;
     printf("%lld", *(Num*)t);
     return (t+sizeof(Num));
-  } else {
+
+  case Node:
     t++;
     printf("(");
     TreeRef t2 = printTree(t);
     printf(",");
     TreeRef t3 = printTree(t2);
     printf(")");
-    return t3;    
+    return t3;
+    
+  case Indirect:
+    t++;
+    printTree(*((TreeRef*)t));
+    // Then scroll past the pointer and return.
+    return (t + sizeof(void*));
+
+  default:
+    fprintf(stderr, "Error! corrupt tree in printTree, at %p, expected tag got: %d\n", t, (*t));
+    exit(-1);
+    return 0;
   }
 }
 
+typedef struct { TreeRef tin; TreeRef tout; } RefPair;
+
 TreeRef add1Tree(TreeRef t, TreeRef tout) {
-  if (*t == Leaf) {
+  // printf("Add1tree %p -> %p, ", t, tout);
+  switch(*t) {
+  case Leaf:
+    // printf(" leaf %lld\n", *(Num*)(t+1));
     *tout = Leaf;    
     t++; tout++;
     *(Num*)tout = *(Num*)t + 1;
     return (t+sizeof(Num));
-  } else {
+
+  case Node:
+    // printf(" node\n");
     *tout = Node;
     t++; tout++;
 
@@ -87,8 +139,23 @@ TreeRef add1Tree(TreeRef t, TreeRef tout) {
     // tout += 4;
     
     TreeRef t2 = add1Tree(t,tout);
-    tout += (t2 - t);
+    tout += (t2 - t); // HACK: FIXME - Won't work with indirections.
     return add1Tree(t2,tout);
+
+  // Here we have a choice, we could keep the indirection or inline
+  // it.  For starters let's just inline.  We'll need to compute a
+  // length of the output buffer to create if we don't.
+  case Indirect:
+    // fprintf(stderr, "Indirect, tout = %p\n", tout);
+    t++;
+    TreeRef tnew = *((TreeRef*)t);
+    add1Tree(tnew, tout);
+    return t + sizeof(void*);
+    
+  default:
+    fprintf(stderr, "Corrupt tree in add1Tree, at %p expected tag got: %d\n", t, (*t));
+    exit(-1);
+    return 0;
   }
 }
 
@@ -100,6 +167,16 @@ int compare_doubles (const void *a, const void *b)
 }
 
 int main(int argc, char** argv) {
+
+  TreeRef ta = buildTree(3);
+  TreeRef tb = malloc(treeSize(3));
+  printf("Allocated tb space: %d, %p \n", treeSize(3), tb);
+  add1Tree(ta, tb);
+  printTree(ta);
+  printf("\ntb:\n");
+  printTree(tb);  
+  return 0;
+  
   int depth;
   if (argc > 1)
     depth = atoi(argv[1]);
