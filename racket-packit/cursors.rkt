fbabb8dc7f2aa57cce9c93f6c641d1724ea16fce
Ryan Newton <rrnewton@gmail.com>
2016-10-15 14:14:05 -0400

    Implement bytestring version in Racket.. it's too slow.  Twice as slow as vector

--- racket-packit/cursors.rkt
+++ racket-packit/cursors.rkt
@@ -1,11 +1,20 @@
 #lang racket
 
 (require racket/performance-hint)
+(require (for-syntax (only-in racket range)))
 (provide (all-defined-out))
+
+;; Safe mode for debugging:
+(require (rename-in racket/fixnum [fx= unsafe-fx=]))
+;; Unsafe mode for performance:
+; (require racket/unsafe/ops)
+
+;;--------------------------------------------------------------------------------
+
 
 (define-inline (f x) (add1 x))
 
- (f 99)
+(f 99)
 
 (struct cursor (buf [offset #:mutable]) #:transparent)
 
@@ -16,6 +25,7 @@
 ;; Option 1: vector based implementation of buffers
 ;--------------------------------------------------------------------------------
 
+#;
 (begin 
   ;; FINISHME - indirections:
   ; (define indirection-tag (gensym 'indirection))
@@ -62,6 +72,66 @@
 ;; Option 2: bytestring based:
 ;--------------------------------------------------------------------------------
 
+(begin 
 
+  (define (new-buffer n) (cursor (make-bytes n) 0))
+
+  (define (buffer-size c) (bytes-length (cursor-buf c)))
+
+  (define-syntax define-tags
+    (lambda (x)
+      (syntax-case x ()
+        [(define-tags (name ...))
+         (let ([lst (syntax->list #'(name ...))])
+           #`(begin
+               #,@(datum->syntax x
+                   (for/list ([nm lst]
+                              [ind (in-range 255 (- 255 (length lst)) -1)])
+                     #`(define #,nm #,(datum->syntax nm ind)))))
+           )])))
+
+  (define-inline (tag=? a b) (unsafe-fx= a b))
+
+  (define-inline (advance-cursor! c bytes)
+    (set-cursor-offset! c (+ (cursor-offset c) bytes)))
+
+  (define-inline (read-tag! c)
+    (let* ([off (cursor-offset c)])
+      (set-cursor-offset! c (add1 off))
+      (bytes-ref (cursor-buf c) off)))
+
+  (define-inline (read-int64! c)
+    (let* ([off (cursor-offset c)])
+      (set-cursor-offset! c (+ int64-slots off))
+      (integer-bytes->integer (cursor-buf c) #t ; signed
+                              #f off (+ off 8))))
+
+  (define-inline (write-tag! c x)
+    (let* ([off (cursor-offset c)])
+      (set-cursor-offset! c (add1 off))
+      (bytes-set! (cursor-buf c) off x)))
+
+  (define-inline (write-int64! c x)
+    (let* ([off (cursor-offset c)])
+      (set-cursor-offset! c (+ int64-slots off))
+      (integer->integer-bytes
+       x 8 #t ; signed
+       #f ; big endian       
+       (cursor-buf c) off)))
+  
+  (define tag-slots   1) ; single byte
+  (define int64-slots 8) ; (ctype-sizeof _llong)
+  )
   
 ;--------------------------------------------------------------------------------
+
+;; Example:
+#;
+(begin 
+  (define-tags (Leaf Node))
+  (let ([b (new-buffer 30)])
+    (write-tag! b Node)
+    (write-tag! b Leaf) (write-int64! b 34)
+    (write-tag! b Leaf) (write-int64! b 12)
+    (bytes->list (cursor-buf b))))
+
