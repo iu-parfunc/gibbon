14d745eda9e8831b9874e17404ca670374b3872f
Michael Vollmer <vollmerm@indiana.edu>
2016-10-19 16:19:07 -0400

    Comment out stuff, add thoughts

--- ToyTree/src/Lang/Regions.hs
+++ ToyTree/src/Lang/Regions.hs
@@ -47,15 +47,33 @@ data RTopLevel = RTopLevel RTyEnv [RFunDecl] RExpr
 data RFunDecl = RFunDecl Name [Name] RExpr
   deriving (Read,Show,Eq,Ord,Generic)
 
+-- * Expressions in the region language
+--   How do we express the concept of threading regions through the computation?
+--   We could do it explicitly in the AST with something like the commented out code
+--   below, or we could take advantage of the fact that everything has a unique name
+--   and use our region relations/constraints above.
+--   For example, if we have:
+--     case e of
+--       A b c -> let x = f b
+--                    y = f c
+--                    z = A x y
+--                in z
+--   Here we can have relations like (At e p1) (At b p2) (At c p3) (Inside p1 p2)
+--   and even (After p3 p2)...
+--   But how do we say that doing `f b` should get us the region to read c?
+--   If we had a notion of linearity for regions, we could say that having consumed
+--   some name x where we know (At x p1) and (After p2 p1) we can then consume
+--   some name y where (At y p2). 
 data RExpr = RVarE Name
            | RCaseE [(Name,Name,RExpr)]
-           | RLetE [(Name,RExpr)] RExpr
-           | RLetRegE Name RExpr
-           | RLetStepE [((Name,Region),RExpr)] (RExpr,Region)
-           | RConstrE Name [(RExpr,Region)]
+           | RLetE [(Name,RExpr)] RExpr 
+           -- | RLetStepE [((Name,Region),RExpr)] (RExpr,Region)
+           -- | RConstrE Name [(RExpr,Region)]
+           | RConstrE Name [Name] -- params already have regions... is that enough?
            | RPrimOpE L1.Prim [Name]
            | RIfE Name RExpr RExpr
            | RAppE Name Name
+           -- | RLetRegE Name RExpr              
            | RIntE Int
            | RBoolE Bool
   deriving (Read,Show,Eq,Ord,Generic)
