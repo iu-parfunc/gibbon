567029e12a1996f4bfdfb463e68214d6598fb9a2
Mike Vollmer <mike@talkingincircles.net>
2016-10-21 01:30:33 -0400

    Write add1 in region lang with holes

--- ToyTree/src/Lang/Regions.hs
+++ ToyTree/src/Lang/Regions.hs
@@ -26,21 +26,17 @@ data RTy = ArrTy [Region] [Ty] Ty [Region]
          | TyCon TyName
   deriving (Read,Show,Eq,Ord,Generic)
 
-data Ty = Ty RTy Region
-  deriving (Read,Show,Eq,Ord,Generic)
+type Ty = (RTy,Region)
 
 -- * Region types
 data Region = Reg Name
             | RegZero
-            | RegHole Name
+            | RegHole -- Name
             | RegAfter Region
   deriving (Read,Show,Eq,Ord,Generic)
 
 -- * Relations on regions. Constraints? Don't know what to call these.
---   Associations between names and regions go here, rather than in the AST,
---   since we're assuming all names are unique and using ANF. 
 data Rel = RelInside Region Region
-         | RelAt Name Region
          | RelEquiv Region Region
  deriving (Read,Show,Eq,Ord,Generic)
            
@@ -59,10 +55,11 @@ data FunDecl = FunDecl Name [Name] Expr
 --   is like a limited form of let-values, and a return form, which is
 --   like a limited form of values.
 data Expr = VarE Name
-          | CaseE [(Name,Name,Expr)]
+          | CaseE Name [(Name,Name,Expr)]
           | LetValE [(Name,Expr)] Expr
           | LetCallE Name [Name] [Region] Name [Region] Expr
-          | ConstrE TyName [Name]
+          | ConstrPE TyName [Name]
+          | ConstrSE TyName Name
           | ProjE TyName Name Int
           | PrimOpE L1.Prim [Name]
           | IfE Name Expr Expr
@@ -72,4 +69,45 @@ data Expr = VarE Name
           | LetRegion Region Expr
           | Traverse Region Name Region -- consume a variable somehow
   deriving (Read,Show,Eq,Ord,Generic)
+
+exadd1 :: TopLevel
+exadd1 =
+  TopLevel
+  (TyEnv [("Tree", (SumTy [("LeafP", (TyCon "LeafP",RegHole)),
+                           ("NodeP", (TyCon "NodeP",RegHole))],RegHole)),
+          ("NodeP", (PackProdTy [(TyCon "Tree",RegHole),(TyCon "Tree",RegHole)],RegHole)),
+          ("LeafP", (PackProdTy [(IntTy,RegHole)],RegHole)),
+          ("add1", ((ArrTy [RegHole,RegHole]
+                     [(TyCon "Tree",RegHole)]
+                     ((TyCon "Tree"),RegHole)
+                     [RegHole,RegHole]
+                    ),RegHole)),
+          ("t", ((TyCon "Tree"),RegHole)),
+          ("xl", ((TyCon "LeafP"),RegHole)),
+          ("xn", ((TyCon "NodeP"),RegHole)),
+          ("v1", ((IntTy),RegHole)),
+          ("v2", ((IntTy),RegHole)),
+          ("v3", ((IntTy),RegHole)),
+          ("x1", ((TyCon "Tree"),RegHole)),
+          ("x2", ((TyCon "Tree"),RegHole)),
+          ("y1", ((TyCon "Tree"),RegHole)),
+          ("y2", ((TyCon "Tree"),RegHole)),
+          ("y3", ((TyCon "NodeP"),RegHole)),
+          ("v4", ((TyCon "LeafP"),RegHole))]
+  [])
+  [(FunDecl "add1" ["t"]
+     (CaseE "t" [
+         ("Leaf", "xl",
+          (LetValE [("v1",(IntE 1)),("v2",(ProjE "LeafP" "x" 0))]
+           (LetValE [("v3",(PrimOpE L1.PlusP ["v1","v2"]))]
+            (LetValE [("v4",(ConstrPE "LeafP" ["v3"]))]
+             (ConstrSE "Tree" "v4"))))),
+         ("Node", "xn",
+          (LetValE [("x1",(ProjE "NodeP" "x" 0)),("x2",(ProjE "NodeP" "x" 1))]
+           (LetCallE "add1" ["x1"] [RegHole,RegHole] "y1" [RegHole,RegHole]
+            (LetCallE "add1" ["x2"] [RegHole,RegHole] "y2" [RegHole,RegHole]
+             (LetValE [("y3", (ConstrPE "NodeP" ["y1","y2"]))]
+              (ConstrSE "Tree" "y3"))))))
+         ]))]
+  (IntE 0)
 
