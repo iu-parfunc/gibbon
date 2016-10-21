cfa284fff3ef6e777f710e47a945a650819759d7
Mike Vollmer <mike@talkingincircles.net>
2016-10-21 01:06:44 -0400

    Slight changes to region types

--- ToyTree/src/Lang/Regions.hs
+++ ToyTree/src/Lang/Regions.hs
@@ -11,59 +11,65 @@ type TyName = L1.TyName
     
 -- * Types for the region language are similar to the source language
 --   Cursors have a region in their type.
-data RTy = RArrTy [Region] [RTy] RTy [Region]
-         | RPackProdTy [RTy]
-         | RTupleTy [RTy]
-         | RSumTy [(TyName,[RTy])]
-         | RIntTy 
-         | RBoolTy 
-         | RVarTy TyName
-         | RCursor Region
-         | RSymTy
-         | RSymMapTy RTy
-         | RTyCon TyName
+--   New experiment: associate a region with each type by making
+--   RTy and Ty mutually recursive data types.
+data RTy = ArrTy [Region] [Ty] Ty [Region]
+         | PackProdTy [Ty]
+         | TupleTy [Ty]
+         | SumTy [(TyName,Ty)]
+         | IntTy 
+         | BoolTy 
+         | VarTy TyName
+         | Cursor Region
+         | SymTy
+         | SymMapTy Ty
+         | TyCon TyName
   deriving (Read,Show,Eq,Ord,Generic)
-           
--- * Region types are either a region parameter or region zero
+
+data Ty = Ty RTy Region
+  deriving (Read,Show,Eq,Ord,Generic)
+
+-- * Region types
 data Region = Reg Name
             | RegZero
-        --  | RegHole Name 
+            | RegHole Name
+            | RegAfter Region
   deriving (Read,Show,Eq,Ord,Generic)
 
 -- * Relations on regions. Constraints? Don't know what to call these.
 --   Associations between names and regions go here, rather than in the AST,
 --   since we're assuming all names are unique and using ANF. 
-data RRel = After Region Region
-          | Inside Region Region
-          | At Name Region
+data Rel = RelInside Region Region
+         | RelAt Name Region
+         | RelEquiv Region Region
  deriving (Read,Show,Eq,Ord,Generic)
            
 -- * Region type environments also have a list of things we know about the regions
-data RTyEnv = RTyEnv [(TyName,RTy)] [RRel]
+data TyEnv = TyEnv [(TyName,Ty)] [Rel]
   deriving (Read,Show,Eq,Ord,Generic)
 
-data RTopLevel = RTopLevel RTyEnv [RFunDecl] RExpr
+data TopLevel = TopLevel TyEnv [FunDecl] Expr
   deriving (Read,Show,Eq,Ord,Generic)
 
-data RFunDecl = RFunDecl Name [Name] RExpr
+data FunDecl = FunDecl Name [Name] Expr
   deriving (Read,Show,Eq,Ord,Generic)
     
 -- * Expressions in the region language
 --   Here the application form is replaced with a letcall form, which
---   is like a limited form of let-values, and a yield form, which is
+--   is like a limited form of let-values, and a return form, which is
 --   like a limited form of values.
-data RExpr = RVarE Name
-           | RCaseE [(Name,Name,RExpr)]
-           | RLetValE [(Name,RExpr)] RExpr
-           | RLetCallE Name [Name] [Region] Name [Region] RExpr
-           | RConstrE TyName [Name]
-           | RProjE TyName Name Int
-           | RPrimOpE L1.Prim [Name]
-           | RIfE Name RExpr RExpr
-           | RIntE Int
-           | RBoolE Bool
-           | RReturn Name [Region]
-           | RLetRegion Region RExpr
-           | RTraverse Region Name Region -- consume a variable somehow
+data Expr = VarE Name
+          | CaseE [(Name,Name,Expr)]
+          | LetValE [(Name,Expr)] Expr
+          | LetCallE Name [Name] [Region] Name [Region] Expr
+          | ConstrE TyName [Name]
+          | ProjE TyName Name Int
+          | PrimOpE L1.Prim [Name]
+          | IfE Name Expr Expr
+          | IntE Int
+          | BoolE Bool
+          | Return Name [Region]
+          | LetRegion Region Expr
+          | Traverse Region Name Region -- consume a variable somehow
   deriving (Read,Show,Eq,Ord,Generic)
 
