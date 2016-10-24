7c9cb4494f9d62b1686f230bc37954534a415eb1
Michael Vollmer <vollmerm@indiana.edu>
2016-10-23 21:13:10 -0400

    Update Target lang, write add1

--- PackedTreesFormal/src/Packed/FirstOrder/Target.hs
+++ PackedTreesFormal/src/Packed/FirstOrder/Target.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes     #-}
 {-# LANGUAGE TemplateHaskell #-}
 
@@ -21,49 +22,59 @@ import Packed.HigherOrder.L1_Source (T1 (..))
 -- * AST definition
 
 type Var = String
+type Tag = Word8
 
 data Triv
-  = VarTriv Var
-  | IntTriv Int
+    = VarTriv Var
+    | IntTriv Int
+    | TagTriv Tag
   deriving (Show)
 
-data Exp
-  = TrivE Triv
-  | PrimAppE Prim [Triv]
-  | AppE Var [Triv]
-  | MkTupleE [Triv]
-  | ProjE Var Int
-  | LetE [(Var,Ty,Exp)] Exp
-  | IfEqE (Triv,Triv) Exp Exp
-      -- ^ For casing on numeric tags.
-  | NewBufE Int
-      -- ^ Allocate a new buffer, return a cursor.
-  | WriteTagE Var Word8
-      -- ^ Write a tag at a cursor.
-  | WriteIntE Var Triv
-      -- ^ Write (leaf) data
-  | ReadTagE Var
-      -- ^ Read one byte from the cursor and advance it.
-  | ReadIntE Var
-      -- ^ Read an 8 byte Int from the cursor and advance.
+data Tail
+    = RetValsT [Triv] -- ^ Only in tail position, for returning from a function.
+    | LetCallT { binds :: [(Var,Ty)],
+                 rator :: Var,
+                 rands :: [Triv],
+                 bod   :: Tail }
+    | LetPrimCallT { binds :: [(Var,Ty)],
+                     prim  :: Prim,
+                     rands :: [Triv],
+                     bod   :: Tail }
+    | Switch Triv [(Tag,Tail)] (Maybe Tail)
+    -- ^ For casing on numeric tags.
+    | TailCall Var [Triv]
   deriving (Show)
 
 data Ty
-  = IntTy
-  | SymTy
-      -- ^ Symbols used in writing compiler passes
-  | CursorTy
-  | ProdTy [Ty]
-  | SymDictTy T1
+    = IntTy
+    | TagTy -- ^ A single byte.
+    | SymTy -- ^ Symbols used in writing compiler passes.
+            --   It's an alias for Int, an index into a symbol table.
+    | CursorTy -- ^ A byte-indexing pointer.
+    | ProdTy [Ty]
+    | SymDictTy T1
       -- ^ We allow built-in dictionaries from symbols to a value type.
-  deriving (Show)
+  deriving (Show,Eq,Ord)
 
 data Prim
-  = AddP
-  | SubP
-  | MulP
-  | DictInsertP -- ^ takes k,v,dict
-  | DictLookupP -- ^ takes k,dict, errors if absent
+    = AddP
+    | SubP
+    | MulP
+    | DictInsertP -- ^ takes k,v,dict
+    | DictLookupP -- ^ takes k,dict, errors if absent
+    | NewBuf 
+    -- ^ Allocate a new buffer, return a cursor.
+    | WriteTag
+    -- ^ Write a static tag value, takes a cursor to target.
+    | WriteInt
+    -- ^ Write (leaf) data, takes cursor,int
+    | ReadTag
+    -- ^ Read one byte from the cursor and advance it.
+    | ReadInt
+      -- ^ Read an 8 byte Int from the cursor and advance.
+  deriving (Show)
+
+data FunDecl = FunDecl Var [(Var,Ty)] Ty Tail
   deriving (Show)
 
 --------------------------------------------------------------------------------
@@ -74,97 +85,124 @@ newtype Cg = Cg { unwrapCg :: () }
 codegen_triv :: Triv -> C.Exp
 codegen_triv (VarTriv v) = C.Var (C.toIdent v noLoc) noLoc
 codegen_triv (IntTriv i) = [cexp| $i |]
+codegen_triv (TagTriv i) = [cexp| $i |]
 
-codegen_exp
-  :: Exp     -- ^ source expression
-  -> C.Exp   -- ^ where to bind the result (must be a valid LHS)
-  -> [C.BlockItem]
+-- codegen_exp
+--   :: Exp     -- ^ source expression
+--   -> C.Exp   -- ^ where to bind the result (must be a valid LHS)
+--   -> [C.BlockItem]
 
-codegen_exp (TrivE triv) ret =
-    [ C.BlockStm [cstm| $ret = $(codegen_triv triv); |] ]
+-- codegen_exp (TrivE triv) ret =
+--     [ C.BlockStm [cstm| $ret = $(codegen_triv triv); |] ]
 
-codegen_exp (PrimAppE AddP as) ret = codegen_binop C.Add as ret
-codegen_exp (PrimAppE SubP as) ret = codegen_binop C.Sub as ret
-codegen_exp (PrimAppE MulP as) ret = codegen_binop C.Mul as ret
+-- codegen_exp (PrimAppE AddP as) ret = codegen_binop C.Add as ret
+-- codegen_exp (PrimAppE SubP as) ret = codegen_binop C.Sub as ret
+-- codegen_exp (PrimAppE MulP as) ret = codegen_binop C.Mul as ret
 
-codegen_exp (PrimAppE DictInsertP [k, v, dict]) ret =
-    [ C.BlockStm [cstm| $ret = dict_insert($dict', $k', $v'); |] ]
-  where
-    k' = codegen_triv k
-    v' = codegen_triv v
-    dict' = codegen_triv dict
+-- codegen_exp (PrimAppE DictInsertP [k, v, dict]) ret =
+--     [ C.BlockStm [cstm| $ret = dict_insert($dict', $k', $v'); |] ]
+--   where
+--     k' = codegen_triv k
+--     v' = codegen_triv v
+--     dict' = codegen_triv dict
 
-codegen_exp (PrimAppE DictLookupP [k, dict]) ret =
-    [ C.BlockStm [cstm| $ret = dict_lookup($dict', $k'); |] ]
-  where
-    k' = codegen_triv k
-    dict' = codegen_triv dict
+-- codegen_exp (PrimAppE DictLookupP [k, dict]) ret =
+--     [ C.BlockStm [cstm| $ret = dict_lookup($dict', $k'); |] ]
+--   where
+--     k' = codegen_triv k
+--     dict' = codegen_triv dict
 
-codegen_exp invalid@PrimAppE{} _ =
-    error ("codgen_exp: Invalid PrimApp: " ++ show invalid)
+-- codegen_exp invalid@PrimAppE{} _ =
+--     error ("codgen_exp: Invalid PrimApp: " ++ show invalid)
 
-codegen_exp (AppE v as) ret =
-    [ C.BlockStm [cstm| $ret = $( C.FnCall fn args noLoc ); |] ]
-  where
-    fn = C.Var (C.toIdent v noLoc) noLoc
-    args = map codegen_triv as
+-- codegen_exp (AppE v as) ret =
+--     [ C.BlockStm [cstm| $ret = $( C.FnCall fn args noLoc ); |] ]
+--   where
+--     fn = C.Var (C.toIdent v noLoc) noLoc
+--     args = map codegen_triv as
 
--- TODO: MkTupleE
--- TODO: ProjE
+-- -- TODO: MkTupleE
+-- -- TODO: ProjE
 
-codegen_exp (LetE binds e) ret =
-    concatMap codegen_binds binds ++ codegen_exp e ret
+-- codegen_exp (LetE binds e) ret =
+--     concatMap codegen_binds binds ++ codegen_exp e ret
 
-codegen_exp (IfEqE (t1, t2) then_ else_) ret =
-    [ C.BlockStm $
-      C.If [cexp| $t1' == $t2' |]
-           (mkBlock (codegen_exp then_ ret))
-           (Just (mkBlock (codegen_exp else_ ret)))
-           noLoc ]
-  where
-    t1' = codegen_triv t1
-    t2' = codegen_triv t2
+-- codegen_exp (IfEqE (t1, t2) then_ else_) ret =
+--     [ C.BlockStm $
+--       C.If [cexp| $t1' == $t2' |]
+--            (mkBlock (codegen_exp then_ ret))
+--            (Just (mkBlock (codegen_exp else_ ret)))
+--            noLoc ]
+--   where
+--     t1' = codegen_triv t1
+--     t2' = codegen_triv t2
 
-codegen_exp (NewBufE size) ret =
-    [ C.BlockStm [cstm| $ret = (char*)malloc($size); |] ]
+-- codegen_exp (NewBufE size) ret =
+--     [ C.BlockStm [cstm| $ret = (char*)malloc($size); |] ]
 
-codegen_exp (WriteTagE ptr w) _ =
-    [ C.BlockStm [cstm| *$(cid ptr) = $w; |]
-    , C.BlockStm [cstm| $(cid ptr)++;     |] ]
+-- codegen_exp (WriteTagE ptr w) _ =
+--     [ C.BlockStm [cstm| *$(cid ptr) = $w; |]
+--     , C.BlockStm [cstm| $(cid ptr)++;     |] ]
 
-codegen_exp (WriteIntE ptr t) _ =
-    [ C.BlockStm [cstm| *((int*)$(cid ptr)) = $(codegen_triv t); |]
-    , C.BlockStm [cstm| $(cid ptr) += sizeof(int);               |] ]
-  where
-    ptr' = C.Var (C.toIdent ptr noLoc) noLoc
+-- codegen_exp (WriteIntE ptr t) _ =
+--     [ C.BlockStm [cstm| *((int*)$(cid ptr)) = $(codegen_triv t); |]
+--     , C.BlockStm [cstm| $(cid ptr) += sizeof(int);               |] ]
+--   where
+--     ptr' = C.Var (C.toIdent ptr noLoc) noLoc
 
-codegen_exp (ReadTagE ptr) ret =
-    [ C.BlockStm [cstm| $ret = *$(cid ptr); |]
-    , C.BlockStm [cstm| $(cid ptr)++;       |] ]
+-- codegen_exp (ReadTagE ptr) ret =
+--     [ C.BlockStm [cstm| $ret = *$(cid ptr); |]
+--     , C.BlockStm [cstm| $(cid ptr)++;       |] ]
 
-codegen_exp (ReadIntE ptr) ret =
-    [ C.BlockStm [cstm| $ret = *((int*)$(cid ptr)); |]
-    , C.BlockStm [cstm| $(cid ptr) += sizeof(int);  |] ]
+-- codegen_exp (ReadIntE ptr) ret =
+--     [ C.BlockStm [cstm| $ret = *((int*)$(cid ptr)); |]
+--     , C.BlockStm [cstm| $(cid ptr) += sizeof(int);  |] ]
 
-codegen_binds :: (Var, Ty, Exp) -> [C.BlockItem]
-codegen_binds (v, ty, e) =
-    ( C.BlockDecl (C.AntiDecl (codegen_ty ty ++ " " ++ v ++ ";") noLoc)
-    : codegen_exp e (cid v) )
+-- codegen_binds :: (Var, Ty, Exp) -> [C.BlockItem]
+-- codegen_binds (v, ty, e) =
+--     ( C.BlockDecl (C.AntiDecl (codegen_ty ty ++ " " ++ v ++ ";") noLoc)
+--     : codegen_exp e (cid v) )
 
-codegen_binop :: C.BinOp -> [Triv] -> C.Exp -> [C.BlockItem]
-codegen_binop op as ret =
-    [ C.BlockStm
-      [cstm| $ret = $(foldl1' (\e1 e2 -> C.BinOp op e1 e2 noLoc) (map codegen_triv as)); |] ]
+-- codegen_binop :: C.BinOp -> [Triv] -> C.Exp -> [C.BlockItem]
+-- codegen_binop op as ret =
+--     [ C.BlockStm
+--       [cstm| $ret = $(foldl1' (\e1 e2 -> C.BinOp op e1 e2 noLoc) (map codegen_triv as)); |] ]
 
-codegen_ty :: Ty -> String
-codegen_ty IntTy    = "int"
-codegen_ty SymTy    = "sym"
-codegen_ty CursorTy = "cursor"
-codegen_ty ProdTy{} = undefined -- TODO: This is more general than we need I think
-codegen_ty SymDictTy{} = undefined -- TODO: Not sure what this is
+-- codegen_ty :: Ty -> String
+-- codegen_ty IntTy    = "int"
+-- codegen_ty SymTy    = "sym"
+-- codegen_ty CursorTy = "cursor"
+-- codegen_ty ProdTy{} = undefined -- TODO: This is more general than we need I think
+-- codegen_ty SymDictTy{} = undefined -- TODO: Not sure what this is
 
 mkBlock :: [C.BlockItem] -> C.Stm
 mkBlock ss = C.Block ss noLoc
 
 cid :: Var -> C.Exp
 cid v = C.Var (C.toIdent v noLoc) noLoc
+
+leafTag :: Word8
+leafTag = fromIntegral 0
+
+nodeTag :: Word8
+nodeTag = fromIntegral 1
+
+exadd1 :: FunDecl
+exadd1 = FunDecl "add1" [("t",CursorTy),("tout",CursorTy)] CursorTy exadd1Tail 
+
+exadd1Tail :: Tail
+exadd1Tail =
+    LetPrimCallT [("ttag",TagTy),("t2",CursorTy)] ReadTag [VarTriv "t"]
+  $ Switch (VarTriv "ttag")
+           [(leafTag,leafCase),
+            (nodeTag,nodeCase)]
+           Nothing
+    where leafCase =
+              LetPrimCallT [("tout2",CursorTy)] WriteTag [TagTriv leafTag, VarTriv "tout"]
+            $ LetPrimCallT [("n",IntTy),("t3",CursorTy)] ReadInt [VarTriv "t2"]
+            $ LetPrimCallT [("tout3",CursorTy)] WriteInt [VarTriv "n", VarTriv "tout2"]
+            $ RetValsT [VarTriv "t3", VarTriv "tout3"]
+          nodeCase =
+              LetPrimCallT [("tout2",CursorTy)] WriteTag [TagTriv nodeTag, VarTriv "tout"]
+            $ LetCallT [("t3",CursorTy),("tout3",CursorTy)] "add1" [VarTriv "t2", VarTriv "tout2"]
+            $ TailCall "add1" [VarTriv "t3", VarTriv "tout3"]
