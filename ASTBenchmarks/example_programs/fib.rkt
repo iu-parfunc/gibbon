62c70ea517804ba751cce1fb7c0d68a876b2145d
Ryan Newton <rrnewton@gmail.com>
2016-10-30 13:39:39 -0400

    Correct mistakes in simplified/cleaned fib.rkt

--- ASTBenchmarks/example_programs/fib.rkt
+++ ASTBenchmarks/example_programs/fib.rkt
@@ -4,12 +4,12 @@
  (
  (DefineValues (fib)
    (Lambda (F1 (n))
-           (If (App ((VARREF <=) (VARREF n) (Quote 2)))
-               (Quote 1)
+           (If (App ((VARREF <=) (VARREF n) (Quote (INTLIT 2))))
+               (Quote (INTLIT 1))
                (App ( (VARREF +)
                       (App ((VARREF fib)
-                            (App ((VARREF -) (VARREF n) (Quote 1)))))
+                            (App ((VARREF -) (VARREF n) (Quote (INTLIT 1))))))
                       (App ((VARREF fib)
-                            (App ((VARREF -) (VARREF n) (Quote 2))))))))))
+                            (App ((VARREF -) (VARREF n) (Quote (INTLIT 2)))))))))))
  ))
 
