32756f367bba1bf3af304c407f0456a99d29cba1
Ryan Newton <rrnewton@gmail.com>
2016-11-01 23:40:08 -0400

    Modify rewrite.rkt to strip symbols down to something basic that will make any sexp parser happy.

--- ASTBenchmarks/rewrite.rkt
+++ ASTBenchmarks/rewrite.rkt
@@ -7,6 +7,36 @@
       `(INTLIT ,e)
       '(INTLIT 5)))
 
+(define (sym s)
+  (define str (list->string (filter legit-char? (string->list (symbol->string s)))))
+  (define str2
+    (cond
+      [(zero? (string-length str))
+       "symbol-wiped-out-by-character-filtration"]
+      [(not (ascii-alphabetic? (string-ref str 0)))
+       (string-append "sym_" str)]
+      [else str]))
+  (string->symbol str2))
+
+(define (legit-char? c)
+  (or (ascii-alphabetic? c)
+      (ascii-digit? c)
+      (eq? c #\-)
+      (eq? c #\_)
+      ))
+
+(define (ascii-alphabetic? c)
+  (define n (char->integer c))
+  (or (<= 65 n 90)  ;; A-Z
+      (<= 97 n 122) ;; a-z
+      ))
+
+(define (ascii-digit? c)
+  (define n (char->integer c))
+  (<= 48 n 57))
+
+  
+
 (define (xform-expression e)
   (define xf xform-expression)
   #;
@@ -17,25 +47,25 @@
 
   (match e
     [`(if ,e1 ,e2 ,e3)          `(If ,(xf e1) ,(xf e2) ,(xf e3))]
-    [`(set! ,x ,e)              `(SetBang ,x ,(xf e))]
-    [`(quote ,e)                `(Quote ,(datum e))]
-    [`(quote-syntax ,e)         `(QuoteSyntax ,(datum e))]
+    [`(set! ,x ,e)              `(SetBang ,(sym x) ,(xf e))]
+    [`(quote ,e)                `(Quote            ,(datum e))]
+    [`(quote-syntax ,e)         `(QuoteSyntax      ,(datum e))]
     [`(quote-syntax ,e #:local) `(QuoteSyntaxLocal ,(datum e))]
     [`(module ,m ,lang ,e ...)  `(Begin ,(map xf e))]
     [`(module* ,m ,lang ,e ...) `(Begin ,(map xf e))]
-    [`(#%top . ,s) #:when (symbol? s) `(Top ,s)] ;; RRN: fixed
+    [`(#%top . ,s) #:when (symbol? s) `(Top ,(sym s))] ;; RRN: fixed
     [`(begin ,e ...)            `(Begin ,(map xf e))]
     [`(begin0 ,e0 ,e ...)           `(Begin0 ,(xf e0) ,(map xf e))]
     [`(#%variable-reference)    `(VariableReferenceNull)]
-    [`(#%variable-reference (#%top . ,i)) `(VariableReferenceTop ,i)]
-    [`(#%variable-reference ,i) `(VariableReference ,i)]
+    [`(#%variable-reference (#%top . ,i)) #:when (symbol? i) `(VariableReferenceTop ,(sym i))]
+    [`(#%variable-reference ,i)           #:when (symbol? i) `(VariableReference ,(sym i))]
     [`(#%app ,e ...)            `(App ,(map xf e))]
     [`(#%plain-app ,e ...)      `(App ,(map xf e))]
     [`(let-values (,binds ...) ,body ...)
      `(LetValues ,(lvbind binds) ,(map xf body))]
     [`(letrec-values (,binds ...) ,body ...)
      `(LetrecValues ,(lvbind binds) ,(map xf body))] ;; RRN: fixed.
-    [(? symbol?) `(VARREF ,e)]
+    [(? symbol? s) `(VARREF ,(sym s))]
     [`(lambda ,fmls ,body ...)
      `(Lambda ,(xform-fmls fmls) ,(map xf body))]
     [`(#%plain-lambda ,fmls ,body ...)
@@ -47,22 +77,22 @@
                                     ,(map xf (rest c)))))]
     [`(with-continuation-mark ,e1 ,e2 ,e3)
      `(WithContinuationMark ,(xf e1) ,(xf e2) ,(xf e3))]
-    [(? symbol? s) `(VARREF ,s)]
+    ; [(? symbol? s) `(VARREF ,s)]
     ))
 
 (define (lvbind l)
   (for/list ([c (in-list l)])
     (match c
       [`((,x ...) ,e) #:when (andmap symbol? x)
-       `(MKLVBIND ,x ,(xform-expression e))])))
+       `(MKLVBIND ,(map sym x) ,(xform-expression e))])))
 
 (define (xform-fmls a)
   (match a
-    [`(,fmls ...) `(F1 ,fmls)]
-    [`(,fmls ... . ,(? symbol? x)) 
-     `(F2 ,fmls ,x)]
+    [`(,fmls ...) `(F1 ,(map sym fmls))]
+    [`(,fmls ... . ,(? symbol? x))
+     `(F2 ,(map sym fmls) ,(sym x))]
     [(? symbol? x)
-     `(F3 ,x)]))
+     `(F3 ,(sym x))]))
 
 (define (xform-top e)
   (match e
@@ -81,13 +111,14 @@
     [`(#%plain-module-begin ,e ...)
      `(BeginTop ,(map xform-top e))]
     [`(define-values (,x ...) ,e)
-     `(DefineValues ,x ,(xform-expression e))]
+     `(DefineValues ,(map sym x) ,(xform-expression e))]
     [`(define-syntaxes (,x ...) ,e)
-     `(DefineSyntaxes ,x ,(xform-expression e))]
+     `(DefineSyntaxes ,(map sym x) ,(xform-expression e))]
     [e `(Expression ,(xform-expression e))]))
 
 ;; ----------------------------------------
 
+;; Very hacky for reading paths:
 (define (to-sexps s)
   (with-input-from-string s
     (lambda ()
@@ -100,6 +131,7 @@
 ;; ----------------------------------------
 
 ;; expects "<infile> <outfile>" lines on stdin
+;; runs until it gets no more such lines (EOF)
 (module+ main
   (let ([errors 0])
     (let loop ()
