7450aa70ed3fd1244ae8188552c5b283e792988f
Ryan Newton <rrnewton@gmail.com>
2016-11-04 19:51:45 -0400

    Add makefile target and script to do the binary packing

---
+++ ASTBenchmarks/binary_pack.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(require racket/exn)
+; (require "grammar_racket.sexp")
+(require "./common/racket/parse.rkt")
+
+(provide (all-defined-out))
+
+;; ----------------------------------------
+
+               
+;; expects "<infile> <outfile>" lines on stdin
+;; runs until it gets no more such lines (EOF)
+(module+ main
+  (let ([errors 0])
+    (let loop ()
+      (define l (read-line))
+      (if (eof-object? l)
+          (printf "binary_pack.rkt: Reached EOF; done.\n")
+          (match (string-split l)
+            [(list infile outfile)
+             (printf "Converting to binary: ~a ~a\n" infile outfile)
+             (with-handlers ([(lambda (_) #t)
+                              (lambda (e)
+                                (printf "ERROR while converting:\n  ~a"
+                                        (exn->string e))
+                                (set! errors (add1 errors)))])
+               (call-with-output-file outfile
+                 (lambda (outp)                  
+                   (parse-pack-write (file->value infile) outp))
+                 #:mode 'binary))
+             (loop)]
+            )))
+    (if (zero? errors)
+        (printf "Completed all conversions without error.\n")
+        (begin (printf "Encountered ~a errors while converting.  Failing job.\n" errors)
+               (exit 1)))))
