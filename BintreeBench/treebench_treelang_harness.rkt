999813481f20c9787080952d975926e2c861f1ff
Sarah Jane Spall <sjspall@tank.soic.indiana.edu>
2016-11-06 22:15:52 -0500

    fixed timing code

--- BintreeBench/treebench_treelang_harness.rkt
+++ BintreeBench/treebench_treelang_harness.rkt
@@ -1,21 +1,30 @@
 #lang typed/racket/base
 
-(require racket/system)
+(require racket/system
+	 "treebench_treelang.rkt")
 ;; Infrastructure to set up and run the benchmark.
+
 
 ;;
 (define ARGS 1) ;; max 25
 (define ARGSMAX 25)
 ;; double iters until batchtime is over 1 sec
-(define target-time 1.0) 
+(define target-time 1.0)
+
+(define (time-add1 [size : Fixnum] [iters : Fixnum])
+  (let ([tr0 : Tree (build-tree size)])
+    (time-apply (lambda ()
+    		  (for/list : (Listof Tree) ([_ (in-range iters)])
+		    (add1-tree tr0)))
+                '()))) 
 
 (define (func)
   (for ([args (in-range 1 26)])
-    (let loop ([iters 1])
+    (let loop ([iters : Fixnum 1])
       (collect-garbage)
       (define-values (_ cpu real gc)
-        (time-apply (lambda () (system (format "../../racket/bin/racket ./treebench_treelang.rkt ~v ~v" args iters)))
-		    '()))
+        (time-add1 args iters))
+		
       (define batchseconds (/ real 1000.0))
       (if (>= batchseconds target-time)
       	  (let ([meantime (exact->inexact (/ batchseconds iters))])
@@ -24,6 +33,6 @@
             (printf "BATCHTIME: ~a\n" (exact->inexact batchseconds))
             (printf "MEANTIME: ~a\n" meantime)
             (printf "Done with pass,.\n" ))
-	  (loop (* 2 iters))))))
+	  (loop (cast (* 2 iters) Fixnum))))))
 
 (func)
