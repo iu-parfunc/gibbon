904e1a01b3b59935b68a986a4733cbcc4d08aa40
Michael Vollmer <vollmerm@indiana.edu>
2016-11-08 11:23:34 -0500

    work on findWitnesses

--- TreeLang/src/Packed/FirstOrder/Passes/FindWitnesses.hs
+++ TreeLang/src/Packed/FirstOrder/Passes/FindWitnesses.hs
@@ -1,13 +1,14 @@
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 -- | 
 
-module Packed.FirstOrder.Passes.FindWitnesses 
-    ( findWitnesses) where
+module Packed.FirstOrder.Passes.FindWitnesses (findWitnesses) where
 
-import           Packed.FirstOrder.Common hiding (FunDef)
-import           Packed.FirstOrder.LTraverse as L2
+import Packed.FirstOrder.Common hiding (FunDef)
+import Packed.FirstOrder.LTraverse as L2
+import qualified Packed.FirstOrder.L1_Source as L1
 import Data.List as L hiding (tail)
-
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 -- | This pass must find witnesses if they exist in the lexical
 -- environment, and it must *reorder* let bindings to bring start/end
@@ -15,20 +16,35 @@ import Data.List as L hiding (tail)
 findWitnesses :: L2.Prog -> SyM L2.Prog
 findWitnesses = L2.mapMExprs fn
  where
-  fn _ ex = return (go ex)
-  go ex =
+  fn _ ex = return (go Map.empty ex)
+  go mp ex =
     case ex of 
-      VarE v   -> VarE v
+      VarE v   -> buildLets mp [v] $ VarE v
       LitE n   -> LitE n
-      AppE v e -> AppE v (go e)
-      PrimAppE p ls      -> PrimAppE p (L.map go ls)
-      LetE (v,t,rhs) bod -> LetE (v,t,go rhs) (go bod)
-      ProjE i e      -> ProjE i (go e)
-      CaseE e ls     -> CaseE (go e) [ (k,vs,go e) | (k,vs,e) <- ls ]
-      MkProdE ls     -> MkProdE (L.map go ls)
-      MkPackedE k ls -> MkPackedE k (L.map go ls)
-      TimeIt e t     -> TimeIt (go e) t
-      IfE a b c      -> IfE (go a) (go b) (go c)
-      MapE (v,t,rhs) bod -> MapE (v,t,go rhs) (go bod)
-      FoldE (v1,t1,r1) (v2,t2,r2) bod -> FoldE (v1,t1,go r1) (v2,t2,go r2) (go bod)
+      AppE v e -> handle mp $ AppE v (go Map.empty e)
+      PrimAppE p ls      -> handle mp $ PrimAppE p (map (go Map.empty) ls)
+      LetE (v,t,rhs) bod -> go (Map.insert v (v,t,rhs) mp) bod
+      ProjE i e      -> handle mp $ ProjE i (go Map.empty e)
+      CaseE e ls     -> handle mp $ CaseE e [ (k,vs,go Map.empty e) | (k,vs,e) <- ls ] 
+      MkProdE ls     -> handle mp $ MkProdE (map (go Map.empty) ls)
+      MkPackedE k ls -> handle mp $ MkPackedE k (map (go Map.empty) ls)
+      TimeIt e t     -> TimeIt (go mp e) t
+      IfE a b c      -> handle mp $ IfE a (go Map.empty b) (go Map.empty c)
+      MapE (v,t,rhs) bod -> handle mp $ MapE (v,t,rhs) (go Map.empty bod)
+      FoldE (v1,t1,r1) (v2,t2,r2) bod -> handle mp $ FoldE (v1,t1,r1) (v2,t2,r2) (go Map.empty bod)
 
+  buildLets _mp [] bod = bod
+  buildLets mp (v:vs) bod =
+      case Map.lookup v mp of
+        Nothing -> buildLets mp vs bod
+        Just bnd -> LetE bnd $ buildLets mp vs bod
+
+  handle mp exp = buildLets mp (vars fvs []) exp
+      where fvs = Set.toList $ L1.freeVars exp
+            freeInBind v = case Map.lookup v mp of
+                             Nothing -> [] -- error $ "error in findWitnesses, binding for " ++ v ++ " not found."
+                             Just (_v,_t,e) -> Set.toList $ L1.freeVars e
+            vars [] found = found
+            vars (w:ws) found =
+                let nw = freeInBind w
+                in vars (((nw \\ ws) \\ found) ++ ws) (w:found)
