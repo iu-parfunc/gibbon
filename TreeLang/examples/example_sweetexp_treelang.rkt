a58e267f81fe3b0eb769cb50046db815e6cc53ab
Ryan Newton <rrnewton@gmail.com>
2016-11-09 05:04:18 -0500

    Oops, fix recent makefile mistake

---
+++ TreeLang/examples/example_sweetexp_treelang.rkt
@@ -0,0 +1,25 @@
+#lang sweet-exp "./TreeLang/treelang.rkt"
+
+(require (only-in racket bytes-length))
+; define x 3
+; printf("~a\n" x)
+
+
+data Tree
+  Leaf Int
+  Node Tree Tree
+
+let ([x : Tree  Leaf(3)])
+  case x
+    Leaf(n)   99
+    Node(x y) 100
+
+ ; bytes-length(pack-Tree(x))
+
+
+;; (data Tree
+;;   [Leaf Int]
+;;   [Node Tree Tree])
+
+;; (let ([x : Tree (Leaf 3)])
+;;   (bytes-length (pack-Tree x)))
