d2fdc366938ce3892f2b68146a4bc53abc342ca1
Ryan Newton <rrnewton@gmail.com>
2016-11-11 12:48:13 -0500

    Add (duplicated, repetitive) benchmark runner for sumtree

---
+++ BintreeBench/benchmark_sumtree.rkt
@@ -0,0 +1,13 @@
+#lang typed/racket/base
+
+;; WARNING: Code duplication with benchmark_buildtree.rkt
+
+;; Run make sumtree before this
+
+(require "command_line_runner.rkt")
+
+(define PASSNAME "sumtree")
+
+(launch-benchmarks "./sumtree_treelang_c_packed.exe -benchmark " PASSNAME "treelang-c-packed")
+
+(launch-benchmarks "racket sumtree_treelang.sexp " PASSNAME "treelang-racket")
