7f2eeae4a117aca800a6f8f749f3ffa4084ec50f
Sam Tobin-Hochstadt <samth@ccs.neu.edu>
2017-01-12 19:12:18 -0500

    Add a combiner.

---
+++ ASTBenchmarks/combine.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(module+ main
+  (require racket/cmdline)
+  (define outf #f)
+  (define args (command-line
+                #:once-any
+                ["--out" out "output-file"
+                         (unless (equal? out "-")
+                           (set! outf out))]
+                #:args args args))
+  (combine-all args outf))
+
+(define (combine-all fs out)
+  (define ls (map file->value fs))
+  (write
+   `(BeginTop
+     ,(for/fold ([v '(NULLTOPLVL)]) ([l ls])
+        `(CONSTOPLVL l v)))
+   (if out (open-output-file out) (current-output-port))))
