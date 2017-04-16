a77b95bf9290704a570fcce6e29cd67c19630d47
cskksc <ckoparkar@live.in>
2017-04-16 12:00:01 -0400

    [Fix #42] relative path handling for require

--- gibbon-compiler/src/Packed/FirstOrder/SExpFrontend.hs
+++ gibbon-compiler/src/Packed/FirstOrder/SExpFrontend.hs
@@ -11,7 +11,7 @@
 
 -- |  Parse an SExp representaton of our tree-walk language.
 
-module Packed.FirstOrder.SExpFrontend 
+module Packed.FirstOrder.SExpFrontend
        (parseFile, parseSExp, primMap, main) where
 
 import Data.Char
@@ -20,12 +20,13 @@ import Data.List as L
 import Data.Set as S
 import Data.Map as M
 import Data.Text.IO (readFile)
+import System.FilePath
 import System.Environment
-import Text.Parsec 
+import Text.Parsec
 -- import GHC.Generics (Generic)
 import Text.PrettyPrint.GenericPretty
 import Packed.FirstOrder.L1_Source as S
-import Packed.FirstOrder.Common 
+import Packed.FirstOrder.Common
 import Prelude hiding (readFile, exp)
 
 -- There are several options for s-expression parsing, including these
@@ -33,15 +34,20 @@ import Prelude hiding (readFile, exp)
 --  * sexp
 --  * sexpr
 --  * sexp-grammar
---  * atto-lisp 
+--  * atto-lisp
 --  * lispparser
--- Using 's-cargo' for the first attempt:    
+-- Using 's-cargo' for the first attempt:
 import Data.SCargot.Language.HaskLike
 import Data.SCargot.Parse
-import Data.SCargot.Print 
+import Data.SCargot.Print
 import Data.SCargot.Repr -- (SExpr, RichSExpr, toRich)
 
 --------------------------------------------------------------------------------
+
+-- | Baseline chatter level for this module:
+lvl :: Int
+lvl = 5
+
 
 deriving instance Generic a => Generic (SExpr a)
 deriving instance Generic a => Generic (RichSExpr a)
@@ -73,14 +79,14 @@ treelangParser =
         eatline = manyTill anyChar newline *> pure ()
     in
     -- setCarrier (return . asRich) $
-    setComment (comment <|> langline) $ 
+    setComment (comment <|> langline) $
     haskLikeParser
 
 -- Hack:
 _stripHashLang :: Text -> Text
 _stripHashLang txt =
   if T.isPrefixOf "#lang" txt
-  then snd $ T.break (== '\n') txt 
+  then snd $ T.break (== '\n') txt
        -- (\c -> generalCategory c == LineSeparator)
   else txt
 
@@ -89,19 +95,19 @@ bracketHacks = T.map $ \case '[' -> '('
                              ']' -> ')'
                              x   -> x
 
--- | Change regular applications into data constructor syntax.             
+-- | Change regular applications into data constructor syntax.
 tagDataCons :: DDefs Ty -> Exp -> Exp
 tagDataCons ddefs = go allCons
-  where 
+  where
    allCons = S.fromList [ (toVar con)
                         | DDef{dataCons} <- M.elems ddefs
                         , (con,_tys) <- dataCons ]
-   go cons ex = 
+   go cons ex =
      case ex of
        AppE v (MkProdE ls) | S.member v cons -> MkPackedE (fromVar v) (L.map (go cons) ls)
        AppE v e | S.member v cons -> MkPackedE (fromVar v) [go cons e]
                 | otherwise       -> AppE      v (go cons e)
-       LetE (v,t,rhs) bod -> 
+       LetE (v,t,rhs) bod ->
          let go' = if S.member v cons
                       then go (S.delete v cons)
                       else go cons
@@ -116,13 +122,13 @@ tagDataCons ddefs = go allCons
        MkProdE ls     -> MkProdE $ L.map (go cons) ls
        MkPackedE k ls -> MkPackedE k $ L.map (go cons) ls
        TimeIt e t b -> TimeIt (go cons e) t b
-       IfE a b c -> IfE (go cons a) (go cons b) (go cons c)   
+       IfE a b c -> IfE (go cons a) (go cons b) (go cons c)
 
        MapE  (v,t,e) bod -> MapE (v,t, go cons e) (go cons bod)
        FoldE (v1,t1,e1) (v2,t2,e2) b -> FoldE (v1,t1,go cons e1) (v2,t2,go cons e2) (go cons b)
-                    
+
 parseSExp :: [Sexp] -> SyM Prog
-parseSExp ses = 
+parseSExp ses =
   do prog@Prog {ddefs} <- go ses [] [] [] Nothing
      return $ mapExprs (tagDataCons ddefs) prog
  where
@@ -130,14 +136,14 @@ parseSExp ses =
    -- WARNING: top-level constant definitions are INLINED everywhere.
    inlineConstDefs [] p = p
    inlineConstDefs ((vr,ty,rhs) : cds) p =
-       inlineConstDefs cds $ 
+       inlineConstDefs cds $
         mapExprs (subst vr rhs) p
-   
+
    -- Processes an sexpression while accumulating data, function, and constant defs.
-   go xs dds fds cds mn = 
+   go xs dds fds cds mn =
     case xs of
      [] -> return $
-           inlineConstDefs cds $ 
+           inlineConstDefs cds $
            Prog (fromListDD dds) (fromListFD fds) mn
 
      -- IGNORED!:
@@ -179,10 +185,10 @@ parseSExp ses =
      (L (A "define" : _) : _) -> error$ "Badly formed function:\n  "++prnt (head xs)
 
      (L (A "data" : _) : _) -> error$ "Badly formed data definition:\n  "++prnt (head xs)
-                                 
+
      (L3 "module+" _ bod : rst) -> go (bod:rst) dds fds cds mn
 
-     (ex : rst) -> 
+     (ex : rst) ->
        let ex' = exp ex
        in go rst dds fds cds (case mn of
                             Nothing -> Just ex'
@@ -190,13 +196,13 @@ parseSExp ses =
                                              sdoc x++"\nAnd:\n"++prnt ex)
 
 tuplizeRefs :: Var -> [Var] -> Exp -> Exp
-tuplizeRefs tmp ls  = go (L.zip [0..] ls) 
-  where 
+tuplizeRefs tmp ls  = go (L.zip [0..] ls)
+  where
    go []          e = e
    go ((ix,v):vs) e = go vs (subst v (ProjE ix (VarE tmp)) e)
 
 typ :: RichSExpr HaskLikeAtom -> Ty
-typ s = case s of          
+typ s = case s of
          (A "Int")  -> IntTy
          (A "Sym")  -> SymTy
          (A "Bool") -> BoolTy
@@ -211,15 +217,15 @@ getSym (RSAtom (HSIdent id)) = textToVar id
 getSym s = error $ "expected identifier sexpr, got: "++prnt s
 
 docasety :: Sexp -> (DataCon,[Ty])
-docasety s = 
+docasety s =
   case s of
     (RSList ((A id) : tys)) -> (textToDataCon id, L.map typ tys)
     _ -> error$ "Badly formed variant of datatype:\n "++prnt s
 
 pattern A s = RSAtom (HSIdent s)
 
-pattern L  a         = RSList a 
-pattern L1 a         = RSList [a]   
+pattern L  a         = RSList a
+pattern L1 a         = RSList [a]
 pattern L2 a b       = RSList [A a, b]
 pattern L3 a b c     = RSList [A a, b, c]
 pattern L4 a b c d   = RSList [A a, b, c, d]
@@ -233,62 +239,62 @@ falseE = PrimAppE MkFalse []
 
 -- FIXME: we cannot intern strings until runtime.
 hackySymbol :: String -> Int
-hackySymbol s = product (L.map ord s)  
+hackySymbol s = product (L.map ord s)
 
 keywords :: S.Set Text
-keywords = S.fromList $ L.map pack $ 
+keywords = S.fromList $ L.map pack $
            [ "quote", "if", "or", "and", "time", "let"
            , "case", "vector-ref", "for/fold", "for/list"
            , "insert", "empty-dict", "lookup", "error", "ann"
            ]
 
-isKeyword :: Text -> Bool       
+isKeyword :: Text -> Bool
 isKeyword s = s `S.member` keywords
-                
+
 exp :: Sexp -> Exp
 exp se =
- -- trace ("\n ==> Processing Exp:\n  "++prnt se)  $ 
+ -- trace ("\n ==> Processing Exp:\n  "++prnt se)  $
  case se of
    A "True"          -> trueE
    A "False"         -> falseE
    L ("and" : args)  -> go args
      where go [] = trueE
-           go (x:xs) = IfE (exp x) (go xs) falseE       
+           go (x:xs) = IfE (exp x) (go xs) falseE
    L ("or" : args)  -> go args
      where go [] = falseE
            go (x:xs) = IfE (exp x) trueE (go xs)
 
-   L4 "if" test conseq altern -> 
+   L4 "if" test conseq altern ->
      IfE (exp test) (exp conseq) (exp altern)
 
    L2 "quote" (A v) -> LitSymE (textToVar v)
-     
+
    -- Any other naked symbol is a variable:
    A v -> VarE (textToVar v)
    RSAtom (HSInt n)  -> LitE (fromIntegral n)
-                        
+
    -- | This type gets replaced later in flatten:
    L2 "time" arg -> (TimeIt (exp arg) (PackedTy "DUMMY_TY" ()) False)
 
    -- | This variant inserts a loop, controlled by the iters argument on the command line.
    L2 "iterate" arg -> (TimeIt (exp arg) (PackedTy "DUMMY_TY" ()) True)
-                    
-   L3 "let" (L bnds) bod -> 
-     mkLets (L.map letbind bnds) (exp bod) 
-     
-   L (A "case": scrut: cases) -> 
+
+   L3 "let" (L bnds) bod ->
+     mkLets (L.map letbind bnds) (exp bod)
+
+   L (A "case": scrut: cases) ->
      CaseE (exp scrut) (L.map docase cases)
 
    L (A p : ls) | isPrim p -> PrimAppE (prim p) $ L.map exp ls
 
-   L3 "for/list" (L1 (L4 v ":" t e)) bod ->     
+   L3 "for/list" (L1 (L4 v ":" t e)) bod ->
      S.MapE (textToVar v, typ t, exp e) (exp bod)
 
    -- I don't see why we need the extra type annotation:
    L4 "for/fold"
           (L1 (L4 v1 ":" t1 e1))
           (L1 (L4 v2 ":" t2 e2))
-          bod -> 
+          bod ->
      S.FoldE (textToVar v1, typ t1, exp e1)
              (textToVar v2, typ t2, exp e2)
              (exp bod)
@@ -312,7 +318,7 @@ exp se =
      PrimAppE (DictHasKeyP $ typ ty) [(exp d),(exp k)]
 
    -- L [A "error",arg] ->
-   L3 "ann" (L2 "error" arg) ty -> 
+   L3 "ann" (L2 "error" arg) ty ->
       case arg of
         RSAtom (HSString str) -> PrimAppE (ErrorP (T.unpack str) (typ ty)) []
         _ -> error$ "bad argument to 'error' primitive: "++prnt arg
@@ -322,23 +328,23 @@ exp se =
 
    L (A kwd : args) | isKeyword kwd ->
       error $ "Error reading treelang.  Badly formed expression:\n "++prnt se
-                     
-   ----------------------------------------                                                
-   -- If NOTHING else matches, we are an application.  Be careful we didn't miss anything:             
-   L (A rator : rands) -> 
+
+   ----------------------------------------
+   -- If NOTHING else matches, we are an application.  Be careful we didn't miss anything:
+   L (A rator : rands) ->
      let app = AppE (textToVar rator)
-     in case rands of   
+     in case rands of
          [] -> app (MkProdE [])
          [rand] -> app (exp rand)
          _ -> app (MkProdE (L.map exp rands))
 
-   _ -> error $ "Expression form not handled (yet):\n  "++ 
+   _ -> error $ "Expression form not handled (yet):\n  "++
                sdoc se ++ "\nMore concisely:\n  "++ prnt se
 
 
 -- | One case of a case expression
 docase :: Sexp -> (DataCon,[Var],Exp)
-docase s = 
+docase s =
   case s of
     RSList [ RSList (A con : args)
            , rhs ]
@@ -347,7 +353,7 @@ docase s =
 
 
 letbind :: Sexp -> (Var,Ty,Exp)
-letbind s = 
+letbind s =
   case s of
    RSList [A vr, A ":",
            ty, rhs]
@@ -378,24 +384,24 @@ main = do
   _ <- parseFile file
   return ()
 
-handleRequire :: [Sexp] -> IO [Sexp]
-handleRequire [] = return []
-handleRequire ((L2 "require" arg):ls) = do
-  ls' <- handleRequire ls
+handleRequire :: FilePath -> [Sexp] -> IO [Sexp]
+handleRequire _ [] = return []
+handleRequire baseFile ((L2 "require" arg):ls) = do
+  ls' <- handleRequire baseFile ls
   let file = case arg of
-               RSAtom (HSString str) -> unpack str
+               RSAtom (HSString str) -> (takeDirectory baseFile) </> (unpack str)
                _ -> error $ "bad require line: " ++ (show arg)
-  dbgPrintLn 5 $ "Including required file: "++show file
+  dbgPrintLn lvl $ "Including required file: "++show file
   txt <- fmap bracketHacks $ readFile file
-  -- dbgPrintLn 5 $ "Parsing required text: "++show txt
+  dbgPrintLn lvl $ "Parsing required text: "++show txt
   let res :: Either String [RichSExpr HaskLikeAtom]
       res = fmap (fmap toRich) $
             decode treelangParser txt
   case res of
     Left err -> error err
     Right l' -> return $ l' ++ ls'
-handleRequire (l:ls) = do
-  ls' <- handleRequire ls
+handleRequire baseFile (l:ls) = do
+  ls' <- handleRequire baseFile ls
   return $ l:ls'
 
 parseFile :: FilePath -> IO (Prog, Int)
@@ -403,12 +409,12 @@ parseFile file = do
   txt    <- fmap bracketHacks $
             -- fmap stripHashLang $
             readFile file
-  dbgPrintLn 5 $ "Parsing text: "++show txt
+  dbgPrintLn lvl $ "Parsing text: "++show txt
   let res :: Either String [RichSExpr HaskLikeAtom]
       res = fmap (fmap toRich) $
             decode treelangParser txt
-  dbgPrintLn 5 "Result of parsing:"
+  dbgPrintLn lvl "Result of parsing:"
   case res of
      Left err -> error err
-     Right ls -> do ls' <- handleRequire ls
+     Right ls -> do ls' <- handleRequire file ls
                     return $ runSyM 0 $ parseSExp ls'
