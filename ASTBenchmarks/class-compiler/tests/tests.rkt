33fee9d58a54c2fa7abdbfc9c35bc988b6eacd19
cskksc <ckoparkar@live.in>
2017-08-25 14:54:40 -0400

    Port uniqify to sym-append, and run tests

---
+++ ASTBenchmarks/class-compiler/tests/tests.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require "test-uniqify.rkt")
+(require rackunit)
+
+;; TODO: write a parser for R0 so that we can re-use all tests from the class
+
+(define tests
+  (list uniqify-tests))
+
+(define (run-tests)
+  (let* ([test-results (flatten (map run-test tests))]
+         [failures (filter (compose not test-success?) test-results)]
+         [passed (filter test-success? test-results)])
+    (if (empty? failures)
+        (printf "All ~a tests passed.\n" (length passed))
+        (printf "~s tests failed.\n~a"
+                (length failures)
+                (string-join (map test-result-test-case-name failures) "\n")))))
+
+(run-tests)
