f48339fbaff166d240174de9de22a71f0447b0a2
ckoparkar <ckoparkar@gmail.com>
2017-09-30 11:57:15 -0400

    Port ShakeTree and HoistNewBuf

--- gibbon-compiler/src/Packed/FirstOrder/Passes/ShakeTree.hs
+++ gibbon-compiler/src/Packed/FirstOrder/Passes/ShakeTree.hs
@@ -6,16 +6,20 @@
 module Packed.FirstOrder.Passes.ShakeTree
     (shakeTree) where
 
-import qualified Data.Map as M
-import Packed.FirstOrder.Common (SyM, dbgTrace)
-import qualified Packed.FirstOrder.L1.Syntax as L1
-import Packed.FirstOrder.L2.Syntax as L2
+import Data.Loc
 import Prelude hiding (exp)
+import qualified Data.Map as M
 import qualified Data.Set as S
 
+import Packed.FirstOrder.GenericOps
+import Packed.FirstOrder.Common (SyM, dbgTrace)
+import Packed.FirstOrder.L1.Syntax hiding (FunDef, Prog(..))
+import Packed.FirstOrder.L3.Syntax
+
+
 -- | Drop all unreferenced let-bindings.
-shakeTree :: L2.Prog -> SyM L2.Prog
-shakeTree prg@L2.Prog{fundefs,mainExp} = return $
+shakeTree :: Prog -> SyM Prog
+shakeTree prg@Prog{fundefs,mainExp} = return $
   prg { fundefs = M.map fd fundefs
       , mainExp = case mainExp of
                     Nothing      -> Nothing
@@ -24,43 +28,51 @@ shakeTree prg@L2.Prog{fundefs,mainExp} = return $
  where
    fd f@FunDef{funbod} = f { funbod = shakeTreeExp funbod }
 
-shakeTreeExp :: L1.Exp -> L1.Exp
+shakeTreeExp :: L Exp3 -> L Exp3
 shakeTreeExp = go
   where
 
-  go :: L1.Exp -> L1.Exp
-  go e0 =
+  go :: L Exp3 -> L Exp3
+  go (L p e0) = L p $
    -- dbgTrace 5 ("Inline, processing with env:\n "++sdoc env++"\n exp: "++sdoc e0) $
    case e0 of
 
-    (LetE (v,t,rhs) bod) ->
+    (LetE (v,locs,t,rhs) bod) ->
         let bod' = go bod
-            fv   = L1.freeVars bod'
+            fv   = gFreeVars bod'
         in
         if S.member v fv || hasEffect rhs
-        then LetE (v,t, go rhs) bod'
-        else dbgTrace 4 (" [shakeTreeExp] dropping binding: "++show (v,t,rhs))$
-             bod'
+        then LetE (v,locs,t, go rhs) bod'
+        else dbgTrace 4 (" [shakeTreeExp] dropping binding: "++show (v,t,rhs))$ unLoc bod'
 
     (VarE v)           -> VarE v
     (LitE i)           -> LitE i
     (LitSymE v)        -> LitSymE v
-    (AppE f e)         -> AppE f $ go e
-    (PrimAppE p es)    -> PrimAppE p $ map (go) es
+    (AppE f locs e)    -> AppE f locs $ go e
+    (PrimAppE pr es)   -> PrimAppE pr $ map (go) es
     (IfE e1 e2 e3)     -> IfE (go e1) (go e2) (go e3)
+
     (ProjE i e)  -> ProjE i $ go e
     (MkProdE es) -> MkProdE $ map (go) es
+
     -- We don't rename field binders with to/from witness:
     (CaseE e mp) -> let mp' = map dorhs mp
                         dorhs (c,args,ae) =
                             (c,args,go ae)
                     in CaseE (go e) mp'
-    (DataConE c es) -> DataConE c $ map (go) es
-    (TimeIt e t b) -> TimeIt (go e) t b
-    (MapE (v,t,e') e) -> MapE (v,t,go e') (go e)
+
+    (DataConE c loc es) -> DataConE c loc $ map (go) es
+    (TimeIt e t b)     -> TimeIt (go e) t b
+
+    (MapE (v,t,e') e)   -> MapE (v,t,go e') (go e)
     (FoldE (v1,t1,e1) (v2,t2,e2) e3) ->
          FoldE (v1,t1,go e1) (v2,t2,go e2)
                (go e3)
+
+    -- Assume that these are trivial, and always have effects
+    Ext _ext -> e0
+
+
 
 
 -- | On cursors we have affine types rather than linear.  Thus, unfortunately, we don't
@@ -69,33 +81,38 @@ shakeTreeExp = go
 --
 -- This contains details that are specific to this pass, which is
 -- post-cursorize.  It's not really a good general definition of "hasEffect".
-hasEffect :: Exp -> Bool
-hasEffect rhs =
+hasEffect :: L Exp3 -> Bool
+hasEffect (L _ rhs) =
     -- Trivials have been inlined, but we're still flat-ish:
     case rhs of
-      DataConE _ _ -> True
-      WriteInt _ _  -> True
-      -- Reads are covered by dataflow...
-      -- AppE _ _
       VarE _ -> False
       LitE _ -> False
       LitSymE _ -> False
 
       -- These might have effects on output cursors, but the output cursors aren't used
-      -- again!  We need to tie the not in dataflow dependencies, making the start (value)
+      -- again!  We need to tie the knot in dataflow dependencies, making the start (value)
       -- depend on the end (final cursor).
-      AppE _ _ -> True  -- For now, don't drop.
+      AppE _ _ _ -> True  -- For now, don't drop.
+
       PrimAppE _ _ -> False -- No prims have effects.
+
+      LetE (_,_,_,e1) e2 -> hasEffect e1 || hasEffect e2
+
       ProjE _ e    -> hasEffect e      -- Flattening should make this equivalent to "False"
       MkProdE ls   -> any hasEffect ls -- Flattening should make this equivalent to "False"
 
       IfE a b c -> hasEffect a || hasEffect b || hasEffect c
+
       CaseE _ _ -> True -- Umm, just don't drop for now. FIXME/ REVISIT THIS!
+
+      DataConE _ _ _ -> True
 
       TimeIt{} -> True -- Yes, has effect of printing!
 
-      LetE (_,_,e1) e2 -> hasEffect e1 || hasEffect e2
       MapE _ _ -> error "hasEffect: FIXME MapE"
       FoldE _ _ _ -> error "hasEffect: FIXME FoldE"
+
+      -- always have effects
+      Ext _ -> True
 
       -- oth -> error $" [shakeTrees] unexpected RHS on Let:\n "++sdoc rhs
