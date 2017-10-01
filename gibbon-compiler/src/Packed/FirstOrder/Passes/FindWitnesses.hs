0dbabe9a737cbba666d2af90639cf940b98592e9
ckoparkar <ckoparkar@gmail.com>
2017-10-01 08:16:44 -0400

    port findWitnesses

--- gibbon-compiler/src/Packed/FirstOrder/Passes/FindWitnesses.hs
+++ gibbon-compiler/src/Packed/FirstOrder/Passes/FindWitnesses.hs
@@ -1,15 +1,23 @@
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
--- | 
 
-module Packed.FirstOrder.Passes.FindWitnesses (findWitnesses) where
+ -- |
 
-import Packed.FirstOrder.Common hiding (FunDef)
-import Packed.FirstOrder.L2.Syntax as L2
-import qualified Packed.FirstOrder.L1.Syntax as L1
--- import Data.List as L hiding (tail)
+module Packed.FirstOrder.Passes.FindWitnesses
+  (findWitnesses) where
+
+
+import Data.Loc
+import Data.Graph
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import Data.Graph
+-- import Data.List as L hiding (tail)
+
+import Packed.FirstOrder.GenericOps
+import Packed.FirstOrder.Common hiding (FunDef)
+import Packed.FirstOrder.L1.Syntax hiding (FunDef, FunDefs, Prog)
+import Packed.FirstOrder.L3.Syntax
+-- import Packed.FirstOrder.L2.Syntax as L2
+
 
 bigNumber :: Int
 bigNumber = 10 -- limit number of loops
@@ -19,9 +27,9 @@ bigNumber = 10 -- limit number of loops
 -- witnesses into scope.
 --
 -- Phase Ordering: This must run after flatten.
-findWitnesses :: L2.Prog -> SyM L2.Prog
-findWitnesses = L2.mapMExprs fn
- where   
+findWitnesses :: Prog -> SyM Prog
+findWitnesses = mapMExprs fn
+ where
   fn Env2{vEnv,fEnv} ex = return (goFix (Map.keysSet vEnv `Set.union` Map.keysSet fEnv)
                                         ex bigNumber)
   goFix _    ex 0 = error $ "timeout in findWitness on " ++ (show ex)
@@ -29,68 +37,72 @@ findWitnesses = L2.mapMExprs fn
                            ex2 = goE bound0 Map.empty ex1
                        in if ex1 == ex2 then ex2
                           else goFix bound0 ex2 (n - 1)
-  goE bound mp ex =
+  goE bound mp (L p ex) =
     let go      = goE bound -- Shorthand.
         goClear = goE (bound `Set.union` Map.keysSet mp) Map.empty
-    in 
-      case ex of 
-        LetE (v,t,TimeIt e ty b) bod -> 
-            handle mp $ LetE (v,t,TimeIt (go Map.empty e) ty b)
-                         (goE (Set.insert v (bound `Set.union` Map.keysSet mp)) Map.empty bod)
+        -- shorthand for applying (L p)
+        handle' e = handle mp $ L p e
+    in
+      case ex of
+        LetE (v,locs,t, L p2 (TimeIt e ty b)) bod ->
+            handle' $ LetE (v,locs,t, L p2 $ TimeIt (go Map.empty e) ty b)
+                      (goE (Set.insert v (bound `Set.union` Map.keysSet mp)) Map.empty bod)
 
-        LetE (v,t,rhs) bod
+        LetE (v,locs,t,rhs) bod
             -- | isWitnessVar v -> error$ " findWitnesses: internal error, did not expect to see BINDING of witness var: "++show v
-            | otherwise -> go (Map.insert v (v,t,rhs') mp) bod -- don't put the bod in the map
+            | otherwise -> go (Map.insert v (v,locs,t,rhs') mp) bod -- don't put the bod in the map
               where rhs' = go Map.empty rhs -- recur on rhs, but flatten makes these pretty boring.
 
-        VarE v         -> handle mp $ VarE v
-        LitE n         -> handle mp $ LitE n
-        LitSymE v      -> handle mp $ LitSymE v
-        AppE v e       -> handle mp $ AppE v (goClear e)
-        PrimAppE p ls  -> handle mp $ PrimAppE p (map goClear ls)
-        ProjE i e      -> handle mp $ ProjE i (goClear e)
+        VarE v         -> handle' $ VarE v
+        LitE n         -> handle' $ LitE n
+        LitSymE v      -> handle' $ LitSymE v
+        AppE v locs e  -> handle' $ AppE v locs (goClear e)
+        PrimAppE p ls  -> handle' $ PrimAppE p (map goClear ls)
+        ProjE i e      -> handle' $ ProjE i (goClear e)
 
         -- It's ok that we don't go deeper on scrutinees, subexpressions
         -- of tuples, and so on, because flatten should have done
         -- let-lifting out of these contexts:
-        MkProdE ls     -> handle mp $ MkProdE (map goClear ls)
-        DataConE k ls -> handle mp $ DataConE k (map goClear ls)
-        TimeIt e t b   -> handle mp $ TimeIt (goClear e) t b -- prevent pushing work into timeit
+        MkProdE ls     -> handle' $ MkProdE (map goClear ls)
+        DataConE loc k ls -> handle' $ DataConE loc k (map goClear ls)
+        TimeIt e t b   -> handle' $ TimeIt (goClear e) t b -- prevent pushing work into timeit
 
         -- FIXME: give CaseE a treatment like IfE:
-        CaseE e ls     -> handle mp $ CaseE e [ (k,vs,goClear e) | (k,vs,e) <- ls ] 
+        CaseE e ls     -> handle' $ CaseE e [ (k,vs,goClear e) | (k,vs,e) <- ls ]
 
         IfE a b c ->
             -- If we have "succeeded" in accumulating all the bindings
             -- we need, then we can discharge their topological sort
             -- here, without duplicating bindings:
-            if   closed bound mp 
-            then handle mp $ IfE a (goClear b) (goClear c)
-            else IfE (go mp a) -- Otherwise we duplicate...
-                     (go mp b)
-                     (go mp c)
-        MapE  (v,t,rhs) bod -> handle mp $ MapE (v,t,rhs) (goClear bod)
-        FoldE (v1,t1,r1) (v2,t2,r2) bod -> handle mp $ FoldE (v1,t1,r1) (v2,t2,r2) (goClear bod)
+            if   closed bound mp
+            then handle' $ IfE a (goClear b) (goClear c)
+            else (L p) $ IfE (go mp a) -- Otherwise we duplicate...
+                             (go mp b)
+                             (go mp c)
+        MapE  (v,t,rhs) bod -> handle' $ MapE (v,t,rhs) (goClear bod)
+        FoldE (v1,t1,r1) (v2,t2,r2) bod -> handle' $ FoldE (v1,t1,r1) (v2,t2,r2) (goClear bod)
+
+        Ext _ -> handle' $ ex
 
 -- TODO: this needs to preserve any bindings that have TimeIt forms (hasTimeIt).
 -- OR we can only match a certain pattern like (Let (_,_,TimeIt _ _) _)
-handle :: Map.Map Var (Var, L1.Ty, Exp) -> Exp -> Exp
+handle :: Map.Map Var (Var, [()], Ty3, L Exp3) -> L Exp3 -> L Exp3
 handle mp exp =
-    dbgTrace 6 (" [findWitnesses] building lets using vars "++show vs++" for expr: "++ take 80 (show exp)) $      
+    dbgTrace 6 (" [findWitnesses] building lets using vars "++show vs++" for expr: "++ take 80 (show exp)) $
     buildLets mp vars exp
     where freeInBind v = case Map.lookup (view v) mp of
                            Nothing -> []
-                           Just (_v,_t,e) -> withWitnesses $ Set.toList $ L1.freeVars e 
+                           Just (_v,_locs,_t,e) -> Set.toList $ gFreeVars e
           (g,vf,_) = graphFromEdges $ zip3 vs vs $ map freeInBind vs
           vars = reverse $ map (\(x,_,_) -> x) $ map vf $ topSort g
           vs = Map.keys mp
 
-                 
-withWitnesses :: [LocVar] -> [LocVar]
-withWitnesses ls = concatMap f ls
-    where f v = if isWitnessVar v
-                then [v]
-                else [v,toWitnessVar v] -- maybe?
+
+-- withWitnesses :: [LocVar] -> [LocVar]
+-- withWitnesses ls = concatMap f ls
+--     where f v = if isWitnessVar v
+--                 then [v]
+--                 else [v,toWitnessVar v] -- maybe?
 
 -- From the point of view of this pass, we "see through" witness markerS:
 view :: Var -> Var
@@ -100,18 +112,18 @@ view v = v  -- RRN: actually, coming up with a good policy here is problematic.
 --        | otherwise      = v
 
 
-buildLets :: Map.Map Var (Var, L1.Ty, Exp) -> [Var] -> Exp -> Exp
+buildLets :: Map.Map Var (Var,[()], Ty3, L Exp3) -> [Var] -> L Exp3-> L Exp3
 buildLets _mp [] bod = bod
 buildLets mp (v:vs) bod =
     case Map.lookup (view v) mp of
       Nothing -> buildLets mp vs bod
-      Just bnd -> LetE bnd $ buildLets mp vs bod
+      Just bnd -> l$ LetE bnd $ buildLets mp vs bod
 
 
 -- | Are all the free variables currently bound (transitively) in the
 -- environment?
-closed :: Set.Set Var -> Map.Map Var (v, t, Exp) -> Bool
+closed :: Set.Set Var -> Map.Map Var (v, [()], t, L Exp3) -> Bool
 closed bound mp = Set.null (allBound `Set.difference` allUsed)
   where
    allBound = bound `Set.union` Map.keysSet mp
-   allUsed = Set.unions [ L1.freeVars rhs | (_,_,rhs) <- Map.elems mp ]
+   allUsed = Set.unions [ gFreeVars rhs | (_,_,_,rhs) <- Map.elems mp ]
