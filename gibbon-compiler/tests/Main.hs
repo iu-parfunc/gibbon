b478a82551edeabc3b86f602c3378bf09284f4da
Michael Vollmer <vollmerm@indiana.edu>
2018-02-20 02:17:00 -0500

    add unit tests for unification

--- gibbon-compiler/tests/Main.hs
+++ gibbon-compiler/tests/Main.hs
@@ -27,12 +27,14 @@ import Compiler
 import L2.Typecheck
 import L1.Typecheck
 import L3.Typecheck
+import InferLocations2
 
 main :: IO ()
 main = defaultMain allTests
   where allTests = testGroup "All"
                    [ tests
                    , routeEnds2Tests
+                   , inferLocations2Tests
                    , inferEffects2Tests
                    , unariser2Tests
                    -- , l2TypecheckerTests
