99836140c3189ac83eb165f4089e8e50a1ab598d
Paulette Koronkevich <pkoronke@iu.edu>
2018-05-04 17:51:45 -0400

    some progress towards monomorphization

--- gibbon-compiler/src/Packed/FirstOrder/L0/Mono.hs
+++ gibbon-compiler/src/Packed/FirstOrder/L0/Mono.hs
@@ -1,184 +1,255 @@
 module Packed.FirstOrder.L0.Mono
-  (specialize, CurFun, CCall, FCall, L0Fun)
+  ()
   where
 
 import Data.Map as M
 import Data.List as L
 import Data.Loc
+import Control.Monad.State
 
 import Packed.FirstOrder.Common as C
+-- import Packed.FirstOrder.GenericOps
 
 import Packed.FirstOrder.L0.Syntax as L0
 import Packed.FirstOrder.L1.Syntax as L1
 
--- some type defns to make things look cleaner
+-- | specializing functions on types 
 type Exp = (L Exp0)
 
--- we now have curried functions and curried calls
--- curried functions are these variable defns
--- but curried calls vs function calls are PolyAppE vs AppE
-type CurFun  = VarDef Ty0 Exp
-type CCall = Exp
+data Defn = VDef (VarDef Ty0 Exp)
+          | FDef (FunDef Ty0 Exp)
+          | Def  (DDef Ty0)
 
-type L0Fun = FunDef Ty0 Exp
-type FCall = Exp
+type Defns = Map Var Defn
 
-type VarMap = Map Var Exp
+isPolyFun :: PFDef Ty0 Exp -> Either (PFDef Ty0 Exp) (FunDef Ty0 Exp)
+isPolyFun (PFDef name arg (ForAll [] (ArrowTy t0 t1)) b) =
+  Right $ FunDef name (arg,t0) t1 b 
+isPolyFun pf = Left pf
 
-specialize :: CurFun -> CCall -> (L0Fun, (CCall -> FCall))
-specialize f@VarDef {varTy=ty, varBody} call = (newFunc, callFunc)
-  where args     = reverse $ getArgs call
-        varMap   = varsToArgs (getVars varBody) args
-        newTy    = updateTy ty [] args
-        newFunc  = specializeFunc f newTy varMap
-        callFunc = \ x -> updateCall x $ funName newFunc
-        
-varsToArgs :: [Var] -> [Exp] -> VarMap
-varsToArgs vs as = M.fromList $ L.filter f $ L.zip vs as
-  where f = \ p -> not $ isVarE $ snd p 
 
-updateTy :: Ty0 -> [Ty0] -> [Exp] -> Ty0
-updateTy oldTy _ [] = oldTy
-updateTy (ArrowTy t0 t1) ts (a:[]) = if isVarE a
-                                     then ArrowTy (L0.ProdTy $ ts++[t0]) t1
-                                     else ArrowTy (L0.ProdTy ts) t1
-updateTy (ArrowTy t0 t1) ts (a:as) = if isVarE a
-                                     then updateTy t1 (t0:ts) as
-                                     else updateTy t1 ts as
-updateTy err _ _ = error $ "Not an arrow type " ++ show err
-                                        
--- now actually specialize the function to the values in the call
-specializeFunc :: CurFun -> Ty0 -> VarMap -> L0Fun
-specializeFunc VarDef {varName, varBody} (ArrowTy t0 t1) varMap =
-  FunDef {funName=newName, funArg=(newVar,t0), funRetTy=t1, funBody=newFB}
-    where
-      newName = fst $ runSyM 1 $ gensym varName
-      newVar  = toVar "x" -- just a variable name, may cause issues later
-      newVM   = varsToTuple 0 $ getVars varBody
-      newFB   = specializeFB newVM varBody
+isPolyVar :: PVDef Ty0 Exp -> Either (PVDef Ty0 Exp) (VarDef Ty0 Exp)
+isPolyVar (PVDef n (ForAll [] t) b) =
+  Right $ VarDef n t b 
+isPolyVar pf = Left pf
 
-      -- map the variables from the lambda to a tuple access
-      -- if the variable is already mapped, keep the old value in the map
-      varsToTuple :: Int -> [Var] -> VarMap
-      varsToTuple _ [] = varMap
-      varsToTuple i (v:vs) =
-        case M.lookup v varMap of
-          Just _  -> varsToTuple i vs
-          Nothing -> M.insert v (l$ ProjE i $ l$ VarE newVar) $ varsToTuple (succ i) vs
+isPolyDef :: PDDef Ty0 -> Either (PDDef Ty0) (DDef Ty0)
+isPolyDef pd@(PDDef n ds) =
+  if allMono then Right $ DDef n mds else Left pd
+   where mds = L.foldr (\ d acc ->
+                          case d of
+                            (c,s) -> ((c,s'):acc)
+                             where s' = L.map (\(i, ForAll _ t) -> (i,t)) s)
+               []
+               (L.filter
+                (\ d -> case d of
+                          (_, s) -> L.foldr (\ (_, ForAll vs _) acc -> if vs == [] then acc else False) True s
+                )
+               ds) 
+         allMono = L.length ds == L.length mds
 
-      -- specialize the function body
-      specializeFB :: VarMap -> Exp -> Exp
-      specializeFB vM expr@(L loc ex) =
-        case ex of
-          VarE x -> case M.lookup x vM of
-                      Just v  -> v
-                      Nothing -> if x == varName then L loc $ VarE newName else expr
-          -- remove the specialized argument(s) from recursive calls
-          -- change from PolyApp to App
-          Ext (PolyAppE _ _) | isSelfCall expr varName ->
-            L loc $ AppE newName [] $ L loc $ MkProdE $ filterArgs expr
-             -- this removes any arguments that exist within the varmap
-             where filterArgs = L.filter f . collectArgs
-                   f = \ x -> case x of
-                                (L _ (VarE y)) -> not $ member y vM
-                                _              -> False
-          -- application with a var (i.e. a lambda)
-          Ext (PolyAppE r@(L _ (VarE x)) rd) ->
-            case M.lookup x vM of
-              Just v  -> replaceLam v $ specializeFB vM rd
-              Nothing -> L loc $ Ext $ PolyAppE r $ specializeFB vM rd
-          -- remove any lambdas with specialized variables
-          -- this includes removing the "top level" lambda(s),
-          -- since the top level variables are assigned to tuple access
-          Ext (LambdaE x bd) ->
-            case M.lookup x vM of
-              Just _  -> specializeFB vM bd
-              Nothing -> L loc $ Ext $ LambdaE x $ specializeFB vM bd
-          LitE _    -> expr
-          LitSymE _ -> expr
-          PrimAppE p ls -> L loc $ PrimAppE p $ L.map (specializeFB vM) ls
-          MkProdE ls    -> L loc $ MkProdE $ L.map (specializeFB vM) ls
-          ProjE i x     -> L loc $ ProjE i $ specializeFB vM x
-          -- application, change name if necessary
-          AppE a ls d   -> if (a == varName)
-                           then L loc $ AppE newName ls $ specializeFB vM d
-                           else L loc $ AppE a ls $ specializeFB vM d
-          IfE p t f     -> L loc $ IfE (specializeFB vM p)
-                           (specializeFB vM t)
-                           (specializeFB vM f)
-          DataConE ls k as   -> L loc $ DataConE ls k $ L.map (specializeFB vM) as
-          CaseE k ls         -> L loc $ CaseE (specializeFB vM k) $ L.map f ls
-                                 where f = (\ (ds,vs,es) -> (ds,vs,specializeFB vM es))
-          LetE (x,ls,d,a) bd -> L loc $ LetE (x,ls,d, specializeFB vM a) $ specializeFB vM bd
-          TimeIt x d p       -> L loc $ TimeIt (specializeFB vM x) d p
-          Ext (PolyAppE a d) -> L loc $ Ext $ PolyAppE (specializeFB vM a) (specializeFB vM d)
-          MapE _ _     -> error $ "not implemented"
-          FoldE _ _ _  -> error $ "not implemented"
-specializeFunc _ t _ = error $ "Not an Arrow type: " ++ show t       
+filterPDefs :: PDDefs Ty0 -> (PDDefs Ty0, DDefs Ty0)
+filterPDefs pds = M.foldr (\ pd (acc1,acc2) ->
+                             case isPolyDef pd of
+                               Left  y -> (M.insert (dName y) y acc1, acc2)
+                               Right n -> (acc1, M.insert (tyName n) n acc2))
+                  (M.empty, M.empty)
+                  pds
 
--- replace the each instance of the variable in the lambda with the given expression
-replaceLam :: Exp -> Exp -> Exp
-replaceLam (L _ (Ext (LambdaE var body))) ex = replace var ex body
-  where
-      -- replace in the body of the lambda
-      replace :: Var -> Exp -> Exp -> Exp
-      replace v e expr@(L loc b) =
-        case b of
-          VarE v2 | v == v2 -> e
-          VarE _    -> error $ "unbound variable in lambda"
-          LitE _    -> expr
-          LitSymE _ -> expr
-          PrimAppE p ls -> L loc $ PrimAppE p $ L.map (replace v e) ls
-          MkProdE ls    -> L loc $ MkProdE $ L.map (replace v e) ls
-          ProjE i x     -> L loc $ ProjE i $ replace v e x
-          AppE a ls d   -> L loc $ AppE a ls $ replace v e d
-          IfE p t f     -> L loc $ IfE (replace v e p)
-                                       (replace v e t)
-                                       (replace v e f)
-          DataConE ls k as   -> L loc $ DataConE ls k $ L.map (replace v e) as
-          CaseE k ls         -> L loc $ CaseE k $ L.map (\ (ds,vs,es) -> (ds,vs,replace v e es)) ls
-          LetE (x,ls,d,a) bd -> L loc $ LetE (x,ls,d, replace v e a) $ replace v e bd
-          TimeIt x d p       -> L loc $ TimeIt (replace v e x) d p
-          Ext (LambdaE x bd) -> L loc $ Ext $ LambdaE x $ replace v e bd
-          Ext (PolyAppE a d) -> L loc $ Ext $ PolyAppE (replace v e a) (replace v e d)
-          MapE _ _     -> error $ "not implemented"
-          FoldE _ _ _  -> error $ "not implemented"
-replaceLam e _ = error $ "Not a lambda : " ++ show e                                                       
+filterPFDefs :: PFDefs Ty0 Exp -> (PFDefs Ty0 Exp, FunDefs Ty0 Exp)
+filterPFDefs pfds = M.foldr (\ pd (acc1,acc2) ->
+                             case isPolyFun pd of
+                               Left  y -> (M.insert (fName y) y acc1,acc2)
+                               Right n -> (acc1, M.insert (funName n) n acc2))
+                  (M.empty, M.empty)
+                  pfds
 
--- used to update the callsite(s)
--- changes curried functions to function calls with tuples
-updateCall :: CCall -> Var -> FCall
-updateCall e@(L rloc ex) newName =
-  case ex of
-    Ext (PolyAppE _ _) -> L rloc $ AppE newName [] $ L rloc $ MkProdE $ collectArgs e
-    err                -> error $ "Not a valid call to update " ++ show err
+filterPVDefs :: PVDefs Ty0 Exp -> (PVDefs Ty0 Exp, VarDefs Ty0 Exp)
+filterPVDefs pfds = M.foldr (\ pv (acc1,acc2) ->
+                             case isPolyVar pv of
+                               Left  y -> (M.insert (vName y) y acc1,acc2)
+                               Right n -> (acc1, M.insert (varName n) n acc2))
+                  (M.empty, M.empty)
+                  pfds
+
+updatePF :: PFDef Ty0 Exp -> (Ty0 , Ty0) -> FunDef Ty0 Exp
+updatePF (PFDef name arg _ b)  (t0, t1) =
+  let newName = addToName name t0 in
+  FunDef newName (arg,t0) t1 $ replaceName name newName b
+
+updatePV :: PVDef Ty0 Exp -> Ty0 -> VarDef Ty0 Exp
+updatePV (PVDef name _ b) t =
+  let newName = addToName name t in
+  VarDef newName t $ replaceName name newName b 
+
+updatePD :: PDDef Ty0 -> [(DataCon, [(IsBoxed,Ty0)])] -> DDef Ty0 
+updatePD (PDDef name _) ts =
+  DDef (addToName name $ L.map snd ts) ts
+   
+
+traverseAndCopy :: PProg -> MProg
+traverseAndCopy (PProg ds fs vs main) =
+  MProg ddefs fdefs vdefs main'
+  where (pds, mds) = filterPDefs ds
+        (pfs, _)   = filterPFDefs fs
+        (pvs, _)   = filterPVDefs vs
+
+        (lfs, defns1)  = runState (mapM (tACFunDef pfs pvs pds . snd) (M.toList fs)) M.empty
+        fds = M.fromList $ L.map (\ f -> ((fName f) , f)) lfs
+        (pfs', mfs) = filterPFDefs fds
+        (lvs, defns2)  = runState (mapM (tACVarDef pfs' pvs pds . snd) (M.toList vs)) defns1
+        vds = M.fromList $ L.map (\ v -> ((vName v) , v)) lvs
+        (pvs', mvs) = filterPVDefs vds
+        (main', defns) =
+          case main of
+            Just p  -> let (p',ds') = runState (tACExp pfs' pvs' pds p) defns2 in
+                         (Just p', ds')
+            Nothing -> (Nothing, defns2)
+        (ddefs, fdefs, vdefs) = mergeDefns defns mds mfs mvs
+
+
+tACFunDef :: PFDefs Ty0 Exp -> PVDefs Ty0 Exp -> PDDefs Ty0 -> PFDef Ty0 Exp -> State Defns (PFDef Ty0 Exp)
+tACFunDef pfs pvs pds PFDef{fName,fArg,fTy,fBody} =
+ PFDef fName fArg fTy <$> tACExp pfs pvs pds fBody
+
+tACVarDef :: PFDefs Ty0 Exp -> PVDefs Ty0 Exp -> PDDefs Ty0 -> PVDef Ty0 Exp -> State Defns (PVDef Ty0 Exp)
+tACVarDef pfs pvs pds PVDef{vName,vTy,vBody} =
+ PVDef vName vTy <$> tACExp pfs pvs pds vBody
+
+tACExp :: PFDefs Ty0 Exp -> PVDefs Ty0 Exp -> PDDefs Ty0 -> Exp -> State Defns Exp
+tACExp pfs pvs pds (L loc e) = L loc <$>
+  case e of
+    -- ^ functions
+    AppE f ls a -> case M.lookup f pfs of
+                     Just fd -> do
+                       a' <- go a
+                       let t = getFDType -- ^ TODO get types
+                           nfd = updatePF fd t
+                           fdName = funName nfd
+                       ds' <- get 
+                       _   <- put $ M.insert fdName (FDef nfd) ds'
+                       return $ AppE fdName ls a'
+                     Nothing -> do
+                       a' <- go a
+                       return $ AppE f ls a'
+    -- ^ calls to variable definitions
+    Ext (PolyAppE f d) | isVarE $ funCall f ->
+                           case M.lookup (var $ funCall f) pvs of
+                             Just vd -> do
+                               d' <- go d
+                               let ts = getVDType -- ^ TODO get types
+                                   nvd = updatePV vd ts
+                                   vdName = varName nvd
+                               ds' <- get
+                               f'  <- updatePolyF f vdName
+                               _   <- put $ M.insert vdName (VDef nvd) ds'
+                               return $ Ext $ PolyAppE f' d'
+                             Nothing -> do
+                               d' <- go d
+                               f' <- go f
+                               return $ Ext $ PolyAppE f' d'
+    -- ^ calls to data defns
+    DataConE lc dc es -> case M.lookup (toVar dc) pds of
+                           Just dd -> do
+                             es' <- mapM go es
+                             let ts = getDDType -- ^ TODO get types
+                                 ndd = updatePD dd ts
+                                 ddName = tyName ndd
+                             ds' <- get
+                             _   <- put $ M.insert ddName (Def ndd) ds'
+                             return $ DataConE lc (fromVar ddName) es'
+                           Nothing -> do
+                             es' <- mapM go es
+                             return $ DataConE lc dc es'
+    PrimAppE p ls -> PrimAppE p <$> mapM go ls
+    MkProdE ls -> MkProdE <$> mapM go ls
+    ProjE i x  -> ProjE i <$> go x
+    IfE p t f -> IfE <$> go p <*> go t <*> go f
+    CaseE k ls -> CaseE <$> go k <*> mapM f ls
+      where f = \ (dc,v,ex) -> (dc,v,) <$> go ex
+    LetE (v,lc,ty,rhs) b -> LetE <$> (v,lc,ty,) <$> go rhs <*> go b
+    TimeIt x t b -> do
+      x' <- go x
+      return $ TimeIt x' t b
+    Ext ext ->
+      case ext of
+        PolyAppE f d -> do
+          f' <- go f
+          d' <- go d
+          return $ Ext $ PolyAppE f' d'
+        LambdaE x b  -> Ext <$> LambdaE x <$> go b
+    _ -> return e
+  where go = tACExp pfs pvs pds
+        updatePolyF :: Exp -> Var -> State Defns Exp
+        updatePolyF (L loc' e') v = L loc' <$>
+          case e' of
+            VarE _ -> return $ VarE v
+            Ext (PolyAppE f d) -> do
+              d' <- go d
+              f' <- updatePolyF f v
+              return $ Ext (PolyAppE f' d')
+            err -> return $ err
 
 -- Helpers
+
+mergeDefns :: Defns -> DDefs Ty0 -> FunDefs Ty0 Exp -> VarDefs Ty0 Exp -> (DDefs Ty0, FunDefs Ty0 Exp, VarDefs Ty0 Exp)
+mergeDefns defns ds fs vs = (M.union dds ds, M.union dfs fs, M.union dvs vs)
+  where (dds, dfs, dvs) = L.foldr f (M.empty, M.empty, M.empty) $ M.toList defns
+        f (v,defn) (dds', dfs', dvs') =
+          case defn of
+            VDef vd -> (dds', dfs', M.insert v vd dvs')
+            FDef fd -> (dds', M.insert v fd dfs', dvs')
+            Def dd  -> (M.insert v dd dds', dfs', dvs')
+
+funCall :: Exp -> Exp
+funCall (L loc ex) = 
+  case ex of
+    Ext (PolyAppE (L _ (VarE v)) _) -> L loc $ VarE v
+    Ext (PolyAppE f _) -> funCall f 
+    e                  -> L loc $ e
+    
+var :: Exp -> Var
+var (L _ (VarE v)) = v
+var e = error $ "Not a var " ++ show e
 
 isVarE :: Exp -> Bool
 isVarE (L _ e) = case e of
                    VarE _ -> True
                    _      -> False
 
-getArgs :: CCall -> [Exp]
-getArgs (L _ call) =
-  case call of
-    Ext (PolyAppE f a) -> a : getArgs f
-    _                  -> []
+replaceName :: Var -> Var -> Exp -> Exp
+replaceName old new (L loc b) = L loc $
+  case b of
+    VarE v | v == old -> VarE new
+    AppE f ls a | f == old -> AppE new ls $ go a
+    DataConE lc dc es -> DataConE lc dc $ L.map go es
+    PrimAppE p ls -> PrimAppE p $ L.map go ls
+    MkProdE ls -> MkProdE $ L.map go ls
+    ProjE i x  -> ProjE i $ go x
+    IfE p t f -> IfE (go p) (go t) (go f)
+    CaseE k ls -> CaseE (go k) $ L.map f ls
+      where f = \ (dc,v,ex) -> (dc,v, go ex)
+    LetE (v,lc,ty,rhs) bd -> LetE (v,lc,ty,go rhs) $ go bd
+    TimeIt x t e -> TimeIt (go x) t e
+    Ext ext ->
+      case ext of
+        PolyAppE f d -> Ext $ PolyAppE (go f) (go d)
+        LambdaE x bd -> Ext $ LambdaE x $ go bd
+    _ -> b
+    where go = replaceName old new
 
-collectArgs :: CCall -> [Exp]
-collectArgs = L.filter isVarE . reverse . getArgs
+addToName :: (Show a) => Var -> a -> Var
+addToName v a = toVar $ fromVar v ++ "_" ++ show a
 
-getVars :: Exp -> [Var]
-getVars (L _ ex) =
-  case ex of
-    Ext (LambdaE v b) -> v : getVars b
-    _                 -> []
 
-isSelfCall :: Exp -> Var -> Bool
-isSelfCall (L _ ex) name =
-  case ex of
-    AppE v _ _ -> v == name
-    Ext (PolyAppE (L _ (VarE v)) _) -> v == name
-    Ext (PolyAppE f _) -> isSelfCall f name
-    err                -> error $ "Not an application " ++ show err
+-- ^ TODO get types
+getVDType :: Ty0
+getVDType = L0.IntTy
+
+getFDType :: (Ty0, Ty0)
+getFDType = (L0.IntTy, L0.IntTy)
+
+getDDType :: [(DataCon, [(IsBoxed,Ty0)])]
+getDDType = []
+
+
