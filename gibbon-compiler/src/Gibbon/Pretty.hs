078fe51ef2d81d8fb2c944f01adbd08cfe54e533
Michael Vollmer <vollmerm@indiana.edu>
2018-05-23 22:34:36 -0400

    switch to pretty printing between passes for L1,L2,L3 #90

--- gibbon-compiler/src/Gibbon/Pretty.hs
+++ gibbon-compiler/src/Gibbon/Pretty.hs
@@ -1,47 +1,69 @@
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Gibbon.Pretty ( Pretty(..), printL1, printL2 ) where
+module Gibbon.Pretty ( Printer(..), Pretty(..), printL1, printL2, printL3 ) where
 
 import Gibbon.L1.Syntax
 import qualified Gibbon.L2.Syntax as L2
+import qualified Gibbon.L3.Syntax as L3
+import qualified Gibbon.L4.Syntax as L4
 import Gibbon.Common
 import Gibbon.GenericOps
 import Data.Loc
-import Data.List
 import qualified Data.Map as M
 import Text.PrettyPrint.HughesPJ as PP
 import Text.PrettyPrint.GenericPretty
-import Prelude (String, Maybe(..), Show(..), ($), error)
 
-class Expression e => Pretty e where
-    pprint :: e -> Doc
+class Printer a where
+    pprinter :: a -> String
 
 printL1 :: Prog -> String
 printL1 (Prog _ funs me) =
     let meDoc = case me of
                   Nothing -> empty
-                  Just (e,_) -> pprint e
-        funsDoc = vcat $ map (\fd -> renderfunc (fromVar $ funName fd) (doc $ funArg fd) (pprint $ funBody fd)) $ M.elems funs 
-    in render $ funsDoc $+$ renderMain meDoc
+                  Just (e,_) -> renderMain $ pprint e
+        funsDoc = vcat $ map (\fd -> renderfunc (fromVar $ funName fd) (doc $ funArg fd) (doc $ fst $ funTy fd) (pprint $ funBody fd)) $ M.elems funs 
+    in render $ funsDoc $+$ meDoc
+
+instance Printer Prog where
+    pprinter = printL1
 
 printL2 :: L2.Prog -> String
 printL2 (L2.Prog _ funs me) =
     let meDoc = case me of
                   Nothing -> empty
-                  Just (e,_) -> pprint e
-        funsDoc = vcat $ map (\fd -> renderfunc (fromVar $ L2.funName fd) (doc $ L2.funArg fd) (pprint $ L2.funBody fd)) $ M.elems funs 
-    in render $ funsDoc $+$ renderMain meDoc
+                  Just (e,_) -> renderMain $ pprint e
+        funsDoc = vcat $ map (\fd -> renderfunc (fromVar $ L2.funName fd) (doc $ L2.funArg fd) (doc $ L2.arrOut $ L2.funTy fd) (pprint $ L2.funBody fd)) $ M.elems funs 
+    in render $ funsDoc $+$ meDoc
 
-renderfunc :: String -> Doc -> Doc -> Doc
-renderfunc f arg m = text f <+> arg <+> equals $$ nest 4 m 
+instance Printer L2.Prog where
+    pprinter = printL2
+
+printL3 :: L3.Prog -> String
+printL3 (L3.Prog _ funs me) = 
+    let meDoc = case me of
+                  Nothing -> empty
+                  Just (e,_) -> renderMain $ pprint e
+        funsDoc = vcat $ map (\fd -> renderfunc (fromVar $ L3.funName fd) (doc $ L3.funArg fd) (doc $ fst $ L3.funTy fd) (pprint $ L3.funBody fd)) $ M.elems funs 
+    in render $ funsDoc $+$ meDoc
+
+instance Printer L3.Prog where
+    pprinter = printL3
+
+instance Printer L4.Prog where
+    pprinter = show -- TODO: implement pretty printing for L4
+
+renderfunc :: String -> Doc -> Doc -> Doc -> Doc
+renderfunc f arg ty m = text f <+> arg <> colon <> ty <+> equals $$ nest 4 m 
     
 renderMain :: Doc -> Doc
-renderMain m = renderfunc "main" (text "()") m
+renderMain m = text "main" <+> equals $$ nest 4 m
+
+class Expression e => Pretty e where
+    pprint :: e -> Doc
 
 instance Pretty (NoExt l d) where
     pprint _ = empty
@@ -80,7 +102,7 @@ instance (Show l, Out l, Pretty (e l (UrTy l)), Expression (e l (UrTy l)), TyOf 
           DataConE l dc es -> text dc <+> lbrack <> doc l <> rbrack <+>
                               lparen <> hcat (punctuate (text ",") (map pprint es)) <> rparen
           TimeIt e _ty _b -> pprint e
-          Ext ext -> pprint ext -- do we want to print this?
+          Ext ext -> pprint ext
           MapE{} -> error $ "Unexpected form in program: " ++ show ex0
           FoldE{} -> error $ "Unexpected form in program: " ++ show ex0
 
@@ -106,6 +128,9 @@ instance (Show l, Out l, Pretty (L (L2.E2 l (UrTy l)))) => Pretty (L2.E2Ext l (U
                                                      hcat (punctuate (text ",") [doc l2,text (fromVar v2)]) <>
                                                      rparen <+>
                                                      pprint e
+
+instance (Show l, Out l) => Pretty (L3.E3Ext l (UrTy l)) where
+    pprint = doc -- TODO: replace this with actual pretty printing for L3 forms
 
 instance (Show l, Out l) => Pretty (L2.PreLocExp l) where
     pprint le =
