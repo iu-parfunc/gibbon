04c892a9d873ba2b3979d8e28dbfacdb46a0b1fb
Chaitanya Koparkar <ckoparkar@gmail.com>
2019-01-15 10:14:08 -0500

    Put all syntax in a single file

--- gibbon-compiler/src/Gibbon/Language.hs
+++ gibbon-compiler/src/Gibbon/Language.hs
@@ -1,9 +1,7 @@
 module Gibbon.Language
     ( module Gibbon.Language.Constants
-    , module Gibbon.Language.Expr
     , module Gibbon.Language.Syntax
     ) where
 
 import Gibbon.Language.Constants
-import Gibbon.Language.Expr
 import Gibbon.Language.Syntax
