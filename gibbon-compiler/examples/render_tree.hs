0837c8ea03d623acaf529a89b87a87b7e8a688d6
laithsakka <lsakka@purdue.edu>
2019-01-24 13:46:28 -0500

    finish computeWidth ,there is interesting cases where wadler approach does not work

--- gibbon-compiler/examples/render_tree.hs
+++ gibbon-compiler/examples/render_tree.hs
@@ -1,75 +1,115 @@
-data NodeData = NodeData Int Int Int Int Float MeasureMode FontStyle 
- getMesureMode :: NodeData -> MeasureMode
- 
+data NodeData = NodeDataK Int Int Int Int Float MeasureMode FontStyle 
+--    NodeDataK posX posY height width relWidth mMode fontStyle 
+
 data MeasureMode = Flex | Relative | Absolute 
-data FontStyle = FontStyle Int Int Int 
-
--- class Data {
---     public:
---       int PosX = 0;
---       int PosY = 0;
---       int Height = 0;
---       int Width = 0;
---       float RelWidth;
---       MEASURE_MODE WMode;
---       FontInfo FontStyle;
--- };
--- struct FontInfo {
---     public:
---       unsigned int Type = -1;
---       unsigned int Size = -1;
---       unsigned int Color = -1;
---     };
-
+data FontStyle = FontStyleK Int Int Int 
 data Document = Document PageList NodeData
-data PageList = PageListInner Page PageList  NodeData
-              | PageListEnd Page NodeData
+data PageList = PageListInner Page PageList  
+              | PageListEnd Page 
 data Page     = Page HorizContainerList NodeData
-data HorizContainerList = HorizContainerListInner ElementsList HorizContainerList NodeData
-                        | HorizContainerListEnd ElementsList NodeData
 
-data ElementsList       = ElementsListInner  Element ElementsList NodeData
-                        | ElementsListEnd Element NodeData
+data HorizContainerList = HorizContainerListInner ElementsList HorizContainerList  
+                        | HorizContainerListEnd ElementsList  
+
+data ElementsList  = ElementsListInner  Element ElementsList 
+                   | ElementsListEnd Element 
 data Element =  ImageCons Int Int StringJ NodeData
               | TextBoxCons Int Int StringJ NodeData
               | VertContainer HorizContainerList NodeData
 data StringJ  = StrChar Char 
              | StrEnd
 
-resolveFlexWidthDoc :: Document -> Document 
-resolveFlexWidthDoc doc = case (doc) of
-     Document pgList -> Document (resolveFlexWidthPList pgList)
+maxI :: Int -> Int -> Int
+maxI a b = if (a>b) then a else b 
 
-resolveFlexWidthPList :: PageList -> PageList 
-resolveFlexWidthPList pgList = case (pgList) of
-    PageListInner page remList ->
-        PageListInner (resolveFlexWidthP page) (resolveFlexWidthPList remList)
-    PageListEnd page -> 
-        PageListEnd (resolveFlexWidthP page)
+addI :: Int -> Int -> Int 
+addI a b = a + b 
 
-resolveFlexWidthP :: Page -> Page
-resolveFlexWidthP p  = case (p) of 
-    Page hzList -> If Page (resolveFlexWidthHzLst hzList)
+getMeasureMode :: NodeData-> MeasureMode
+getMeasureMode node  = case (node) of 
+    NodeDataK posX posY height width relWidth mMode fontStyle -> mMode
 
-resolveFlexWidthHzLst :: HorizContainerList -> HorizContainerList
-resolveFlexWidthHzLst hzList  = case (hzList) of 
+getWidth :: NodeData -> Int  
+getWidth nData  = case (nData) of 
+    NodeDataK posX posY height width relWidth mMode fontStyle -> width
+
+getWidthElement :: Element -> Int  
+getWidthElement e  = case (e) of 
+    ImageCons a b s nData  -> getWidth nData
+    TextBoxCons  a b s nData -> getWidth nData
+    VertContainer hzList nData -> getWidth nData
+ 
+updateWidth :: NodeData -> Int -> NodeData  
+updateWidth nData newWidth = case (nData) of 
+    NodeDataK posX posY height width relWidth mMode fontStyle -> 
+        NodeDataK posX posY height newWidth relWidth mMode fontStyle
+
+-- We might need to rewrite this into two functions .. duh thats alot of work 
+-- and maybe performance reduction 
+updateWidthIfFlex :: NodeData -> Int -> NodeData  
+updateWidthIfFlex nData newWidth = case (nData) of 
+    NodeDataK posX posY height width relWidth mMode fontStyle -> 
+        case (mMode) of
+            Flex ->  updateWidth nData newWidth
+            Absolute -> nData
+            Relative -> nData
+
+getMaxWidthHzList :: HorizContainerList -> Int
+getMaxWidthHzList hzList = case (hzList) of
+    HorizContainerListInner elmList remList -> 
+        maxI (sumWidthsElmList elmList) ( getMaxWidthHzList remList)
+    HorizContainerListEnd elmList -> sumWidthsElmList elmList
+
+sumWidthsElmList :: ElementsList -> Int
+sumWidthsElmList elmList = case (elmList) of 
+    ElementsListInner e remList -> addI (getWidthElement e ) (sumWidthsElmList remList)
+    ElementsListEnd e  -> getWidthElement e 
+----- 
+-- This traversal compute the width of the page which consists of nested 
+-- horizontal and vertical containers with leaf elements 
+-- TODO : remove the widthMode and assume one mode for that
+
+resolveWidthDoc :: Document -> Document 
+resolveWidthDoc doc  = case (doc) of
+     Document pgList nData -> Document (resolveWidthPList pgList) nData
+
+resolveWidthPList :: PageList -> PageList 
+resolveWidthPList pgList  = case (pgList) of
+    PageListInner page remList  ->
+        PageListInner (resolveWidthP page) (resolveWidthPList remList) 
+    PageListEnd page  -> 
+        PageListEnd (resolveWidthP page) 
+
+-- This is different than treefuser implementation where maxWidth was explicitly 
+-- computing maxWidth during resolveWidth and store it on the stack .
+-- we hope that tupling would that for us here.
+resolveWidthP :: Page -> Page
+resolveWidthP p  = case (p) of
+    Page hzList nData-> 
+        let  hzList' = resolveWidthHzLst hzList 
+         in let maxWidth = getMaxWidthHzList hzList' 
+            in let nData' = updateWidthIfFlex nData maxWidth    
+               in Page hzList' nData'
+
+resolveWidthHzLst :: HorizContainerList -> HorizContainerList
+resolveWidthHzLst hzList  = case (hzList) of 
     HorizContainerListInner  elmList remList ->
-        HorizContainerListInner (resolveFlexWidthElmList elmList) 
-           (resolveFlexWidthHzLst remList)
+        HorizContainerListInner (resolveWidthElmList elmList)  
+           (resolveWidthHzLst remList) 
     HorizContainerListEnd elmList -> 
-        HorizContainerListEnd (resolveFlexWidthElmList elmList)
+        HorizContainerListEnd (resolveWidthElmList elmList) 
 
-resolveFlexWidthElmList ::ElementsList -> ElementsList
-resolveFlexWidthElmList elemList  = case (elemList) of 
-    ElementsListInner element remList ->
-        ElementsListInner (resolveFlexWidthElm element) 
-            (resolveFlexWidthElmList remList)
+resolveWidthElmList ::ElementsList -> ElementsList
+resolveWidthElmList elemList  = case (elemList) of 
+    ElementsListInner element remList  ->
+        ElementsListInner (resolveWidthElm element) 
+            (resolveWidthElmList remList) 
     ElementsListEnd element -> 
-        ElementsListEnd (resolveFlexWidthElm element)
+        ElementsListEnd (resolveWidthElm element) 
 
-resolveFlexWidthElm ::Element -> Element
-resolveFlexWidthElm element  = case (element) of 
-    VertContainer hzList  -> VertContainer (resolveFlexWidthHzLst hzList)
+resolveWidthElm ::Element -> Element
+resolveWidthElm element  = case (element) of 
+    VertContainer hzList nData -> VertContainer (resolveWidthHzLst hzList) nData
 
 main =  StrEnd
 
