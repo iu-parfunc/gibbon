fde5952f05a89ead45e23803f9cac79e78851c49
Chaitanya Koparkar <ckoparkar@gmail.com>
2019-06-11 13:38:32 -0400

    Fix the build - check-in tests

---
+++ gibbon-compiler/examples/test15d_symsearch.hs
@@ -0,0 +1,17 @@
+data List = Nil | Cons Sym List
+
+elem_go :: Int -> Sym -> List -> Int
+elem_go n x xs =
+  case xs of
+    Nil       -> -1
+    Cons y ys -> if eqsym y x
+                 then n
+                 else elem_go (n + 1) x ys
+
+elem :: Sym -> List -> Int
+elem x xs = elem_go 0 x xs
+
+
+gibbon_main = let a  = quote "abc"
+                  as = Cons (quote "a") (Cons (quote "b") (Cons (quote "abc") Nil))
+              in elem a as
---
+++ gibbon-compiler/examples/test15e_gensym.hs
@@ -0,0 +1,44 @@
+import Prelude hiding ( Maybe(..), Either (..), succ, not, foldr, map, head)
+
+data List a = Nil
+            | Cons a (List a)
+  deriving Show
+
+head :: a -> List a -> a
+head d xs =
+  case xs of
+    Nil -> d
+    Cons l ls -> l
+
+gen_symbols :: Int -> List Sym
+gen_symbols n =
+ if n == 0
+ then Nil
+ else let s = gensym
+      in Cons s (gen_symbols (n - 1))
+
+count_occ :: Int -> Sym -> List Sym -> Int
+count_occ acc s xs =
+  case xs of
+    Nil -> acc
+    Cons l ls -> if eqsym s l
+                 then count_occ (acc + 1) s ls
+                 else count_occ acc s ls
+
+has_dups :: List Sym -> Bool
+has_dups xs =
+  case xs of
+    Nil       -> False
+    Cons l ls ->
+      if count_occ 0 l ls > 0
+      then True
+      else has_dups ls
+
+gibbon_main =
+  let xs      = gen_symbols 10
+      s       = quote "unused"
+      ys      = Cons (head s xs) xs
+      no_dups = has_dups xs
+      dups    = has_dups ys
+  -- Expected answer: (False, True)
+  in (no_dups, dups)
