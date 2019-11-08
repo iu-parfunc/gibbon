55e50438336a2574e27c595cf4cfd2b6a4004388
Chaitanya Koparkar <ckoparkar@gmail.com>
2019-11-08 15:52:21 -0500

    lc-bench; dead code elimination

--- gibbon-compiler/examples/ast/LC.hs
+++ gibbon-compiler/examples/ast/LC.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 -- Substitution-based lambda calculus benchmark
 module LC where
 
+-- import Control.DeepSeq
+-- import Data.Time.Clock
 --import Helpers
 
 -- anything thing other than the last constuctor should be in a function call
@@ -14,7 +18,7 @@ module LC where
 data Val = IntV Int
          | LamV IntJ Exp
          | ErrorV
-         --  deriving Show
+  deriving Show
 
 data Exp = LamE IntJ Exp
          | AppE Exp Exp
@@ -22,11 +26,16 @@ data Exp = LamE IntJ Exp
          | LitE Int
          | PlusE Exp Exp
          | LetE IntJ Exp Exp
-       --    deriving Show
+  deriving Show
+
+data Binds = NilBinds | ConsBinds Int Exp Binds
+  deriving Show
 
 data IntJ = ZeroJ | SuccJ IntJ
+  deriving Show
 
 data BoolJ = TBool | FBool
+  deriving Show
 
 isEquel :: IntJ -> IntJ -> BoolJ
 isEquel a b =
@@ -45,6 +54,12 @@ isEquelHelper  b a'  =
     ZeroJ -> FBool
     SuccJ b' -> isEquel a' b'
 
+isTrue :: BoolJ -> Bool
+isTrue b =
+  case b of
+    TBool -> True
+    FBool -> False
+
 data Symbol = A| B| C| D |E |F |G
 
 
@@ -59,7 +74,7 @@ interpExp e =
       VarE s -> ErrorV
       LitE i -> IntV i
       PlusE e1 e2 -> interpPlus  (interpExp e1) e2
-      LetE s e1 e2 -> interpExp (substExp e2 s e1 )
+      LetE s e1 e2 -> interpExp (substExp e2 s e1)
 
 interpApp :: Val -> Exp -> Val
 interpApp v e2 =
@@ -75,7 +90,7 @@ interpPlus v e2 =
       IntV i -> addN (interpExp e2) i
       ErrorV -> ErrorV
 
-substExpHelper1 :: BoolJ -> IntJ -> IntJ-> Exp -> Exp -> Exp
+substExpHelper1 :: BoolJ -> IntJ -> IntJ -> Exp -> Exp -> Exp
 substExpHelper1  b s from e1 to =
   case (b) of
     TBool -> LamE s e1
@@ -110,6 +125,7 @@ addN v i1  =
       IntV i2 -> IntV (i1 + i2)
       ErrorV -> ErrorV
 
+--------------------------------------------------------------------------------
 -- Constant propogation
 
 constProp :: Exp -> Exp
@@ -133,6 +149,7 @@ constPropLet e1 e2 s  =
       PlusE e1 e2 -> PlusE (constProp e1) (constProp e2)
       LetE sl el1 el2 -> LetE s (constPropLet  el1 el2 sl) (constProp e2)
 
+--------------------------------------------------------------------------------
 -- Partial evaluation of plus
 ptExp :: Exp -> Exp
 ptExp e =
@@ -164,6 +181,140 @@ plPlusInner e2 i =
       LitE i2 -> LitE (i + i2)
       PlusE ep1 ep2 -> PlusE (LitE  i) (PlusE ep1 ep2)
       LetE s el1 el2 -> PlusE (LitE  i) (LetE s el1 el2)
+
+--------------------------------------------------------------------------------
+-- Dead code elimination
+
+data Vars = ConsVars IntJ Vars | NilVars
+  deriving Show
+
+removeVar :: Vars -> IntJ -> Vars
+removeVar ls i =
+  case ls of
+    NilVars        -> NilVars
+    ConsVars j rst ->
+      let rst' = removeVar rst i
+      in removeVarHelper (isEquel i j) j rst'
+
+removeVarHelper :: BoolJ -> IntJ -> Vars -> Vars
+removeVarHelper b j ls =
+  case b of
+    TBool -> ls
+    FBool -> ConsVars j ls
+
+memqVar :: Vars -> IntJ -> BoolJ
+memqVar ls i =
+  case ls of
+    NilVars -> FBool
+    ConsVars j rst -> memqVarHelper (isEquel i j) rst i
+
+memqVarHelper :: BoolJ -> Vars -> IntJ -> BoolJ
+memqVarHelper b ls i =
+  case b of
+    TBool -> TBool
+    FBool -> memqVar ls i
+
+appendVars :: Vars -> Vars -> Vars
+appendVars xs ys =
+  case xs of
+    NilVars -> ys
+    ConsVars x rst -> ConsVars x (appendVars rst ys)
+
+data ExpVars = ExpVars Exp Vars
+  deriving Show
+
+getExp :: ExpVars -> Exp
+getExp evs =
+  case evs of
+    ExpVars e1 ls -> e1
+
+getVars :: ExpVars -> Vars
+getVars evs =
+  case evs of
+    ExpVars e1 ls -> ls
+
+elimDeadBindings' :: Exp -> Vars -> ExpVars
+elimDeadBindings' e acc =
+  case e of
+    LamE v bod ->
+      let vsbod' = elimDeadBindings' bod acc
+          vs     = getVars vsbod'
+          bod'   = getExp vsbod'
+          acc'   = appendVars vs acc
+      in ExpVars (LamE v bod') acc'
+
+    AppE a b ->
+      let vsa' = elimDeadBindings' a acc
+          vs   = getVars vsa'
+          a'   = getExp vsa'
+          wsb' = elimDeadBindings' b acc
+          ws   = getVars wsb'
+          b'   = getExp wsb'
+      in ExpVars (AppE a' b') (appendVars (appendVars vs ws) acc)
+
+    VarE v -> ExpVars (VarE v) (ConsVars v acc)
+
+    LitE i -> ExpVars (LitE i) acc
+
+    PlusE a b ->
+      let vsa' = elimDeadBindings' a acc
+          vs   = getVars vsa'
+          a'   = getExp vsa'
+          wsb' = elimDeadBindings' b acc
+          ws   = getVars wsb'
+          b'   = getExp wsb'
+      in ExpVars (PlusE a' b') (appendVars (appendVars vs ws) acc)
+
+    LetE v rhs bod ->
+      let vsrhs' = elimDeadBindings' rhs acc
+          vs     = getVars vsrhs'
+          rhs'   = getExp vsrhs'
+          wsbod' = elimDeadBindings' bod acc
+          ws     = getVars wsbod'
+          bod'   = getExp wsbod'
+      in elimDeadBindingsLet' (memqVar ws v) v rhs' vs bod' ws acc
+
+elimDeadBindingsLet' :: BoolJ -> IntJ -> Exp -> Vars -> Exp -> Vars -> Vars -> ExpVars
+elimDeadBindingsLet' b v rhs vs bod ws acc =
+  case b of
+    -- drop this binding
+    FBool -> ExpVars bod (appendVars ws acc)
+    TBool -> ExpVars (LetE v rhs bod) (appendVars (appendVars vs ws) acc)
+
+elimDeadBindings :: Exp -> Exp
+elimDeadBindings e =
+  let evs = elimDeadBindings' e NilVars in
+  case evs of
+    ExpVars e2 vs -> e2
+
+{-
+
+testElimDeadBindings :: Exp
+testElimDeadBindings =
+  LetE varA (LamE varC (PlusE (VarE varC) (LitE 10))) $
+  LetE varB (AppE (VarE varA) (LitE 20)) $
+  LetE varD (LamE varC (PlusE (VarE varC) (LitE 100))) $
+  LetE varE (LitE 20) $
+  VarE varB
+
+-}
+
+--------------------------------------------------------------------------------
+-- Desugar let*, square and incr
+
+desugarExp :: Exp -> Exp
+desugarExp e =
+  case e of
+    LamE s e1 -> LamE s (ptExp e1)
+    AppE e1 e2 -> AppE (ptExp e1) (ptExp e2)
+    VarE s -> VarE s
+    LitE i -> LitE i
+    PlusE e1 e2 -> ptPlus (ptExp e1) (ptExp e2)
+    LetE s e1 e2 -> LetE s (ptExp e1) (ptExp e2)
+
+
+--------------------------------------------------------------------------------
+
 
 -- Example term
 varA :: IntJ
@@ -216,15 +367,16 @@ eval v =
       LamV s e -> -1
       ErrorV -> -2
 
-
+{-
 main =
  do
   let fltAppE507 :: Exp = buildLargeExp 23
   t1 <- fltAppE507  `deepseq` getCurrentTime
   t2 <-  (gibbon_main fltAppE507) `deepseq` getCurrentTime
   print $ diffUTCTime t2 t1
+-}
 
-gibbon_main = (eval (interpExp (ptExp (constProp (buildLargeExp 1)))))
+gibbon_main = (eval (interpExp (elimDeadBindings (ptExp (constProp (buildLargeExp 1))))))
 --  gibbon_main =  (interpExp (ptExp (constProp (buildLargeExp 100))))
 
 -- main :: IO ()
