888193bb148bbae74d298f5215d7b9f4a269720c
Chaitanya Koparkar <ckoparkar@gmail.com>
2020-02-24 10:53:29 -0500

    Update benchmarks

--- gibbon-compiler/examples/parallel/ParAdd1Tree.hs
+++ gibbon-compiler/examples/parallel/ParAdd1Tree.hs
@@ -3,21 +3,10 @@ module ParAdd1Tree where
 data Tree = Leaf Int
           | Node Int Tree Tree
 
-fib_seq :: Int -> Int
-fib_seq n =
-    if n == 0
-    then 0
-    else if n == 1
-    then 1
-    else
-        let x = fib_seq (n - 1)
-            y = fib_seq (n - 2)
-        in x + y
-
 mkTree_seq :: Int -> Tree
 mkTree_seq i =
   if i <= 0
-  then Leaf (fib_seq 20)
+  then Leaf 6765
   else
       let x = mkTree_seq (i-1)
           y = mkTree_seq (i-1)
