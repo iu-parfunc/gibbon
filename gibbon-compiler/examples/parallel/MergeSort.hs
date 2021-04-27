89d975855f682851d66c99ce9aeac61c10bef583
Chaitanya Koparkar <ckoparkar@gmail.com>
2021-04-27 10:26:34 -0400

    Add some comments to merge sort [skip ci]

--- gibbon-compiler/examples/parallel/MergeSort.hs
+++ gibbon-compiler/examples/parallel/MergeSort.hs
@@ -27,225 +27,250 @@ mergesort_debugPrint vec =
     in ()
 
 binarySearch' :: Int -> Int -> (a -> a -> Int) -> Vector a -> a -> Int
-binarySearch' lo hi f s x =
+binarySearch' lo hi cmp vec query =
     let n = hi - lo
     in if n == 0
        then lo
        else let mid = lo + (div n 2)
-                pivot = nth s mid
-                cmp = f x pivot
-            in if cmp < 0
-               then binarySearch' lo mid f s x
-               else if cmp > 0
-                    then binarySearch' (mid+1) hi f s x
+                pivot = nth vec mid
+                -- IMPORTANT:
+                tst = cmp query pivot
+            in if tst < 0
+               then binarySearch' lo mid cmp vec query
+               else if tst > 0
+                    then binarySearch' (mid+1) hi cmp vec query
                     else mid
 
+-- | Return 'query's *position* in 'vec'.
+--
+-- That is, return a *p* s.t.
+-- (1) elements vec[0]..vec[p] are less than query, and
+-- (2) elements vec[p+1]..vec[end] are greater than query.
 binarySearch :: (a -> a -> Int) -> Vector a -> a -> Int
 {-# INLINE binarySearch #-}
-binarySearch f s x = binarySearch' 0 (length s) f s x
-
-write_loop_seq :: Int -> Int -> Int -> Vector a -> Vector a -> Vector a
-write_loop_seq idx offset end from to =
-    if idx == end
-    then to
-    else let to1 = inplacevupdate to (idx+offset) (nth from idx)
-         in write_loop_seq (idx+1) offset end from to1
-
-write_loop :: Int -> Int -> Int -> Vector a -> Vector a -> Vector a
-write_loop idx offset end from to =
-    if (end - idx) < goto_seqmerge
-    then write_loop_seq idx offset end from to
-    else let mid = div (idx + end) 2
-             to1 = spawn (write_loop idx offset mid from to)
-             to2 = write_loop mid offset end from to
-             _ = sync
-         in to2
-
--- i1 index into s1
--- i2 index into s2
--- j index into output
-writeMerge_seq_loop :: Int -> Int -> Int -> Int -> Int -> (a -> a -> Int) -> Vector a -> Vector a -> Vector a -> Vector a
-writeMerge_seq_loop i1 i2 j n1 n2 f s1 s2 t =
-    if i1 == n1
-    then let tmp1 = vslice i2 (n2-i2) s2
-             t2 = write_loop_seq 0 j (n2-i2) tmp1 t
-         in t2
-    else if i2 == n2
-         then let tmp1 = vslice i1 (n1-i1) s1
-                  t1 = write_loop_seq 0 j (n1-i1) tmp1 t
-              in t1
-         else let x1 = nth s1 i1
-                  x2 = nth s2 i2
-              in if f x1 x2 < 0
-                 then let t1 = inplacevupdate t j x1
-                      in writeMerge_seq_loop (i1+1) i2 (j+1) n1 n2 f s1 s2 t1
-                 else let t1 = inplacevupdate t j x2
-                      in writeMerge_seq_loop i1 (i2+1) (j+1) n1 n2 f s1 s2 t1
-
-
-writeMerge_seq :: (a -> a -> Int) -> Vector a -> Vector a -> Vector a -> Vector a
-{-# INLINE writeMerge_seq #-}
-writeMerge_seq f s1 s2 t =
-    let n1 = length s1
-        n2 = length s2
-        res = writeMerge_seq_loop 0 0 0 n1 n2 f s1 s2 t
-    in res
-
-
-writeMerge :: (a -> a -> Int) -> Vector a -> Vector a -> Vector a -> Vector a
-writeMerge f s1 s2 t =
-    if length t < goto_seqmerge
-    then writeMerge_seq f s1 s2 t
-    else
-        let n1 = length s1
-            n2 = length s2
-        in if n1 == 0
-           then write_loop 0 0 n2 s2 t
-           else let mid1 = div n1 2
-                    pivot = nth s1 mid1
-                    mid2 = (binarySearch f s2 pivot)
-                    l1 = vslice 0 mid1 s1
-                    r1 = vslice (mid1+1) (n1 - (mid1+1)) s1
-                    l2 = vslice 0 mid2 s2
-                    r2 = vslice mid2 (n2-mid2) s2
-                    _ = inplacevupdate t (mid1+mid2) pivot
-                    len_t = length t
-                    tl = vslice 0 (mid1+mid2) t
-                    tr = vslice (mid1+mid2+1) (len_t - (mid1+mid2+1)) t
-                    tl1 = spawn (writeMerge f l1 l2 tl)
-                    tr1 = writeMerge f r1 r2 tr
-                    _ = sync
-                in t
+binarySearch cmp vec query = binarySearch' 0 (length vec) cmp vec query
 
 --------------------------------------------------------------------------------
 
+-- | Copy elements from 'from' to 'to'.
+--
+-- from[from_idx]    ===>   to[to_idx]
+-- from[from_idx+1]  ===>   to[to_idx+1]
+-- ...
+write_loop_seq :: Int -> Int -> Int -> Vector a -> Vector a -> Vector a
+write_loop_seq to_idx from_idx end from to =
+    if from_idx == end
+    then to
+    else let to1 = inplacevupdate to to_idx (nth from from_idx)
+         in write_loop_seq (to_idx+1) (from_idx+1) end from to1
+
+-- | Parallel variant of 'write_loop'.
+write_loop :: Int -> Int -> Int -> Vector a -> Vector a -> Vector a
+write_loop to_idx from_idx end from to =
+    if (end - from_idx) < goto_seqmerge
+    then write_loop_seq to_idx from_idx end from to
+    else let mid = div (from_idx + end) 2
+             to1 = spawn (write_loop to_idx from_idx mid from to)
+             to2 = write_loop (to_idx+mid-from_idx) mid end from to
+             _ = sync
+         in to2
+
+-- | The main sequential merge function.
+-- i1 index into src_1
+-- i2 index into src_2
+-- j index into tmp (output).
+writeMerge_seq_loop :: Int -> Int -> Int -> Int -> Int -> (a -> a -> Int) -> Vector a -> Vector a -> Vector a -> Vector a
+writeMerge_seq_loop i1 i2 j n1 n2 cmp src_1 src_2 tmp =
+    -- copy src_2 to tmp.
+    if i1 == n1
+    then let -- tmp_1 = vslice i2 (n2-i2) src_2
+             tmp_2 = write_loop_seq j i2 n2 src_2 tmp
+         in tmp_2
+    -- copy src_1 to tmp.
+    else if i2 == n2
+         then let -- tmp_1 = vslice i1 (n1-i1) src_1
+                  tmp_2 = write_loop_seq j i1 n1 src_1 tmp
+              in tmp_2
+    -- compare elements from both arrays and write the smaller one at j.
+         else let x1 = nth src_1 i1
+                  x2 = nth src_2 i2
+              in if cmp x1 x2 < 0
+                 then let tmp_1 = inplacevupdate tmp j x1
+                      in writeMerge_seq_loop (i1+1) i2 (j+1) n1 n2 cmp src_1 src_2 tmp_1
+                 else let tmp_1 = inplacevupdate tmp j x2
+                      in writeMerge_seq_loop i1 (i2+1) (j+1) n1 n2 cmp src_1 src_2 tmp_1
+
+
+-- | Merge 'src_1' and 'src_2' into 'tmp'.
+writeMerge_seq :: (a -> a -> Int) -> Vector a -> Vector a -> Vector a -> Vector a
+{-# INLINE writeMerge_seq #-}
+writeMerge_seq cmp src_1 src_2 tmp =
+    let n1 = length src_1
+        n2 = length src_2
+        res = writeMerge_seq_loop 0 0 0 n1 n2 cmp src_1 src_2 tmp
+    in res
+
+-- | Parallel version of 'writeMerge_seq'.
+writeMerge :: (a -> a -> Int) -> Vector a -> Vector a -> Vector a -> Vector a
+writeMerge cmp src_1 src_2 tmp =
+    if length tmp < goto_seqmerge
+    then writeMerge_seq cmp src_1 src_2 tmp
+    else
+        let n1 = length src_1
+            n2 = length src_2
+        in if n1 == 0
+           then write_loop 0 0 n2 src_2 tmp
+           else let mid1 = div n1 2
+                    pivot = nth src_1 mid1
+                    mid2 = (binarySearch cmp src_2 pivot)
+                    -- cannot use splitAt because we want the second array
+                    -- to be from (mid1+1) to (n1 - (mid1+1)).
+                    src_1_l = vslice 0 mid1 src_1
+                    src_1_r = vslice (mid1+1) (n1 - (mid1+1)) src_1
+                    -- splitAt-able.
+                    src_2_l = vslice 0 mid2 src_2
+                    src_2_r = vslice mid2 (n2-mid2) src_2
+                    _ = inplacevupdate tmp (mid1+mid2) pivot
+                    len_t = length tmp
+                    -- cannot use splitAt here as well.
+                    tmp_l = vslice 0 (mid1+mid2) tmp
+                    tmp_r = vslice (mid1+mid2+1) (len_t - (mid1+mid2+1)) tmp
+                    tmp_l1 = spawn (writeMerge cmp src_1_l src_2_l tmp_l)
+                    tmp_r1 = writeMerge cmp src_1_r src_2_r tmp_r
+                    _ = sync
+                in tmp
+
+--------------------------------------------------------------------------------
+
+-- | Sort the left and right halves of 'src' into 'tmp', and merge the results
+-- back into 'src'.
 writeSort1 :: (a -> a -> Int) -> Vector a -> Vector a -> Vector a
-writeSort1 f s t =
-    let len = length s in
+writeSort1 cmp src tmp =
+    let len = length src in
     if len < gotoQuickSort
     -- then quickSort_par' 0 (length s) f s
-    then inplacevsort s f
+    then inplacevsort src cmp
     else
         let half = div len 2
-            (sl,sr) = splitAt half s
-            (tl,tr) = splitAt half t
-            tl1 = spawn (writeSort2 f sl tl)
-            tr1 = writeSort2 f sr tr
+            (src_l,src_r) = splitAt half src
+            (tmp_l,tmp_r) = splitAt half tmp
+            tmp_l1 = spawn (writeSort2 cmp src_l tmp_l)
+            tmp_r1 = writeSort2 cmp src_r tmp_r
             _ = sync
-            res = writeMerge f tl1 tr1 s
+            res = writeMerge cmp tmp_l1 tmp_r1 src
         in res
 
+-- | Sequential variant of 'writeSort1'.
 writeSort1_seq :: (a -> a -> Int) -> Vector a -> Vector a -> Vector a
-writeSort1_seq f s t =
-    let len = length s in
+writeSort1_seq cmp src tmp =
+    let len = length src in
     if len < gotoQuickSort
     -- then quickSort_par' 0 (length s) f s
-    then inplacevsort s f
+    then inplacevsort src cmp
     else
         let half = div len 2
-            (sl,sr) = splitAt half s
-            (tl,tr) = splitAt half t
-            tl1 = writeSort2_seq f sl tl
-            tr1 = writeSort2_seq f sr tr
-            res = writeMerge_seq f tl1 tr1 s
+            (src_l,src_r) = splitAt half src
+            (tmp_l,tmp_r) = splitAt half tmp
+            tmp_l1 = writeSort2_seq cmp src_l tmp_l
+            tmp_r1 = writeSort2_seq cmp src_r tmp_r
+            res = writeMerge_seq cmp tmp_l1 tmp_r1 src
         in res
 
+-- | Sort the left and right halves of 'tmp' into 'src', and merge the results
+-- back into 'tmp'.
 writeSort2 :: (a -> a -> Int) -> Vector a -> Vector a -> Vector a
-writeSort2 f s t =
-    let len =length s in
+writeSort2 cmp src tmp =
+    let len = length src in
     if len < gotoQuickSort
     then
-        let t1 = write_loop 0 0 len s t
+        let tmp_1 = write_loop 0 0 len src tmp
         -- in quickSort_par' 0 (length t1) f t1
-        in inplacevsort t1 f
+        in inplacevsort tmp_1 cmp
     else
         let half = div len 2
-            (sl,sr) = splitAt half s
-            (tl,tr) = splitAt half t
-            sl1 = spawn (writeSort1 f sl tl)
-            sr1 = (writeSort1 f sr tr)
+            (src_l,src_r) = splitAt half src
+            (tmp_l,tmp_r) = splitAt half tmp
+            src_l1 = spawn (writeSort1 cmp src_l tmp_l)
+            src_r1 = (writeSort1 cmp src_r tmp_r)
             _ = sync
-            res = writeMerge f sl1 sr1 t
+            res = writeMerge cmp src_l1 src_r1 tmp
         in res
 
+-- | Sequential variant of 'writeSort2'.
 writeSort2_seq :: (a -> a -> Int) -> Vector a -> Vector a -> Vector a
-writeSort2_seq f s t =
-    let len =length s in
+writeSort2_seq cmp src tmp =
+    let len = length src in
     if len < gotoQuickSort
     then
-        let t1 = write_loop_seq 0 0 len s t
+        let tmp_1 = write_loop_seq 0 0 len src tmp
         -- in quickSort_par' 0 (length t1) f t1
-        in inplacevsort t1 f
+        in inplacevsort tmp_1 cmp
     else
         let half = div len 2
-            (sl,sr) = splitAt half s
-            (tl,tr) = splitAt half t
-            sl1 = writeSort1_seq f sl tl
-            sr1 = writeSort1_seq f sr tr
-            res = writeMerge_seq f sl1 sr1 t
+            (src_l,src_r) = splitAt half src
+            (tmp_l,tmp_r) = splitAt half tmp
+            src_l1 = writeSort1_seq cmp src_l tmp_l
+            src_r1 = writeSort1_seq cmp src_r tmp_r
+            res = writeMerge_seq cmp src_l1 src_r1 tmp
         in res
 
+-- | Parallel in-place merge sort.
 mergeSort' :: (a -> a -> Int) -> Vector a -> Vector a
 {-# INLINE mergeSort' #-}
-mergeSort' f s =
-    let t :: Vector a
-        t = valloc (length s)
-        t2 = writeSort1 f s t
-    in s
+mergeSort' cmp src =
+    let tmp :: Vector a
+        tmp = valloc (length src)
+        tmp2 = writeSort1 cmp src tmp
+    in src
 
+-- | Sequential in-place merge sort.
 mergeSort'_seq :: (a -> a -> Int) -> Vector a -> Vector a
 {-# INLINE mergeSort'_seq #-}
-mergeSort'_seq f s =
-    let t :: Vector a
-        t = valloc (length s)
-        t2 = writeSort1_seq f s t
-    in s
+mergeSort'_seq cmp src =
+    let tmp :: Vector a
+        tmp = valloc (length src)
+        tmp2 = writeSort1_seq cmp src tmp
+    in src
 
+-- | Parallel merge sort, copies the input into a separate array.
 mergeSort :: (a -> a -> Int) -> Vector a -> Vector a
 {-# INLINE mergeSort #-}
-mergeSort f vec =
+mergeSort cmp vec =
     let vec2 = copy_par vec
-        vec3 = mergeSort' f vec2
+        vec3 = mergeSort' cmp vec2
     in vec3
 
+-- | Sequential merge sort, copies the input into a separate array.
 mergeSort_seq :: (a -> a -> Int) -> Vector a -> Vector a
 {-# INLINE mergeSort_seq #-}
-mergeSort_seq f vec =
+mergeSort_seq cmp vec =
     let vec2 = copy vec
-        vec3 = mergeSort'_seq f vec2
+        vec3 = mergeSort'_seq cmp vec2
     in vec3
 
 cStdlibSort :: (a -> a -> Int) -> Vector a -> Vector a
-cStdlibSort f vec =
+cStdlibSort cmp vec =
   let vec2 = copy_par vec
-      vec3 = inplacevsort vec2 f
+      vec3 = inplacevsort vec2 cmp
   in vec3
 
 cStdlibSort_seq :: (a -> a -> Int) -> Vector a -> Vector a
-cStdlibSort_seq f vec =
+cStdlibSort_seq cmp vec =
   let vec2 = copy vec
-      vec3 = inplacevsort vec2 f
+      vec3 = inplacevsort vec2 cmp
   in vec3
 
-check_sorted_floats :: (Float -> Float -> Int) -> Vector Float -> ()
-check_sorted_floats f sorted =
-  let arr1 = slice 0 (length sorted - 1) sorted
-      arr2 = slice 1 (length sorted - 1) sorted
-      check = ifoldl (\acc i elt1 -> let elt2 = nth arr2 i
-                                     in acc && (f elt1 elt2 <= 0))
-              True
-              arr1
-  in print_check check
-
-check_sorted_ints :: (Int -> Int -> Int) -> Vector Int -> ()
-check_sorted_ints f sorted =
-  let arr1 = slice 0 (length sorted - 1) sorted
-      arr2 = slice 1 (length sorted - 1) sorted
-      check = ifoldl (\acc i elt1 -> let elt2 = nth arr2 i
-                                     in acc && (f elt1 elt2 <= 0))
-              True
-              arr1
-  in print_check check
+check_sorted :: (a -> a -> Int) -> Vector a -> ()
+check_sorted cmp sorted =
+  let len = length sorted in
+  if len <= 1
+  then print_check True
+  else let arr1 = slice 0 (len - 2) sorted
+           check = ifoldl (\acc i elt1 -> let elt2 = nth arr1 (i+1)
+                                          in acc && (cmp elt1 elt2 <= 0))
+                   True
+                   arr1
+       in print_check check
 
 --------------------------------------------------------------------------------
 
@@ -256,9 +281,9 @@ gibbon_main =
             let n = sizeParam
                 arr = generate n (\i -> intToFloat (rand))
                 sorted = iterate (mergeSort_seq compare_float arr)
-            in check_sorted_floats compare_float sorted
+            in check_sorted compare_float sorted
         else
             let n = sizeParam
                 arr = generate n (\i -> intToFloat (rand))
                 sorted = iterate (mergeSort compare_float arr)
-            in check_sorted_floats compare_float sorted
+            in check_sorted compare_float sorted
