6a66b65dbc8cdb1625ccb110987729b766cad746
Chaitanya Koparkar <ckoparkar@gmail.com>
2021-05-29 15:49:53 -0400

    loop versions of traversal benchmarks

--- gibbon-compiler/examples/parallel/BenchFrag.hs
+++ gibbon-compiler/examples/parallel/BenchFrag.hs
@@ -5,9 +5,11 @@ import BinTree
 import KdTree
 import FoldConstants
 
+--------------------------------------------------------------------------------
+
 bench_frag_seqbuildfib :: ()
 bench_frag_seqbuildfib =
-  let n = sizeParam
+  let n = 18
       tr = (mkTreeFib_seq n)
       expected = (2 ^ n) * (fib_seq 20)
       n = iterate (sumTree_seq tr)
@@ -17,7 +19,7 @@ bench_frag_seqbuildfib =
 
 bench_frag_parbuildfib :: ()
 bench_frag_parbuildfib =
-  let n = sizeParam
+  let n = 18
       cutoff = 6
       tr = (mkTreeFib_par cutoff n)
       expected = (2 ^ n) * (fib_seq 20)
@@ -26,12 +28,50 @@ bench_frag_parbuildfib =
       _ = printint n
   in ()
 
+loop_trav_buildfib :: Int -> Tree -> Int
+loop_trav_buildfib iter tr =
+    let n = sumTree_seq tr
+    in if iter == 1
+       then n
+       else loop_trav_buildfib (iter-1) tr
+
+bench_frag_seqbuildfib_loop :: ()
+bench_frag_seqbuildfib_loop =
+  let n = 18
+      cutoff = 6
+      tr = (mkTreeFib_seq n)
+      expected = (2 ^ n) * (fib_seq 20)
+      iters = sizeParam
+      _ = printsym (quote "running ")
+      _ = printint iters
+      _ = printsym (quote " times.\n")
+      m = iterate (loop_trav_buildfib iters tr)
+      _ = print_check (expected == m)
+      _ = printint m
+  in ()
+
+bench_frag_parbuildfib_loop :: ()
+bench_frag_parbuildfib_loop =
+  let n = 18
+      cutoff = 6
+      tr = (mkTreeFib_par cutoff n)
+      expected = (2 ^ n) * (fib_seq 20)
+      iters = sizeParam
+      _ = printsym (quote "running ")
+      _ = printint iters
+      _ = printsym (quote " times.\n")
+      m = iterate (loop_trav_buildfib iters tr)
+      _ = print_check (expected == n)
+      _ = printint m
+  in ()
+
+
+--------------------------------------------------------------------------------
+
 bench_frag_seqbuildkdtree :: ()
 bench_frag_seqbuildkdtree =
     let pts :: Vector (Float, Float, Float)
         pts = readArrayFile Nothing
-        n       = sizeParam
-        radius  = intToFloat n
         tr      = (mkKdTree_seq pts)
         n       = iterate (sumKdTree tr)
         _       = check_buildkdtree pts tr
@@ -42,14 +82,54 @@ bench_frag_parbuildkdtree :: ()
 bench_frag_parbuildkdtree =
     let pts :: Vector (Float, Float, Float)
         pts = readArrayFile Nothing
-        n       = sizeParam
-        radius  = intToFloat n
         cutoff  = 32000
         tr      = (mkKdTree_par cutoff pts)
         n       = iterate (sumKdTree tr)
         _       = check_buildkdtree pts tr
         _       = printfloat n
     in ()
+
+
+loop_trav_buildkdtree :: Int -> KdTree -> Float
+loop_trav_buildkdtree iter tr =
+    let n = sumKdTree tr
+    in if iter == 1
+       then n
+       else loop_trav_buildkdtree (iter-1) tr
+
+
+bench_frag_seqbuildkdtree_loop :: ()
+bench_frag_seqbuildkdtree_loop =
+    let pts :: Vector (Float, Float, Float)
+        pts = readArrayFile Nothing
+        tr      = (mkKdTree_seq pts)
+        iters = sizeParam
+        _ = printsym (quote "running ")
+        _ = printint iters
+        _ = printsym (quote " times.\n")
+        n       = iterate (loop_trav_buildkdtree iters tr)
+        _       = check_buildkdtree pts tr
+        _       = printfloat n
+    in ()
+
+
+bench_frag_parbuildkdtree_loop :: ()
+bench_frag_parbuildkdtree_loop =
+    let pts :: Vector (Float, Float, Float)
+        pts = readArrayFile Nothing
+        cutoff  = 32000
+        tr      = (mkKdTree_par cutoff pts)
+        iters = sizeParam
+        _ = printsym (quote "running ")
+        _ = printint iters
+        _ = printsym (quote " times.\n")
+        n       = iterate (loop_trav_buildkdtree iters tr)
+        _       = check_buildkdtree pts tr
+        _       = printfloat n
+    in ()
+
+
+--------------------------------------------------------------------------------
 
 bench_frag_seqcoins :: ()
 bench_frag_seqcoins =
@@ -61,11 +141,12 @@ bench_frag_seqcoins =
         coins4 = cons_ll (10,99) coins3
         coins5 = cons_ll (5,122) coins4
         coins6 = cons_ll (1,177) coins5
-        amt = sizeParam
+        amt = 999
         tr = (payA_seq amt coins6)
         len = iterate (lenA tr)
         _ = printint len
     in ()
+
 
 bench_frag_parcoins :: ()
 bench_frag_parcoins =
@@ -78,15 +159,67 @@ bench_frag_parcoins =
         coins5 = cons_ll (5,122) coins4
         coins6 = cons_ll (1,177) coins5
     -- in printCoins coins6
-        amt = sizeParam
+        amt = 999
         tr = (payA_par 3 amt coins6)
         len = iterate (lenA tr)
         _ = printint len
     in ()
 
+loop_trav_coins :: Int -> AList -> Int
+loop_trav_coins iter tr =
+    let n = lenA tr
+    in if iter == 1
+       then n
+       else loop_trav_coins (iter-1) tr
+
+
+bench_frag_seqcoins_loop :: ()
+bench_frag_seqcoins_loop =
+    let coins0 :: List (Int,Int)
+        coins0 = alloc_ll
+        coins1 = cons_ll (250,55) coins0
+        coins2 = cons_ll (100,88) coins1
+        coins3 = cons_ll (25,88) coins2
+        coins4 = cons_ll (10,99) coins3
+        coins5 = cons_ll (5,122) coins4
+        coins6 = cons_ll (1,177) coins5
+    -- in printCoins coins6
+        amt = 999
+        tr = (payA_seq amt coins6)
+        iters = sizeParam
+        _ = printsym (quote "running ")
+        _ = printint iters
+        _ = printsym (quote " times.\n")
+        len = iterate (loop_trav_coins iters tr)
+        _ = printint len
+    in ()
+
+bench_frag_parcoins_loop :: ()
+bench_frag_parcoins_loop =
+    let coins0 :: List (Int,Int)
+        coins0 = alloc_ll
+        coins1 = cons_ll (250,55) coins0
+        coins2 = cons_ll (100,88) coins1
+        coins3 = cons_ll (25,88) coins2
+        coins4 = cons_ll (10,99) coins3
+        coins5 = cons_ll (5,122) coins4
+        coins6 = cons_ll (1,177) coins5
+    -- in printCoins coins6
+        amt = 999
+        tr = (payA_par 3 amt coins6)
+        iters = sizeParam
+        _ = printsym (quote "running ")
+        _ = printint iters
+        _ = printsym (quote " times.\n")
+        len = iterate (loop_trav_coins iters tr)
+        _ = printint len
+    in ()
+
+--------------------------------------------------------------------------------
+
 bench_frag_seqfoldconstants :: ()
 bench_frag_seqfoldconstants =
-    let exp = buildExp sizeParam
+    let exp = buildExp 26
         exp1 = (foldConstants2 exp)
         m = iterate (sumExp exp1)
         _ = printint m
@@ -94,27 +227,77 @@ bench_frag_seqfoldconstants =
 
 bench_frag_parfoldconstants :: ()
 bench_frag_parfoldconstants =
-    let exp = buildExp sizeParam
+    let exp = buildExp 26
         exp1 = (foldConstants2_par 0 exp)
         m = iterate (sumExp exp1)
         _ = printint m
     in ()
+
+loop_trav_foldconstants :: Int -> Exp -> Int
+loop_trav_foldconstants iter tr =
+    let n = sumExp tr
+    in if iter == 1
+       then n
+       else loop_trav_foldconstants (iter-1) tr
+
+bench_frag_seqfoldconstants_loop :: ()
+bench_frag_seqfoldconstants_loop =
+    let exp = buildExp 26
+        exp1 = (foldConstants2 exp)
+        iters = sizeParam
+        _ = printsym (quote "running ")
+        _ = printint iters
+        _ = printsym (quote " times.\n")
+        m = iterate (loop_trav_foldconstants iters exp1)
+        _ = printint m
+    in ()
+
+bench_frag_parfoldconstants_loop :: ()
+bench_frag_parfoldconstants_loop =
+    let exp = buildExp 26
+        exp1 = (foldConstants2_par 0 exp)
+        iters = sizeParam
+        _ = printsym (quote "running ")
+        _ = printint iters
+        _ = printsym (quote " times.\n")
+        m = iterate (loop_trav_foldconstants 26 exp1)
+        _ = printint m
+    in ()
+
+
+--------------------------------------------------------------------------------
 
 gibbon_main =
     if eqBenchProg "frag_seqbuildfib"
     then bench_frag_seqbuildfib
     else if eqBenchProg "frag_parbuildfib"
     then bench_frag_parbuildfib
+    else if eqBenchProg "frag_seqbuildfib_loop"
+    then bench_frag_seqbuildfib_loop
+    else if eqBenchProg "frag_parbuildfib_loop"
+    then bench_frag_parbuildfib_loop
     else if eqBenchProg "frag_seqbuildkdtree"
     then bench_frag_seqbuildkdtree
     else if eqBenchProg "frag_parbuildkdtree"
     then bench_frag_parbuildkdtree
+    else if eqBenchProg "frag_seqbuildkdtree_loop"
+    then bench_frag_seqbuildkdtree_loop
+    else if eqBenchProg "frag_parbuildkdtree_loop"
+    then bench_frag_parbuildkdtree_loop
     else if eqBenchProg "frag_seqcoins"
     then bench_frag_seqcoins
     else if eqBenchProg "frag_parcoins"
     then bench_frag_parcoins
+    else if eqBenchProg "frag_seqcoins_loop"
+    then bench_frag_seqcoins_loop
+    else if eqBenchProg "frag_parcoins_loop"
+    then bench_frag_parcoins_loop
     else if eqBenchProg "frag_seqfoldconstants"
     then bench_frag_seqfoldconstants
     else if eqBenchProg "frag_parfoldconstants"
     then bench_frag_parfoldconstants
+    else if eqBenchProg "frag_seqfoldconstants_loop"
+    then bench_frag_seqfoldconstants_loop
+    else if eqBenchProg "frag_parfoldconstants_loop"
+    then bench_frag_parfoldconstants_loop
     else ()
--- gibbon-compiler/examples/parallel/compiler/CompilerFrag.hs
+++ gibbon-compiler/examples/parallel/compiler/CompilerFrag.hs
@@ -4,7 +4,7 @@ import Compiler
 
 bench_frag_seq_compiler :: ()
 bench_frag_seq_compiler =
-  let ex = make_big_ex sizeParam 0
+  let ex = make_big_ex 15 0
       p = ProgramA (quote "Int") ex
       compiled = (compile2 p)
       n = iterate (countLeavesX86 compiled)
@@ -13,10 +13,43 @@ bench_frag_seq_compiler =
 
 bench_frag_par_compiler :: ()
 bench_frag_par_compiler =
-  let ex = make_big_ex sizeParam 0
+  let ex = make_big_ex 15 0
       p = ProgramA (quote "Int") ex
       compiled_par = (compile2_par p)
       n = iterate (countLeavesX86 compiled_par)
+      _ = printint n
+  in ()
+
+loop_trav_compiler :: Int -> PseudoX86 -> Int
+loop_trav_compiler iter tr =
+    let n = countLeavesX86 tr
+    in if iter == 1
+       then n
+       else loop_trav_compiler (iter-1) tr
+
+bench_frag_seq_compiler_loop :: ()
+bench_frag_seq_compiler_loop =
+  let ex = make_big_ex 15 0
+      p = ProgramA (quote "Int") ex
+      compiled = (compile2 p)
+      iters = sizeParam
+      _ = printsym (quote "running ")
+      _ = printint iters
+      _ = printsym (quote " times.\n")
+      n = iterate (loop_trav_compiler iters compiled)
+      _ = printint n
+  in ()
+
+bench_frag_par_compiler_loop :: ()
+bench_frag_par_compiler_loop =
+  let ex = make_big_ex 15 0
+      p = ProgramA (quote "Int") ex
+      compiled_par = (compile2_par p)
+      iters = sizeParam
+      _ = printsym (quote "running ")
+      _ = printint iters
+      _ = printsym (quote " times.\n")
+      n = iterate (loop_trav_compiler iters compiled_par)
       _ = printint n
   in ()
 
@@ -24,4 +57,10 @@ bench_frag_par_compiler =
 gibbon_main =
   if eqBenchProg "frag_seqcompiler"
   then bench_frag_seq_compiler
-  else bench_frag_par_compiler
+  else if eqBenchProg "frag_parcompiler"
+  then bench_frag_par_compiler
+  else if eqBenchProg "frag_seqcompiler_loop"
+  then bench_frag_seq_compiler_loop
+  else if eqBenchProg "frag_parcompiler_loop"
+  then bench_frag_par_compiler_loop
+  else ()
