5f8e1086eeeb0a702d8fe7acfbbf39dd48594477
Aditya Gupta <adityagupta1089@gmail.com>
2021-09-12 23:40:45 +0000

    Added failing Nested Tuples Test

---
+++ gibbon-compiler/examples/poly/NestedTuples.hs
@@ -0,0 +1,7 @@
+module NestedTuples where
+
+
+foo (a, b, (c, d, (e, f, (g, h, (i, j))))) = ((a,b), (c,(d, e), f), (g, h), (i, j))
+
+gibbon_main = foo (1, (2,3), (4, (5,6), (7,8, (9,10, (11,12))))) 
+
