4da9145cb5cf47d348521f3140fe22bb76ef87f7
Chaitanya Koparkar <ckoparkar@gmail.com>
2022-02-01 10:00:58 -0500

    Update nixpkgs

--- shell.nix
+++ shell.nix
@@ -7,24 +7,22 @@ let
                    rev = "6070a8ee799f629cb1d0004821f77ceed94d3992";
                  });
   pkgs = import (builtins.fetchGit {
-                   name = "nixos-unstable-2021-03-11";
                    url = "https://github.com/nixos/nixpkgs/";
                    ref = "refs/heads/master";
-                   # Commit hash for nixos-unstable as of 2021-03-11
-                   rev = "a3228bb6e8bdbb9900f30a11fe09006fdabf7b71";
+                   # Commit hash for nixos-unstable as of 2022-01-30
+                   rev = "8b01281b66cba818abea8dbbdb3614b1b38961e3";
                  }) { overlays = [ moz_overlay ]; };
   stdenv = pkgs.overrideCC pkgs.stdenv pkgs.gcc7;
-  ghc = pkgs.haskell.compiler.ghc865;
-  ghc901 = pkgs.haskell.compiler.ghc901;
+  ghc = pkgs.haskell.compiler.ghc902;
   rust = (pkgs.rustChannelOf { rustToolchain = ./gibbon-rts/rust-toolchain; }).rust;
   gibbon_dir = builtins.toString ./.;
 in
   with pkgs;
   stdenv.mkDerivation {
     name = "basicGibbonEnv";
-    buildInputs = [ ghc ghc901 gcc7 which boehmgc uthash racket cabal-install ghcid
-                    gdb valgrind stack stdenv ncurses unzip rr
-                    rust rustfmt clippy
+    buildInputs = [ ghc cabal-install stack gcc7 boehmgc uthash rust racket
+                    # dev environment
+                    stdenv ncurses unzip which rr rustfmt clippy ghcid gdb valgrind
                   ];
     shellHook = ''
       export GIBBONDIR=${gibbon_dir} \
