a025f8d5f630a243521d2f96f57fdc540b7917ca
vidush <vidush.sl@gmail.com>
2022-02-12 16:06:02 -0500

    edits

--- gibbon-compiler/src/Gibbon/Passes/RegionsInwards.hs
+++ gibbon-compiler/src/Gibbon/Passes/RegionsInwards.hs
@@ -9,6 +9,8 @@ import Data.Foldable as F
 import Gibbon.Common
 import Gibbon.L2.Syntax
 
+import Gibbon.LocExp
+
 --define data type that can be Region, Loc, LocExp
 data DelayedBind = DelayRegion Region
                  | DelayLoc LocVar LocExp | DelayParRegion Region
@@ -24,9 +26,7 @@ regionsInwards Prog{ddefs,fundefs,mainExp} = do
     mainExp' <- case mainExp of
         Nothing -> return Nothing
         Just (mn, ty)-> do 
-          let keySet   = S.empty
-              bindings = []           --Use M.empty for creating the empty dictionary
-              dict     = M.singleton keySet bindings
+          let dict = M.empty      -- Use M.empty for creating the empty dictionary
               in Just <$> (,ty) <$> placeRegionInwards dict mn
 
     --returns the new  ddefs, fundefs and mainExpressions
@@ -36,16 +36,14 @@ regionsInwards Prog{ddefs,fundefs,mainExp} = do
 
 placeRegionsInwardsFunBody :: FunDef2 -> PassM FunDef2
 placeRegionsInwardsFunBody f@FunDef{funBody} = do
-  let keySet   = S.empty 
-      bindings = []
-      dict     = M.singleton keySet bindings
+  let dict = M.empty
   funBody' <- placeRegionInwards dict funBody
   return $ f {funBody = funBody'}
 
 -- Recursive funtion that will move the regions inwards
 placeRegionInwards :: DelayedBindEnv-> Exp2 -> PassM Exp2
 placeRegionInwards dictionary ex =
-    case ex of
+  case ex of
     Ext ext ->
       case ext of
         --take care of regions
@@ -125,9 +123,9 @@ placeRegionInwards dictionary ex =
                           newDict  = M.insert myKey' valList' tempDict
                           in placeRegionInwards newDict rhs
 
-            FreeLE -> error "How do we handle a Free LE" --Recurse on the rhs of this
+            FreeLE -> error "How do we handle a Free LE" --For FreeLE we need to figure out how to handle this
         
-        --Handle a parallel LetRegion ? 
+        --Handle a parallel LetRegion 
         LetParRegionE r rhs -> do
           let key' = S.singleton (regionToVar r)
               val' = [DelayParRegion r]
@@ -135,39 +133,69 @@ placeRegionInwards dictionary ex =
               in placeRegionInwards dictionary' rhs
 
 
-        RetE locList variable                          -> return ex --Does this need to handle anything special?
-        FromEndE loc                                   -> return ex --don't need to worry about it will appear later in the pipeline
-        BoundsCheck integer l1 l2                      -> return ex --dont't need to worry about it
-        AddFixed variable integer                      -> return ex --Return
-        IndirectionE tyCon dataCon (l1,v1) (l2,v2) rhs -> placeRegionInwards dictionary rhs -- Skip the recursion and won't appear here
-        GetCilkWorkerNum                               -> return ex --Just Return
-        LetAvail varList rhs                           -> placeRegionInwards dictionary rhs --This will Recurse on the rhs directly, any special handling here?
+        RetE locList variable                          -> return ex {-Here you can't just return the expression, you will need to look at the locList to see 
+                                                                      which variables are in the dictionary which are alive (meaning codegen for them hasn't happened yet) 
+                                                                      if they are not codegen yet we will just write them out before-}
+
+        FromEndE loc                                   -> return ex {-Don't need to worry about it will appear later in the pipeline, Just return the expression-}
+
+        BoundsCheck integer l1 l2                      -> return ex {-Don't need to worry about it will appear later in the pipeline, Just return the expression-}
+        AddFixed variable integer                      -> return ex {-Return the expression-}
+        IndirectionE tyCon dataCon (l1,v1) (l2,v2) rhs -> return ex {-We skip the recursion, the IndirectionE does not appear until later in the IR language, only return the expression-}
+        GetCilkWorkerNum                               -> return ex {-Just return the expression, there is no recusrion to do here-}
+
+        LetAvail vs e                           -> Ext <$> LetAvail vs <$> go e {-This will Recurse on the rhs directly-}
         
 
      -- Straightforward recursion ...
-    VarE{}                        -> return ex --Just return Nothing special here? 
-    LitE{}                        -> return ex --Just return Nothing special here? 
-    FloatE{}                      -> return ex --Just return Nothing special here? 
-    LitSymE{}                     -> return ex --Just return Nothing special here? 
-    AppE{}                        -> return ex --Just return Nothing special here? 
-    PrimAppE{}                    -> return ex --Just return Nothing special here? 
-    DataConE{}                    -> error "I am not sure that this means here, what is DataConE?" --might need to codegen before
-    ProjE i e                     -> error "What is ProjE?" --Tuple (i, e), recurse
-    IfE a b c                     -> error "This needs to be implemented, here we would move the regions inwards or not"
-    MkProdE ls                    -> error "What is MkProdE" --Tuple (), recurse
-    LetE (v,locs,ty,rhs) bod      -> error "Don't really know what this is?" -- will need to check rhs
+    VarE{}                        -> return ex --Just return Nothing special here 
+    LitE{}                        -> return ex --Just return Nothing special here 
+    FloatE{}                      -> return ex --Just return Nothing special here 
+    LitSymE{}                     -> return ex --Just return Nothing special here 
+    AppE{}                        -> return ex --Just return Nothing special here 
+    PrimAppE{}                    -> return ex --Just return Nothing special here 
+    DataConE loc dataCons args    -> _ {-Can't just return the expression here, We would need to check if the loc is 
+                                                 there in any key in the dictionary, if its there then codegen and recuse on  
+                                                 the args-}
+    ProjE i e                     -> ProjE i <$> go e {-Simple recursion on e-}
+    IfE a b c                     -> do  {-Check if there are freeVariales in the condition a, if the set has any freeVars from "a" then codegen all the locations and regions before the IfE-}
+                                     let freeSet = freeVars a
+                                          in case freeSet of
+                                            Nothing      -> (IfE a) <$> go b <*> go c
+                                            Just freeSet -> do
+                                              
+
+        
+    MkProdE ls                    -> MkProdE <$> mapM go ls {-Recurse over all expression in the tuple in the expression ls-}
+    LetE (v,locs,ty,rhs) bod      -> _ {-Need to check the locs in the keys of the set before recursing-}
     CaseE scrt mp                 -> error "For CaseE statements we would need to check for the free variable and decide to move the regions inwards or not"
-    TimeIt e ty b                 -> error "What is TimeIt" --Benchmarking function, recuse e
-    SpawnE{}                      -> return ex --I think we can just return the expression for this
-    SyncE{}                       -> return ex --I think we can just return the expression for this
-    WithArenaE v e                -> error "Not sure what WithArena is"
+    TimeIt e ty b                 -> do
+      e' <- go e
+      return $ TimeIt e' ty b
+    SpawnE{}                      -> pure ex   
+    SyncE{}                       -> pure ex
+    WithArenaE v e                -> WithArenaE v <$> go e
     MapE{}                        -> return ex --Is there a recursion element to this?
     FoldE{}                       -> return ex --Is there a recursion element to this?
+  where 
+    go = placeRegionInwards dictionary
 
-
+    --Cases for which checking the local variables might be important
     --AppE
     --DataConE
     --IfE
     --LetE
     --CaseE
     --SpawnE
+
+
+codeGen :: [LocVar] -> DelayedBindEnv -> Exp2
+codeGen list dict = do 
+  let keyList' = M.keys dictionary
+    in case list of
+      []     -> Nothing
+      (x:xs) -> do
+        let key' = F.find (\a -> (S.member x a)) keyList'
+            in case key' of 
+              Nothing -> error "Could not find any existing variables"
+              Just key' -> 
