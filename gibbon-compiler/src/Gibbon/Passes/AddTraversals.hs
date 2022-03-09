35ca0288b2e89393f0a600900594437f16b9544e
Chaitanya Koparkar <ckoparkar@gmail.com>
2022-03-09 09:55:26 -0500

    Fix GHC warnings

--- gibbon-compiler/src/Gibbon/Passes/AddTraversals.hs
+++ gibbon-compiler/src/Gibbon/Passes/AddTraversals.hs
@@ -2,7 +2,7 @@ module Gibbon.Passes.AddTraversals
   (addTraversals, needsTraversalCase) where
 
 import Control.Monad ( forM, when )
-import Data.List as L
+import qualified Data.List as L
 import Data.Map as M
 import Data.Set as S
 
