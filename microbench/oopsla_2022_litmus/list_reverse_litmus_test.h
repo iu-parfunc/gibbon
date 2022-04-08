158291c26776dc5a977f2b804817385ab4c15c50
Vidush SInghal <vidush.sl@gmail.com>
2022-04-08 00:30:39 -0400

    function defs in header

--- microbench/oopsla_2022_litmus/list_reverse_litmus_test.h
+++ microbench/oopsla_2022_litmus/list_reverse_litmus_test.h
@@ -5,3 +5,10 @@ typedef struct list {
     struct list *next;
     int val;
 } List;
+
+
+List *createListnode(int val);
+List * createList(int length);
+List * reverseList(List *head);
+void printList(List *head);
+void freeList(List * head);
