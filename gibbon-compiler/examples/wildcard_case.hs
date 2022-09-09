1faecea0bf7f1e893e9493aaa18d1d104ddc2286
Aditya Gupta <adityagupta1089@gmail.com>
2022-09-09 04:08:18 +0000

    Uncomment tuple values
    nested wildcard working, but taking very long

--- gibbon-compiler/examples/wildcard_case.hs
+++ gibbon-compiler/examples/wildcard_case.hs
@@ -37,15 +37,14 @@ gibbon_main =
         x4 =  fun2 (A 3 6) == 98
         x5 =  fun2 (B 9)   == 36 
         x6 =  fun2 (C)     == 98
-        -- x7 =  eq (A 1 2) (A 1 2)
-        -- x9 =  not (eq (A 1 3) (A 1 4))
-        -- x8 =  not (eq (A 1 3) (B 1))
-        -- y1 =  eq (B 6) (B 6)
-        -- y2 =  not (eq (B 2) (B 4))
-        -- y3 =  not (eq (B 4) (C))
-        -- y4 =  eq (C) (C)
-        -- y5 =  not (eq (C) (A 2 4))
+        x7 =  eq (A 1 2) (A 1 2)
+        x9 =  not (eq (A 1 3) (A 1 4))
+        x8 =  not (eq (A 1 3) (B 1))
+        y1 =  eq (B 6) (B 6)
+        y2 =  not (eq (B 2) (B 4))
+        y3 =  not (eq (B 4) (C))
+        y4 =  eq (C) (C)
+        y5 =  not (eq (C) (A 2 4))
     in 
-        (x1, x2, x3, x4, x5, x6)
-        -- (x1, x2, x3, x4, x5, x6, x7, x8, x9, y1, y2, y3, y4, y5)
+        (x1, x2, x3, x4, x5, x6, x7, x8, x9, y1, y2, y3, y4, y5)
     
