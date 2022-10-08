fc7f1b6164530a275aba2e2d1181a1ae7d7fedce
Chaitanya Koparkar <ckoparkar@gmail.com>
2022-10-08 10:57:02 -0400

    Move GC test examples under a different directory

--- gibbon-compiler/run.sh
+++ gibbon-compiler/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# ./examples/test_tree_update.exe 2
+# ./examples/gc/test_tree_update.exe 2
 
-# ./examples/tree_update2.exe 2
+# ./examples/gc/tree_update2.exe 2
 
-./examples/tree_update3.exe 14
+./examples/gc/tree_update3.exe 14
