3989a35b2779673030fdba22f37949ecc98b8f3c
Vidush Singhal <vidush.sl@gmail.com>
2023-01-21 16:27:24 -0500

    edits

--- gibbon-compiler/examples/layout_benchmarks/blog_management/marmoset/check_fields_pass.hs
+++ gibbon-compiler/examples/layout_benchmarks/blog_management/marmoset/check_fields_pass.hs
@@ -1,16 +1,6 @@
+data Recursive = Constructor (Recursive) (Float) (Int)
 
-type Field2 = Vector Char
-
---data Blog = Layout1 (Int) (Field2) (Blog)
-
-data Blog = Layout1 (Float) (Blog) (Float) (Int)
-
-id_blog :: Blog -> Blog 
-id_blog blog = case blog of 
-    --Layout1 val field2 rst -> Layout1 (val+1) field2 rst
-    Layout1 fl1 rst fl2 int   -> let newRst = id_blog rst 
-                                   in Layout1 (fl1) (copyPacked newRst) (fl2) (int+1)
-    
-    
---id_blog' :: Blog -> Blog 
---id_blog' blog = blog
+foo :: Recursive -> Recursive 
+foo rec = case rec of 
+    Constructor rst fl val -> let newRst = foo rst 
+			      in Constructor (newRst) (fl) (val+1)
