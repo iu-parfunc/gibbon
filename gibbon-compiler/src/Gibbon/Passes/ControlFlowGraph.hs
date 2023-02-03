951be124e9d2fc90f875dca6d2ed5bde18c073de
Vidush Singhal <singhav@europa.ecn.purdue.edu>
2023-02-02 20:47:00 -0500

    edits

--- gibbon-compiler/src/Gibbon/Passes/ControlFlowGraph.hs
+++ gibbon-compiler/src/Gibbon/Passes/ControlFlowGraph.hs
@@ -2,6 +2,8 @@ module Gibbon.Passes.ControlFlowGraph (generateCfg) where
 
 import Data.Graph as G
 import Data.Map as M
+import Data.Set as S
+import Data.List as L
 
 import Gibbon.Common
 import Gibbon.L1.Syntax as L1 
@@ -13,6 +15,8 @@ import Control.Monad as Mo
 -- We should use annotations form the front end to onlt contruct CFG's 
 -- for functions that are annotated. 
 type CFGfunctionMap = M.Map FunDef1 (G.Graph, G.Vertex -> (Exp1, Int, [Int]), Int -> Maybe G.Vertex)
+
+type VariableMap = M.Map Var Maybe (DataCon, Int)
 
 -- For now make this return, the CFGFunctionMap 
 -- But this should ideally return the Constraints 
@@ -44,8 +48,8 @@ generateCfgFunction :: CFGfunctionMap -> FunDef1 -> PassM (CFGfunctionMap, [(Exp
 generateCfgFunction cfgMap f@FunDef { funName, funBody, funTy, funArgs } = do  
     (edgeList, succ, maxDepth) <- generateCFGExp 0 funBody
     let (graph, nodeFromVertex, vertexFromKey) = G.graphFromEdges edgeList
-    let x  = topSort graph --P.map ((\ (vid, _, _) -> vid) . nodeFromVertex) (vertices graph)
-    let x' = P.map nodeFromVertex x 
+    -- let x  = topSort graph
+    -- let x' = P.map nodeFromVertex x 
 
     dbgTraceIt (sdoc x') pure (cfgMap, edgeList)
 
@@ -146,3 +150,63 @@ generateVerticesCase currVertex branch = do
     let newEdges = edgeList ++ [edge]
     pure (newEdges, currVertex, maxDepth) 
     
+
+generateVariableToDataCon :: [(Exp1, Int, [Int])] -> VariableMap
+generateVariableToDataCon vertices = 
+
+
+processVertex :: [(Exp1, Int, [Int])] -> (Exp1, Int, [Int]) -> VariableMap -> VariableMap
+processVertex graph node map = case node of 
+    (expression, id, succ) -> case expression of
+        DataConE loc dcon args ->                    -- do you really need to do it for this expression the way we've designed our passes ??
+        VarE var -> let status = getIndexFromVariableDataCon graph var
+                      in case status of 
+                          Nothing -> map
+                          Maybe position -> --update the map 
+        LitE val -> map
+        CharE ch -> map
+        FloatE val -> map
+        LitSymE var -> let status = getIndexFromVariableDataCon graph var
+                         in case status of 
+                             Nothing -> map 
+                             Maybe position -> --update the map
+        AppE f locs args -> let freeVariables =  S.toList (gFreeVars args)
+                                indexes       = P.map (getIndexFromVariableDataCon graph) freeVariables
+                              in -- update the map with the mapping of the new variables. 
+        PrimAppE f args -> let freeVariables = S.toList (gFreeVars args)
+                               indexes       = P.map 
+        LetE (v,loc,ty,rhs) bod -> 
+        CaseE scrt mp -> 
+        IfE a b c -> 
+        MkProdE xs -> 
+        ProjE i e -> error "processVertex: TODO ProjE"
+        TimeIt e ty b -> error "processVertex: TODO TimeIt"
+        WithArenaE v e -> error "processVertex: TODO WithArenaE"
+        SpawnE f locs args -> error "processVertex: TODO SpawnE"
+        SyncE   -> error "processVertex: TODO SyncE"
+        Ext _   -> error "processVertex: TODO Ext"
+        MapE{}  -> error "processVertex: TODO MapE"
+        FoldE{} -> error "processVertex: TODO FoldE"
+
+getIndexFromVariableDataCon :: [(Exp1, Int, [Int])] -> Var -> Maybe (DataCon, Int)
+getIndexFromVariableDataCon graph variable = case graph of 
+         [] -> Nothing 
+         x:xs -> let status = checkIndexVertex x variable
+                   in case status of 
+                       Nothing -> getIndexFromVariableDataCon xs variable
+                       Just val -> Just val 
+
+checkIndexVertex :: (Exp1, Int, [Int]) -> Var -> Maybe (DataCon, Int)
+checkIndexVertex node variable = case node of 
+    (exp, id, _) -> case exp of 
+        DataConE loc dcon args -> let variables = [var | VarE var <- args]
+                                      results   = map (variable ==) variables
+                                      maybeIndex = L.elemIndex True results
+                                    in case maybeIndex of 
+                                            Nothing  -> Nothing 
+                                            Just val -> Just (dcon, val) 
+        _ -> Nothing    
+
+
+
+-- S.toList (gFreeVars rhs)
