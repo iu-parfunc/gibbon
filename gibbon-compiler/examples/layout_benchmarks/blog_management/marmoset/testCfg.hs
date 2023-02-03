f411a780b0a675afa56ba5e9e7686cd0531e4c4d
Vidush Singhal <singhav@europa.ecn.purdue.edu>
2023-02-03 18:57:27 -0500

    edits

--- gibbon-compiler/examples/layout_benchmarks/blog_management/marmoset/testCfg.hs
+++ gibbon-compiler/examples/layout_benchmarks/blog_management/marmoset/testCfg.hs
@@ -1,6 +1,10 @@
-
 data Tree = Leaf Int | Node Int Tree Tree
 
+--type A = Float 
+--type B = Float
+--type C = Float
+
+--data Struct = Nil | Cons A B C (Struct)   
 
 --testCFG :: Tree -> Int 
 --testCFG tree = case tree of 
@@ -12,39 +16,48 @@ data Tree = Leaf Int | Node Int Tree Tree
 --                rightVal = testCFG right
 --              in newVal + leftVal + rightVal
 
---imkTree :: Int -> Tree
---mkTree i =
---  if i <= 0
---  then Leaf 1
---  else
---      let x = (mkTree (i-1))
---          y = (mkTree (i-1))
---      in Node i x y
+mkTree :: Int -> Tree
+mkTree i =
+  if i <= 0
+  then Leaf 1
+  else
+      let x = (mkTree (i-1))
+          y = (mkTree (i-1))
+       in Node i x y
 
-
---testCFG1 :: Tree -> Int 
---testCFG1 tree = case tree of 
---	Leaf val            -> 2*val + 3*val + 5*val 
---        Node val left right -> 
---	    let 
---	       newVal = val 
---              leftVal = testCFG1 left 
---               rightVal = testCFG1 right 
---             in newVal + leftVal + rightVal   
+testCFG1 :: Tree -> Int 
+testCFG1 tree = case tree of 
+	Leaf val            -> 2*val + 3*val + 5*val 
+        Node val left right -> 
+	    let 
+	       newVal = val 
+               leftVal = testCFG1 left 
+               rightVal = testCFG1 right 
+             in newVal + leftVal + rightVal   
               
 
-foo :: Bool -> Int -> Int -> Int 
-foo a b c = if (a) then 
-                   let b' = b 
-                       c' = c
-                     in b' + c' 
-                   else 
-                   let c' = c 
-                    in c' 
-
+--foo :: Bool ->  -> Int -> Int 
+--foo a b c = if (a) then 
+--                   let b' = b 
+--                       c' = c
+--                     in b' + c' 
+--                   else 
+--                   let c' = c 
+--                    in c' 
+--
 --testCFG2 :: Int -> Int 
 --testCFG2 val = 2*val + 3*val + 5*val 
 --
 --
+--
 
-gibbon_main = foo True 1 2    --testCFG1 (mkTree 10)
+
+--foo :: Bool -> Struct -> Int 
+--foo bool struct = if (bool) then 
+--			    let newStruct = Struct 1.0 2.0 3.0 Nil 
+--                             in  
+--                            else 
+--                             struct 
+
+
+gibbon_main = testCFG1 (mkTree 10)
--- gibbon-compiler/src/Gibbon/Passes/ControlFlowGraph.hs
+++ gibbon-compiler/src/Gibbon/Passes/ControlFlowGraph.hs
@@ -11,12 +11,19 @@ import Prelude as P
 import Control.Monad as Mo
 
 
+import           Data.Set.Ordered ((|>), (|<), (|<>), OSet)
+import qualified Data.Set.Ordered as OSet
+import           Data.Sequence (Seq(..))
+
+
 -- Type to store the CFG's for each function that apprears in code.
 -- We should use annotations form the front end to onlt contruct CFG's 
 -- for functions that are annotated. 
 type CFGfunctionMap = M.Map FunDef1 (G.Graph, G.Vertex -> (Exp1, Int, [Int]), Int -> Maybe G.Vertex)
 
-type VariableMap = M.Map Var Maybe (DataCon, Int)
+type VariableMap = M.Map Var (Maybe (DataCon, Int))
+
+type FieldMap = M.Map FunDef1 (G.Graph, G.Vertex -> ((DataCon, Int), Int, [Int]), Int -> Maybe G.Vertex)
 
 -- For now make this return, the CFGFunctionMap 
 -- But this should ideally return the Constraints 
@@ -48,10 +55,12 @@ generateCfgFunction :: CFGfunctionMap -> FunDef1 -> PassM (CFGfunctionMap, [(Exp
 generateCfgFunction cfgMap f@FunDef { funName, funBody, funTy, funArgs } = do  
     (edgeList, succ, maxDepth) <- generateCFGExp 0 funBody
     let (graph, nodeFromVertex, vertexFromKey) = G.graphFromEdges edgeList
-    -- let x  = topSort graph
-    -- let x' = P.map nodeFromVertex x 
+    let x  = topSort graph
+    let x' = P.map nodeFromVertex x
+    let map = generateVariableToDataCon x'
+     
 
-    dbgTraceIt (sdoc x') pure (cfgMap, edgeList)
+    dbgTraceIt (sdoc map) pure (cfgMap, edgeList)
 
 
 generateCFGExp :: Int -> Exp1 -> PassM ( [(Exp1, Int, [Int])] , Int, Int)
@@ -152,41 +161,67 @@ generateVerticesCase currVertex branch = do
     
 
 generateVariableToDataCon :: [(Exp1, Int, [Int])] -> VariableMap
-generateVariableToDataCon vertices = 
+generateVariableToDataCon vertices = case vertices of 
+    [] -> M.empty
+    x:xs -> let newMap = processVertex vertices x M.empty
+                l      = M.toList (newMap)
+                m      = generateVariableToDataCon xs
+                l'     = M.toList m
+                newMap' = M.fromList (l ++ l')
+             in newMap'
 
 
 processVertex :: [(Exp1, Int, [Int])] -> (Exp1, Int, [Int]) -> VariableMap -> VariableMap
 processVertex graph node map = case node of 
-    (expression, id, succ) -> case expression of
-        DataConE loc dcon args ->                    -- do you really need to do it for this expression the way we've designed our passes ??
-        VarE var -> let status = getIndexFromVariableDataCon graph var
-                      in case status of 
-                          Nothing -> map
-                          Maybe position -> --update the map 
-        LitE val -> map
-        CharE ch -> map
-        FloatE val -> map
-        LitSymE var -> let status = getIndexFromVariableDataCon graph var
-                         in case status of 
-                             Nothing -> map 
-                             Maybe position -> --update the map
-        AppE f locs args -> let freeVariables =  S.toList (gFreeVars args)
-                                indexes       = P.map (getIndexFromVariableDataCon graph) freeVariables
-                              in -- update the map with the mapping of the new variables. 
-        PrimAppE f args -> let freeVariables = S.toList (gFreeVars args)
-                               indexes       = P.map 
-        LetE (v,loc,ty,rhs) bod -> 
-        CaseE scrt mp -> 
-        IfE a b c -> 
-        MkProdE xs -> 
-        ProjE i e -> error "processVertex: TODO ProjE"
-        TimeIt e ty b -> error "processVertex: TODO TimeIt"
-        WithArenaE v e -> error "processVertex: TODO WithArenaE"
-        SpawnE f locs args -> error "processVertex: TODO SpawnE"
-        SyncE   -> error "processVertex: TODO SyncE"
-        Ext _   -> error "processVertex: TODO Ext"
-        MapE{}  -> error "processVertex: TODO MapE"
-        FoldE{} -> error "processVertex: TODO FoldE"
+    (expression, id, succ) -> let newMap = processExpVertex graph expression map
+                                in newMap
+
+
+processExpVertex :: [(Exp1, Int, [Int])] -> Exp1 -> VariableMap -> VariableMap
+processExpVertex graph exp map = case exp of
+    DataConE loc dcon args -> let freeVariables = L.concat (P.map (\x -> S.toList (gFreeVars x)) args)
+                                  maybeIndexes  = P.map (getIndexFromVariableDataCon graph) freeVariables 
+                                  l    = M.toList map 
+                                  l'   = P.zipWith (\x y -> (x, y)) freeVariables maybeIndexes
+                                in M.fromList (l ++ l')
+
+    _                     -> map
+
+        -- VarE var -> let status = getIndexFromVariableDataCon graph var
+        --               in case status of 
+        --                   Nothing -> map
+        --                   Just position -> let newMap = M.insert var status
+        -- LitE val -> map
+        -- CharE ch -> map
+        -- FloatE val -> map
+        -- LitSymE var -> let status = getIndexFromVariableDataCon graph var
+        --                  in case status of 
+        --                      Nothing -> map 
+        --                      Maybe position -> --update the map
+        -- AppE f locs args -> let freeVariables =  S.toList (gFreeVars args)
+        --                         indexes       = P.map (getIndexFromVariableDataCon graph) freeVariables
+        --                       in -- update the map with the mapping of the new variables. 
+        -- PrimAppE f args -> let freeVariables = S.toList (gFreeVars args)
+        --                        indexes       = P.map (getIndexFromVariableDataCon graph) freeVariables 
+
+        --                        -- The body is just the variable that's written to. 
+        -- LetE (v,loc,ty,rhs) bod -> let read_variables = S.toList (gFreeVars rhs)
+        --                                indexes = P.map (getIndexFromVariableDataCon graph) read_variables
+                                       
+        -- CaseE scrt mp -> map -- this case shouldn't happen in the IR 
+        
+        -- IfE a b c -> map  -- This case also should no happen in the IR
+        -- MkProdE xs -> let freeVariables = S.toList (gFreeVars xs)
+        --                   indexes       = P.map (getIndexFromVariableDataCon graph) freeVariables
+        -- ProjE i e -> error "processVertex: TODO ProjE"
+        -- TimeIt e ty b -> error "processVertex: TODO TimeIt"
+        -- WithArenaE v e -> error "processVertex: TODO WithArenaE"
+        -- SpawnE f locs args -> error "processVertex: TODO SpawnE"
+        -- SyncE   -> error "processVertex: TODO SyncE"
+        -- Ext _   -> error "processVertex: TODO Ext"
+        -- MapE{}  -> error "processVertex: TODO MapE"
+        -- FoldE{} -> error "processVertex: TODO FoldE"
+
 
 getIndexFromVariableDataCon :: [(Exp1, Int, [Int])] -> Var -> Maybe (DataCon, Int)
 getIndexFromVariableDataCon graph variable = case graph of 
@@ -200,13 +235,102 @@ checkIndexVertex :: (Exp1, Int, [Int]) -> Var -> Maybe (DataCon, Int)
 checkIndexVertex node variable = case node of 
     (exp, id, _) -> case exp of 
         DataConE loc dcon args -> let variables = [var | VarE var <- args]
-                                      results   = map (variable ==) variables
+                                      results   = P.map (variable ==) variables
                                       maybeIndex = L.elemIndex True results
                                     in case maybeIndex of 
                                             Nothing  -> Nothing 
                                             Just val -> Just (dcon, val) 
-        _ -> Nothing    
+        _ -> Nothing
 
 
+generateFieldGraph :: Int -> [(Exp1, Int, [Int])] -> VariableMap -> ([((DataCon, Int), Int, [Int])], Int, Int)
+generateFieldGraph id graph map = case graph of 
+    [] -> ([], id, id)
+    x:xs -> case x of 
+        (exp, id, succ) -> case exp of
+            DataConE loc dcon args -> let edges  = generateFieldGraphExpList xs args map  
+                                          edges' = generateFieldGraph xs map
+                                        in (edges ++ edges')
+            VarE var -> let edges  = generateFieldGraphVar xs var map
+                            edges' = generateFieldGraph xs map
+                          in edges ++ edges'  
 
--- S.toList (gFreeVars rhs)
+            LitE{}      -> generateFieldGraph xs map 
+            CharE{}     -> generateFieldGraph xs map
+            FloatE{}    -> generateFieldGraph xs map
+            LitSymE var -> let edges  = generateFieldGraphVar xs var map
+                               edges' = generateFieldGraph xs map  
+                             in edges ++ edges'
+
+            AppE f locs args -> let edges  = generateFieldGraphExpList xs args map
+                                    edges' = generateFieldGraph xs map
+                                  in edges ++ edges' 
+
+            PrimAppE f args -> let edges  = generateFieldGraphExpList xs args map 
+                                   edges' = generateFieldGraph xs map
+                                 in edges ++ edges' 
+
+            LetE (v,loc,ty,rhs) bod -> let freeVars = S.toList () 
+
+            CaseE scrt mp -> 
+            IfE a b c -> 
+            MkProdE xs -> 
+            ProjE i e -> error "ControlFlowGraph: TODO ProjE"
+            TimeIt e ty b -> error "ControlFlowGraph: TODO TimeIt"
+            WithArenaE v e -> error "ControlFlowGraph: TODO WithArenaE"
+            SpawnE f locs args -> error "ControlFlowGraph: TODO SpawnE"
+            SyncE   -> error "ControlFlowGraph: TODO SyncE"
+            Ext _   -> error "ControlFlowGraph: TODO Ext"
+            MapE{}  -> error "ControlFlowGraph: TODO MapE"
+            FoldE{} -> error "ControlFlowGraph: TODO FoldE"
+
+generateFieldGraphExpList :: Int -> [Exp1] -> (  [((DataCon, Int), Int, [Int])]  , [Int], Int)
+generateFieldGraphExpList id expList = case expList of 
+    [] -> ([], [], id)
+    x:xs -> let (edgeList, succList, d1)   = generateFieldGraphExpression (id+1) x
+                (edgeList', succList', d2) = generateFieldGraphExpList (d1+1) xs
+                newEdgeList = edgeList ++ edgeList' 
+                newSuccList = succList = succList' 
+              in (newEdgeList, newSuccList, d2)
+
+
+generateFieldGraphExpression :: Int -> Exp1 -> VariableMap -> ([((DataCon, Int), Int, [Int])], [Int], Int) 
+generateFieldGraphExpression vertexCounter exp map = case exp of
+        DataConE loc dcon args -> 
+        VarE{} -> 
+        LitE{} -> 
+        CharE{} -> 
+        FloatE{} -> 
+        LitSymE{} -> 
+        AppE f locs args -> 
+        PrimAppE f args -> 
+        LetE (v,loc,ty,rhs) bod -> 
+        CaseE scrt mp -> 
+        IfE a b c -> 
+        MkProdE xs -> 
+        ProjE i e -> error "ControlFlowGraph: TODO ProjE"
+        TimeIt e ty b -> error "ControlFlowGraph: TODO TimeIt"
+        WithArenaE v e -> error "ControlFlowGraph: TODO WithArenaE"
+        SpawnE f locs args -> error "ControlFlowGraph: TODO SpawnE"
+        SyncE   -> error "ControlFlowGraph: TODO SyncE"
+        Ext _   -> error "ControlFlowGraph: TODO Ext"
+        MapE{}  -> error "ControlFlowGraph: TODO MapE"
+        FoldE{} -> error "ControlFlowGraph: TODO FoldE" 
+
+generateFieldGraphVariable :: Int -> [Var] -> VariableMap -> ( [((DataCon, Int), Int, [Int])], [Int], Int )
+generateFieldGraphVariable id varList map = case varList of 
+         [] -> ( [], [], id )
+         x:xs -> let mapping = M.findWithDefault Nothing x map 
+                   in case mapping of 
+                         Nothing -> ( [], [], id )
+                         Just (dcon, index) -> let ( edgeList, succList, maxDepth ) = generateFieldGraphVariable (id+1) map 
+                                                   edge = [ ((dcon, index), id, succList ]
+                                                   newEdges = edgeList ++ [edge]
+                                                 in (newEdges, [id], maxDepth)
+                                                    
+
+
+-- new idea
+-- Create a custom freeVarible (Read Only) function using an ordered set: https://hackage.haskell.org/package/oset-0.4.0.1/docs/Data-Set-Ordered.html from the CFG 
+-- This should preseve the order in which variables are read in the CFG of the function. 
+-- Once this is done, we can use the variable map created before to draw edges from head of the set to the rest of the elements of the set. 
