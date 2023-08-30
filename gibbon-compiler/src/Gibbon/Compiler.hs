bd6b4a6378521aac7e57179c8493029ea5cd6a76
vidsinghal <vidush.sl@gmail.com>
2023-08-30 11:25:33 -0400

    fix compiler pipeline, change failing tests

--- gibbon-compiler/src/Gibbon/Compiler.hs
+++ gibbon-compiler/src/Gibbon/Compiler.hs
@@ -699,16 +699,13 @@ Also see Note [Adding dummy traversals] and Note [Adding random access nodes].
                   let need = needsRAN l2
                   l1 <- goE1 "addRAN"        (addRAN need) l1
                   l1 <- go "L1.typecheck"    L1.tcProg     l1
-                  -- NOTE: Calling copyOutOfOrderPacked here seems redundant since all the copy calls seem be exists in the correct place.
-                  -- In addititon, calling it here gives a compile time error.
-                  --l1 <- goE1 "copyOutOfOrderPacked" copyOutOfOrderPacked l1
-                  -- l1 <- go "L1.typecheck"    L1.tcProg     l1
+                  l1 <- goE1 "copyOutOfOrderPacked" copyOutOfOrderPacked l1
+                  l1 <- go "L1.typecheck"    L1.tcProg     l1
                   l2 <- go "inferLocations2" inferLocs     l1
-                  --l2 <- go "copyOutOfOrderPacked" copyOutOfOrderPacked l2
                   l2 <- go "simplifyLocBinds" (simplifyLocBinds True) l2
                   l2 <- go "fixRANs"         fixRANs       l2
                   l2 <- go   "L2.typecheck"  L2.tcProg     l2
-                  --l2 <- go "regionsInwards" regionsInwards l2
+                  l2 <- go "regionsInwards" regionsInwards l2
                   l2 <- go   "L2.typecheck"  L2.tcProg     l2
                   l2 <- go "L2.flatten"      flattenL2     l2
                   l2 <- go "findWitnesses" findWitnesses   l2
