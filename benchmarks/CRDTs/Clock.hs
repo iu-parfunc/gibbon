f676a823ccc55251d7d0d72f007488ed42e21e1e
timmy <timmy0x@proton.me>
2023-10-10 09:20:55 -0700

    more moving things around

--- benchmarks/CRDTs/Clock.hs
+++ benchmarks/CRDTs/Clock.hs
@@ -1,31 +1,21 @@
 module Clock where
-    import GibbonMap
-    import Gibbon.Prim
+import Map
 
-    data Order = Eq | Lt | Gt | Cc
-        deriving (Eq, Ord, Enum, Read, Show)
+type Clock = Map Int --Int
 
-    type Clock = Map Int Int
+data Timestamp = Timestamp Int Int
 
-    data Timestamp = Timestamp {
-            uid :: Int,
-            v :: Int
-        }
-        deriving (Eq, Ord, Show)
+init :: Int -> Clock
+init uid = singleton uid 0
 
-    init :: Int -> Clock
-    init uid = GibbonMap.insert uid 0 GibbonMap.empty
+step :: Int -> Clock -> Clock
+step uid clk = case (lookup uid clk) of
+                    Just v -> insert uid (v + 1) clk
+                    Nothing -> insert uid 1 clk
 
-    step :: Int -> Clock -> Clock
-    step uid clk = case v of
-                        Just v -> GibbonMap.insert uid (v + 1) clk
-                        Nothing -> GibbonMap.insert uid 1 clk
-                    where v = GibbonMap.lookup uid clk
-
-    stamp :: Int -> Clock -> Timestamp
-    stamp uid clk = case v of
-                        Just v -> Timestamp { uid = uid, v = v }
-                        Nothing -> Timestamp { uid = uid, v = 0 }
-                    where v = GibbonMap.lookup uid clk
+stamp :: Int -> Clock -> Timestamp
+stamp uid clk = case (lookup uid clk) of
+                    Just v -> Timestamp uid v
+                    Nothing -> Timestamp uid 0
     
 gibbon_main = stamp 0 (step 0 (step 1 (step 0 (init 0))))
