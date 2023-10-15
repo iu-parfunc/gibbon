53c0396a4ee080500bdfc7d7997267d0868802ed
timmy <timmy0x@proton.me>
2023-10-15 08:51:04 -0700

    fixed clock map issues in ghc

--- benchmarks/CRDTs/ClockMap.hs
+++ benchmarks/CRDTs/ClockMap.hs
@@ -8,14 +8,14 @@ import Clock
 
 type Size = Int
 data ClockMap a  = Tip
-            | Bin Size Int a (ClockMap a) (ClockMap a)
+            | Bin Size Clock a (ClockMap a) (ClockMap a)
 
 -- Construction -----------------------
 
 empty :: ClockMap a
 empty = Tip
 
-singleton :: Int -> a -> ClockMap a
+singleton :: Clock -> a -> ClockMap a
 singleton k x = Bin 1 k x Tip Tip
 
 
@@ -31,35 +31,35 @@ size m = case m of
             Tip -> 0
             Bin sz _ _ _ _ -> sz
 
-key :: ClockMap a -> Int
+key :: ClockMap a -> Clock
 key s = case s of
-            Tip -> 0
+            Tip -> (Clock.init 0)
             Bin _ k _ _ _ -> k
 
-lookup :: Int -> ClockMap a -> Maybe a
+lookup :: Clock -> ClockMap a -> Common.Maybe a
 lookup k m = 
     case m of
-        Tip -> Nothing
+        Tip -> Common.Nothing
         Bin _ kx v l r ->
-            case (compare k kx) of
-                Eq -> Just v
-                Lt -> lookup k l
-                Gt -> lookup k r
-                Cc -> lookup k r
+            case (Clock.compare k kx) of
+                Eq -> Common.Just v
+                Lt -> ClockMap.lookup k l
+                Gt -> ClockMap.lookup k r
+                Cc -> ClockMap.lookup k r
 
-member :: Int -> ClockMap a -> Bool
-member k m = case (lookup k m) of
-    Nothing -> False
-    Just _  -> True
+member :: Clock -> ClockMap a -> Bool
+member k m = case (ClockMap.lookup k m) of
+    Common.Nothing -> False
+    Common.Just _  -> True
 
 -- Insertion --------------------------
 
-insert :: Int -> a -> ClockMap a -> ClockMap a
+insert :: Clock -> a -> ClockMap a -> ClockMap a
 insert kx x m =
     case m of
         Tip -> singleton kx x
         Bin sz k v l r ->
-            case (compare k kx) of 
+            case (Clock.compare k kx) of 
                 Eq -> Bin sz k x l r
                 Lt -> balance k v (insert kx x l) r
                 Gt -> balance k v l (insert kx x r)
@@ -70,21 +70,21 @@ delta = 4
 ratio :: Int
 ratio = 2
 
-balance :: Int -> a -> ClockMap a -> ClockMap a -> ClockMap a
+balance :: Clock -> a -> ClockMap a -> ClockMap a -> ClockMap a
 balance k x l r =
         if (size l) + (size r) <= 1           then Bin ((size l) + (size r)) k x l r
         else if (size r) >= delta*(size l)    then rotateL k x l r
         else if (size l) >= delta*(size r)    then rotateR k x l r
         else                                       Bin ((size l) + (size r)) k x l r  
 
-rotateL :: Int -> b -> ClockMap b -> ClockMap b -> ClockMap b
+rotateL :: Clock -> b -> ClockMap b -> ClockMap b -> ClockMap b
 rotateL k x l r =
     case r of 
         Bin _ _ _ ly ry ->
             if (size ly) < ratio*(size ry) then singleL k x l r
             else                                doubleL k x l r
         Tip -> empty -- cry
-rotateR :: Int -> b -> Map b -> Map b -> Map b
+rotateR :: Clock -> b -> ClockMap b -> ClockMap b -> ClockMap b
 rotateR k x l r =
     case l of
         Bin _ _ _ ly ry ->
@@ -92,31 +92,31 @@ rotateR k x l r =
             else                                doubleR k x l r
         Tip -> empty --cry
 
-bin :: Int -> a -> ClockMap a -> ClockMap a -> ClockMap a
+bin :: Clock -> a -> ClockMap a -> ClockMap a -> ClockMap a
 bin k x l r = Bin ((size l) + (size r) + 1) k x l r
 
-singleL :: Int -> b -> ClockMap b -> ClockMap b -> ClockMap b
+singleL :: Clock -> b -> ClockMap b -> ClockMap b -> ClockMap b
 singleL k1 x1 t1 m =
     case m of 
         Bin _ k2 x2 t2 t3 -> bin k2 x2 (bin k1 x1 t1 t2) t3
         Tip -> empty --cry
 
-singleR :: Int -> b -> ClockMap b -> ClockMap b -> ClockMap b
+singleR :: Clock -> b -> ClockMap b -> ClockMap b -> ClockMap b
 singleR k1 x1 m t3 =
     case m of
         Bin _ k2 x2 t1 t2 -> bin k2 x2 t1 (bin k1 x1 t2 t3)
         Tip -> empty --cry
 
-doubleL :: Int -> b -> ClockMap b -> ClockMap b -> ClockMap b
+doubleL :: Clock -> b -> ClockMap b -> ClockMap b -> ClockMap b
 doubleL k1 x1 t1 m0 =
     case m0 of
         Bin _ k2 x2 m1 t4 -> 
             case m1 of 
                 Bin _ k3 x3 t2 t3 -> bin k3 x3 (bin k1 x1 t1 t2) (bin k2 x2 t3 t4)
                 Tip -> empty --cry
-        Tip _ _ _ _  -> empty --cry
+        Tip -> empty --cry
 
-doubleR :: Int -> b -> ClockMap b -> ClockMap b -> ClockMap b
+doubleR :: Clock -> b -> ClockMap b -> ClockMap b -> ClockMap b
 doubleR k1 x1 m0 t4 =
     case m0 of
         Bin _ k2 x2 t1 m1 -> 
@@ -125,4 +125,17 @@ doubleR k1 x1 m0 t4 =
                 Tip -> empty
         Tip -> empty --cry
 
-gibbon_main = insert 0 "f" (insert 5 "f" (insert 4 "e" (insert 3 "d" (insert 2 "c" (insert 1 "b" (insert 0 "a" empty))))))
+gibbon_main = 
+    let clk0 = Clock.init 0
+        m0 = singleton clk0 "a"
+        clk1 = step 1 clk0
+        m1 = insert clk1 "a"
+        clk2 = step 2 clk1
+        m2 = insert clk2 "a"
+        clk3 = step 0 clk2
+        m3 = insert clk3 "b"
+        clk4 = step 1 clk3
+        m4 = insert clk4 "b"
+        clk5 = step 2 clk4
+        m5 = insert clk5 "b"
+    in m5
--- benchmarks/CRDTs/Common.hs
+++ benchmarks/CRDTs/Common.hs
@@ -4,3 +4,12 @@ data Maybe a = Nothing
             | Just a
 
 data Ord = Lt | Gt | Eq | Cc
+
+compareInt :: Int -> Int -> Common.Ord
+compareInt a b = 
+    let sub = a - b
+    in 
+        if sub < 0 then Lt
+        else if sub > 0 then Gt
+        else if sub == 0 then Eq
+        else Cc
