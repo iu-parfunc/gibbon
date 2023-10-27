602050893d65371911569eb086188c478049dea5
timmy <timmy0x@proton.me>
2023-10-27 10:58:02 -0700

    repeatable module import collisions

--- gibbon-compiler/examples/addimport.hs
+++ gibbon-compiler/examples/addimport.hs
@@ -1,7 +1,5 @@
 module Addimport where
     import Addone
-    import Addtwo
-
 
     main :: Tree
-    main = Addtwo.add1 2 (Addtwo.add1 (Node (Leaf 1) (Leaf 2)))
+    main = Addone.add1 2 (Addone.add1 (Node (Leaf 1) (Leaf 2)))
