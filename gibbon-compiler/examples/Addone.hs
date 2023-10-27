4e81af39202f74ae9af62dbed5db5bcf9de45d51
timmy <timmy0x@proton.me>
2023-10-27 10:58:02 -0700

    repeatable module import collisions

--- gibbon-compiler/examples/Addone.hs
+++ gibbon-compiler/examples/Addone.hs
@@ -1,8 +1,8 @@
 module Addone where 
     data Tree = Leaf Int | Node Tree Tree
 
-    add1 :: Tree -> Tree
-    add1 t = case t of
+    add/1 :: Tree -> Tree
+    add/1 t = case t of
             Leaf x     -> Leaf (x + 1)
             Node x1 x2 -> Node (add1 x1) (add1 x2)
 
--- gibbon-compiler/examples/Addtwo.hs
+++ gibbon-compiler/examples/Addtwo.hs
@@ -1,7 +1,7 @@
-module Addone where 
+module Addtwo where 
     data Tree = Leaf Int | Node Tree Tree
 
-    add1 :: Tree -> Tree
+    add/1 :: Tree -> Tree
     add1 t = case t of
             Leaf x     -> Leaf (x + 1)
             Node x1 x2 -> Node (add1 x1) (add1 x2)
