33f7d48ad0daf4794948a6936702266592fb50b7
timmy <timmy0x@proton.me>
2023-11-06 06:38:57 -0800

    declaration renaming works

--- gibbon-compiler/examples/add1.hs
+++ gibbon-compiler/examples/add1.hs
@@ -1,9 +1,10 @@
+module Add where
 data Tree = Leaf Int | Node Tree Tree
 
 add1 :: Tree -> Tree
 add1 t = case t of
            Leaf x     -> Leaf (x + 1)
-           Node x1 x2 -> Node (add1 x1) (add1 x2)
+           Node x1 x2 -> Node (Add.add1 x1) (Add.add1 x2)
 
-main :: Tree
-main = add1 (Node (Leaf 1) (Leaf 2))
+gibbon_main :: Tree
+gibbon_main = Add.add1 (Node (Leaf 1) (Leaf 2))
