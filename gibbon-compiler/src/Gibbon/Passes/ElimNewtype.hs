17bff946efffa24f62f9d70551680d85c10228e1
jazullo <josephazullo@gmail.com>
2023-12-16 21:28:45 +0000

    Newtype elimination starting from definitions

--- gibbon-compiler/src/Gibbon/Passes/ElimNewtype.hs
+++ gibbon-compiler/src/Gibbon/Passes/ElimNewtype.hs
@@ -3,24 +3,32 @@ module Gibbon.Passes.ElimNewtype where
 import Gibbon.L1.Syntax
 import Gibbon.Common
 
+import Control.Arrow
 import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.Symbol (unintern)
 
 passProgram :: Prog1 -> Prog1
 passProgram prog = 
-  _
+  Prog 
+    { mainExp= (elimE connames *** elimTy tynames) <$> mainExp prog
+    , fundefs= _
+      -- M.map (\d -> d {funTy=elimTy tynames (funTy d)}) (fundefs prog)
+      -- issue: Ty1 is too degenerate
+    , ddefs=tys
+    }
   where
-    _ = M.filter (\x -> length (dataCons x) == 1)
-    _ = ddefs prog  -- filter out cases of size 1
-  
-  
-  -- hcat
-  -- [ ppDDefs $ ddefs prog
-  -- , ppFunDefs $ fundefs prog
-  -- , ppMainExpr $ mainExp prog
-  -- , "\n"
-  -- ]
+    (newtys, tys) = M.partition (\x -> case dataCons x of
+        [(_, [_])] -> True
+        _ -> False
+      ) (ddefs prog)
+    tynames = S.fromList $ (\(Var x) -> unintern x) <$> M.keys newtys
+    connames = S.fromList $ fst . head . dataCons <$> M.elems newtys
 
--- elimE :: Exp1 -> Exp1
--- elimE e0 = case e0 of
---   DataConE _ty0 s es -> _
---   _ -> _
+elimE :: S.Set String -> Exp1 -> Exp1
+elimE cns e0 = case e0 of
+  DataConE _ty0 s es -> _
+  _ -> _
+
+elimTy :: S.Set String -> Ty1 -> Ty1
+elimTy tns _ = _
