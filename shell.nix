dac5e1f0528c3fd1d883b7e36e2cd572058d54eb
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2024-06-12 19:15:45 +0000

    update shell.nix, add GitHub workflow exercising Nix shell

--- shell.nix
+++ shell.nix
@@ -1,20 +1,16 @@
 let
-  moz_overlay = import (builtins.fetchGit {
-                   name = "nixpkgs-mozilla-2023-11-13";
-                   url = "https://github.com/mozilla/nixpkgs-mozilla";
-                   ref = "refs/heads/master";
-                   # Most recent commit hash as of 2023-11-13
-                   rev = "6eabade97bc28d707a8b9d82ad13ef143836736e";
-                 });
   pkgs = import (builtins.fetchGit {
                    url = "https://github.com/nixos/nixpkgs/";
-                   ref = "refs/tags/23.05";
-                 }) { overlays = [ moz_overlay ]; };
+                   ref = "refs/tags/24.05";
+                 }) {};
   stdenv = pkgs.overrideCC pkgs.stdenv pkgs.gcc7;
-  ghc = pkgs.haskell.compiler.ghc94;
-  rust = (pkgs.rustChannelOf { rustToolchain = ./gibbon-rts/rust-toolchain; }).rust;
-  clang = pkgs.clang_14;
-  llvm = pkgs.llvm_14;
+  # stuck with GCC 7 because Cilk was kicked out in GCC 8,
+  # need OpenCilk packaged in nixpkgs, see
+  # https://github.com/NixOS/nixpkgs/issues/144256
+
+  ghc = pkgs.haskell.compiler.ghc910;
+  clang = pkgs.clang_16;
+  llvm = pkgs.llvm_16;
   gibbon_dir = builtins.toString ./.;
 in
   with pkgs;
@@ -25,7 +21,7 @@ in
                     # C/C++
                     clang llvm gcc7 boehmgc uthash
                     # Rust
-                    rust
+                    rustc cargo
                     # Racket
                     racket
                     # Other utilities
