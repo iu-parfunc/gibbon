0966ccad725690eeba7fbfdbdae0295230123ab0
Michael Vollmer <mike@recurial.com>
2025-01-29 17:08:44 +0000

    add optional 'parallel' parameter to shell.nix to enable gcc7, and disable packages broken on arm macos

--- shell.nix
+++ shell.nix
@@ -1,3 +1,4 @@
+{ parallel ? false }:
 let
   pkgs = import (builtins.fetchGit {
                    url = "https://github.com/nixos/nixpkgs/";
@@ -7,29 +8,34 @@ let
   clang = pkgs.clang_16;
   llvm = pkgs.llvm_16;
   gibbon_dir = builtins.toString ./.;
+
+  # Conditionally include old gcc stdenv
+  std = if parallel then pkgs.gcc7Stdenv else pkgs.gcc13Stdenv;
 in
   with pkgs;
-
-  # we are stuck with GCC 7 because Cilk was kicked out in GCC 8,
-  # OpenCilk needs packaging in nixpkgs, see
-  # https://github.com/NixOS/nixpkgs/issues/144256
-  mkShell.override { stdenv = pkgs.gcc7Stdenv; }  {
-
+  let commonPackages = [
+    # C/C++
+    clang llvm uthash
+    # Rust
+    rustc cargo
+    # Racket
+    racket
+    # Other utilities
+    ncurses unzip which rustfmt clippy ghcid
+  ];
+  # Only include gcc7 if parallel is set
+  gcc = if parallel then [gcc7] else [gcc13];
+  # Exclude gdb, rr, and valgrind on ARM Mac
+  extra = if pkgs.hostPlatform.config != "aarch64-apple-darwin"
+          then [gdb rr valgrind] else [];
+  in mkShell.override { stdenv = std; } ({
     # we use default Haskell toolchain supplied with the chosen nixpkgs; this way we hit their cache
     inputsFrom = [ (pkgs.haskellPackages.callCabal2nix "gibbon-compiler" ./gibbon-compiler { }).env ];
 
     name = "basicGibbonEnv";
-    buildInputs = [
-                    # C/C++
-                    clang llvm gcc7 boehmgc uthash
-                    # Rust
-                    rustc cargo
-                    # Racket
-                    racket
-                    # Other utilities
-                    stdenv ncurses unzip which rr rustfmt clippy ghcid gdb valgrind
-                  ];
+    buildInputs = commonPackages ++ gcc ++ extra;
+
     shellHook = ''
       export GIBBONDIR=${gibbon_dir}
     '';
-  }
+  })
