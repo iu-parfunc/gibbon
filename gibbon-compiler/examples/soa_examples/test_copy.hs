3ae22fd7a8b6f13ff90eb28c0d53fe94e6526a64
vidush <vidush.sl@gmail.com>
2025-10-01 08:48:51 -0400

    edits

--- gibbon-compiler/examples/soa_examples/test_copy.hs
+++ gibbon-compiler/examples/soa_examples/test_copy.hs
@@ -13,13 +13,11 @@ foo' tree = tree
 
 gibbon_main = 
       let t1 = foo 20
-          --t4' = Node 12 (Leaf 1) (Leaf 1)
           t2 = foo 50
           t3 = make_node t1 t2
-          -- t4' = Node 12 (Leaf 1) (Leaf 1)
           t4 = foo' t3
           t4' = Node 12 (Leaf 1) (Leaf 2)
-          t5 = Node 10 (Node 12 t4 t4') (Node 10 (Leaf 1) (Leaf 1))
+          t5 = Node 10 (Node 12 t4 t4') t1
         in printPacked t5
 
  
